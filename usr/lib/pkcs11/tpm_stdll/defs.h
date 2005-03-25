0fcfc35cce02a714df039a2bfcc9e3d820ccb17a
kyoder <kyoder>
2005-03-25 22:11:14 +0000

    intial add, needed for changes to mainline

---
+++ usr/lib/pkcs11/tpm_stdll/defs.h
@@ -0,0 +1,19 @@
+
+/* (C) COPYRIGHT International Business Machines Corp. 2001, 2002, 2005 */
+
+
+// File:  defs.h
+//
+// Contains various definitions needed by both the host-side
+// and coprocessor-side code.
+//
+
+#ifndef _TPM_DEFS_H
+#define _TPM_DEFS_H
+
+#include "../common/defs.h"
+
+#define MAX_PIN_LEN           128
+#define MIN_PIN_LEN           6
+
+#endif
