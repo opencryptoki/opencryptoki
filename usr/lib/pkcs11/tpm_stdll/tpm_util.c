33c784037284e00e97afde9fae0bf20f45f233ff
kyoder <kyoder>
2005-03-28 16:48:21 +0000

    added util_create_id() function and added a few includes

--- usr/lib/pkcs11/tpm_stdll/tpm_util.c
+++ usr/lib/pkcs11/tpm_stdll/tpm_util.c
@@ -20,6 +20,7 @@
  * http://www.opensource.org/licenses/cpl1.0.php.
  */
 
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -27,6 +28,8 @@
 #include <string.h>
 #include <pwd.h>
 #include <grp.h>
+
+#include <openssl/rsa.h>
 
 #include <tss/tss.h>
 
@@ -36,6 +39,77 @@
 #include "host_defs.h"
 #include "args.h"
 #include "h_extern.h"
+
+#include "tpm_specific.h"
+
+char *
+util_create_id(int type)
+{
+	char *ret = NULL;
+	int size = 0;
+
+	switch (type) {
+		case TPMTOK_PUB_ROOT_KEY:
+			size = TPMTOK_PUB_ROOT_KEY_ID_SIZE + 1;
+			if ((ret = malloc(size)) == NULL) {
+				st_err_log("CKR_HOST_MEMORY");
+				break;
+			}
+
+			sprintf(ret, "%s", TPMTOK_PUB_ROOT_KEY_ID);
+			break;
+		case TPMTOK_ROOT_KEY:
+			size = TPMTOK_ROOT_KEY_ID_SIZE + 1;
+			if ((ret = malloc(size)) == NULL) {
+				st_err_log("CKR_HOST_MEMORY");
+				break;
+			}
+
+			sprintf(ret, "%s", TPMTOK_ROOT_KEY_ID);
+			break;
+		case TPMTOK_MIG_ROOT_KEY:
+			size = TPMTOK_MIG_ROOT_KEY_ID_SIZE + 2;
+			if ((ret = malloc(size)) == NULL) {
+				st_err_log("CKR_HOST_MEMORY");
+				break;
+			}
+
+			sprintf(ret, "%s", TPMTOK_MIG_ROOT_KEY_ID);
+			break;
+		case TPMTOK_MIG_LEAF_KEY:
+			size = TPMTOK_MIG_LEAF_KEY_ID_SIZE + 2;
+			if ((ret = malloc(size)) == NULL) {
+				st_err_log("CKR_HOST_MEMORY");
+				break;
+			}
+
+			sprintf(ret, "%s", TPMTOK_MIG_LEAF_KEY_ID);
+			break;
+		case TPMTOK_USER_LEAF_KEY:
+			size = strlen(TPMTOK_USERNAME) + TPMTOK_USER_LEAF_KEY_ID_SIZE + 2;
+			if ((ret = malloc(size)) == NULL) {
+				st_err_log("CKR_HOST_MEMORY");
+				break;
+			}
+
+			sprintf(ret, "%s %s", TPMTOK_USERNAME, TPMTOK_USER_LEAF_KEY_ID);
+			break;
+		case TPMTOK_USER_BASE_KEY:
+			size = strlen(TPMTOK_USERNAME) + TPMTOK_USER_BASE_KEY_ID_SIZE + 2;
+			if ((ret = malloc(size)) == NULL) {
+				st_err_log("CKR_HOST_MEMORY");
+				break;
+			}
+
+			sprintf(ret, "%s %s", TPMTOK_USERNAME, TPMTOK_USER_BASE_KEY_ID);
+			break;
+		default:
+			LogError("Unknown type passed to %s: %d", __FUNCTION__, type);
+			break;
+	}
+
+	return ret;
+}
 
 int
 util_set_file_mode(char *filename, mode_t mode)
