ec1ca33fb4daaed4f55f623489a09ec55b8697f0
kyoder <kyoder>
2005-04-11 20:43:38 +0000

    added CKM_PKCS_RSA_OAEP mechanism for the TPM token only

--- usr/lib/pkcs11/tpm_stdll/decr_mgr.c
+++ usr/lib/pkcs11/tpm_stdll/decr_mgr.c
@@ -351,7 +351,7 @@ decr_mgr_init( SESSION           *sess,
          break;
 
       case CKM_RSA_X_509:
-      case CKM_RSA_PKCS:
+      case CKM_RSA_PKCS_OAEP:
          {
             if (mech->ulParameterLen != 0)
                return CKR_MECHANISM_PARAM_INVALID;
@@ -579,8 +579,8 @@ decr_mgr_decrypt( SESSION           *sess,
                                       in_data,  in_data_len,
                                       out_data, out_data_len );
 
-      case CKM_RSA_PKCS:
-         return rsa_pkcs_decrypt( sess,     length_only,
+      case CKM_RSA_PKCS_OAEP:
+         return rsa_pkcs_oaep_decrypt( sess,     length_only,
                                   ctx,
                                   in_data,  in_data_len,
                                   out_data, out_data_len );
