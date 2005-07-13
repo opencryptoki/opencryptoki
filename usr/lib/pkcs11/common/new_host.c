cfad982124494d6eac5aac66e93ce547bb6520e8
mhalcrow <mhalcrow>
2005-07-13 16:14:41 +0000

    CKF_USER_PIN_TO_BE_CHANGED flag should be set to FALSE when the user PIN is modified.

--- usr/lib/pkcs11/common/new_host.c
+++ usr/lib/pkcs11/common/new_host.c
@@ -1195,8 +1195,9 @@ CK_RV SC_InitPIN( ST_SESSION_HANDLE  sSession,
       st_err_log(150, __FILE__, __LINE__);
       goto done;
    }
-      memcpy( nv_token_data->user_pin_sha, hash_sha, SHA1_HASH_SIZE );
-      nv_token_data->token_info.flags |= CKF_USER_PIN_INITIALIZED;
+   memcpy( nv_token_data->user_pin_sha, hash_sha, SHA1_HASH_SIZE );
+   nv_token_data->token_info.flags |= CKF_USER_PIN_INITIALIZED;
+   nv_token_data->token_info.flags &= ~(CKF_USER_PIN_TO_BE_CHANGED);
    XProcUnLock( xproclock );
 
    memcpy( user_pin_md5, hash_md5, MD5_HASH_SIZE  );
