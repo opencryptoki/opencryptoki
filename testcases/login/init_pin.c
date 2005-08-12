7cde259e4cae52e0e6263b8f3824fde94ee13f55
kyoder <kyoder>
2005-08-12 16:17:12 +0000

    intial add of login testcases

---
+++ testcases/login/init_pin.c
@@ -0,0 +1,120 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+
+#include <dlfcn.h>
+#include <sys/timeb.h>
+
+#include <pkcs11/pkcs11types.h>
+
+#include "common.h"
+
+int
+do_InitPIN(CK_FUNCTION_LIST *funcs, CK_SLOT_ID slot_id, char *sologinpass, char *userinitpass)
+{
+	CK_RV rc;
+	CK_SESSION_HANDLE session;
+	CK_FLAGS flags = CKF_SERIAL_SESSION | CKF_RW_SESSION;
+
+	rc = funcs->C_OpenSession(slot_id, flags, NULL, NULL, &session);
+	if (rc != CKR_OK) {
+		show_error("C_OpenSession", rc);
+		return rc;
+	}
+
+	rc = funcs->C_Login(session, CKU_SO, sologinpass, strlen(sologinpass));
+	if (rc != CKR_OK) {
+		show_error("C_Login", rc);
+		return rc;
+	}
+
+	printf("Logged in the SO successfully, calling C_InitPIN...\n");
+
+	rc = funcs->C_InitPIN(session, userinitpass, strlen(userinitpass));
+	if (rc != CKR_OK) {
+		show_error("C_InitPIN", rc);
+		funcs->C_Logout(session);
+		funcs->C_CloseSession(session);
+		return rc;
+	} else {
+		printf("Success.\n");
+	}
+
+	rc = funcs->C_Logout(session);
+	if (rc != CKR_OK) {
+		show_error("C_Logout", rc);
+		return rc;
+	}
+
+	printf("Logged out.\n");
+
+	rc = funcs->C_CloseSession(session);
+	if (rc != CKR_OK) {
+		show_error("C_CloseSession", rc);
+		return rc;
+	}
+
+	return rc;
+}
+
+void
+usage(char *argv0)
+{
+	printf("usage:  %s [-slot <num>] [-h] [-user|-so] -sopass pass -userpass pass\n\n", argv0 );
+	printf("By default, Slot %d is used, as user (which should fail)\n\n", SLOT_ID_DEFAULT);
+	exit(-1);
+}
+
+//
+//
+int
+main( int argc, char **argv )
+{
+	CK_C_INITIALIZE_ARGS	cinit_args;
+	CK_FUNCTION_LIST	*funcs = NULL;
+	int			rc, i;
+	char			*sopass = NULL, *userpass = NULL;
+	int			slot_id = 0;
+
+	for (i=1; i < argc; i++) {
+		if (strcmp(argv[i], "-sopass") == 0) {
+			++i;
+			sopass = argv[i];
+		} else if (strcmp(argv[i], "-userpass") == 0) {
+			++i;
+			userpass = argv[i];
+		} else if (strcmp(argv[i], "-slot") == 0) {
+			++i;
+			slot_id = atoi(argv[i]);
+		} else {
+			usage(argv[0]);
+		}
+	}
+
+	if (!sopass || !userpass)
+		usage(argv[0]);
+
+	if (slot_id != SLOT_ID_DEFAULT)
+		printf("Using user specified slot %d.\n", slot_id);
+
+	rc = do_GetFunctionList(&funcs);
+	if (funcs == NULL)
+		return -1;
+
+	memset( &cinit_args, 0, sizeof(cinit_args) );
+	cinit_args.flags = CKF_OS_LOCKING_OK;
+
+	rc = funcs->C_Initialize( &cinit_args );
+	if (rc != CKR_OK) {
+		show_error("C_Initialize", rc);
+		return -1;
+	}
+
+	rc = do_InitPIN(funcs, slot_id, sopass, userpass);
+
+	funcs->C_Finalize( NULL );
+
+	return rc;
+}
---
+++ testcases/login/login_test.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+#
+# login_test.sh
+#
+# Kent Yoder <kyoder@users.sf.net>
+#
+# usage: login_test.sh -slot [n]
+#
+# By default, slot 0 is used.  This script will run through several
+# scenarios WRT login's to the PKCS#11 API.  It expects a completely
+# uninitialized token, such as right after installation. It is
+# expected that the token will be reinitialized after running this test.
+#
+set -x
+
+
+DEFAULT_SO_PIN=87654321
+DEFAULT_USER_PIN=12345678
+
+NEW_USER_PIN1=userPW1
+NEW_USER_PIN2=userPW2
+NEW_SO_PIN1=so_PW1
+NEW_SO_PIN2=so_PW2
+BAD_PIN=bad
+
+CKR_PIN_EXPIRED=163
+CKR_PIN_INVALID=161
+CKR_PIN_INCORRECT=160
+CKR_USER_PIN_NOT_INITIALIZED=2
+CKR_OK=0
+
+#init the token
+./init_tok $* -pass $DEFAULT_SO_PIN
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try to login as SO with a bad pass
+./login $* -so -pass bad
+if test $? -ne $CKR_PIN_INCORRECT; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try to login as USER before init
+./login $* -user -pass $DEFAULT_USER_PIN
+if test $? -ne $CKR_USER_PIN_NOT_INITIALIZED; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try a correct SO login, should SUCCEED
+./login $* -so -pass $DEFAULT_SO_PIN
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try to do something after logging in before PIN is set
+./digest_init $* -so -pass $DEFAULT_SO_PIN
+if test $? -ne $CKR_PIN_EXPIRED; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try to set pin to the default value
+./set_pin $* -so -old $DEFAULT_SO_PIN -new $DEFAULT_SO_PIN
+if test $? -ne $CKR_PIN_INVALID; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Do a legitimate pin set for the SO
+./set_pin $* -so -old $DEFAULT_SO_PIN -new $NEW_SO_PIN1
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Init the USER PIN
+./init_pin $* -sopass $NEW_SO_PIN1 -userpass $DEFAULT_USER_PIN
+if test $? -ne $CKR_OK; then
+	echo "TEST_FAIL"
+	exit
+fi
+
+# Try to set pin to the default value
+./set_pin $* -user -old $DEFAULT_USER_PIN -new $DEFAULT_USER_PIN
+if test $? -ne $CKR_PIN_INVALID; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try to do something after logging in before PIN is set
+./digest_init $* -user -pass $DEFAULT_USER_PIN
+if test $? -ne $CKR_PIN_EXPIRED; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Do a legitimate pin set for the USER
+./set_pin $* -user -old $DEFAULT_USER_PIN -new $NEW_USER_PIN1
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# login with the good pins
+./login $* -so -pass $NEW_SO_PIN1
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+./login $* -user -pass $NEW_USER_PIN1
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# Try login with bad pins
+./login $* -so -pass $BAD_PIN
+if test $? -ne $CKR_PIN_INCORRECT; then
+	echo "TEST FAIL"
+	exit
+fi
+
+./login $* -user -pass $BAD_PIN
+if test $? -ne $CKR_PIN_INCORRECT; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# try to change both pins back to defaults (should fail)
+./set_pin $* -so -old $NEW_SO_PIN1 -new $DEFAULT_SO_PIN
+if test $? -ne $CKR_PIN_INVALID; then
+	echo "TEST FAIL"
+	exit
+fi
+
+./set_pin $* -user -old $NEW_USER_PIN1 -new $DEFAULT_USER_PIN
+if test $? -ne $CKR_PIN_INVALID; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# change both pins legitimately
+./set_pin $* -so -old $NEW_SO_PIN1 -new $NEW_SO_PIN2
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+./set_pin $* -user -old $NEW_USER_PIN1 -new $NEW_USER_PIN2
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+# login with new passes
+./login $* -so -pass $NEW_SO_PIN2
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+./login $* -user -pass $NEW_USER_PIN2
+if test $? -ne $CKR_OK; then
+	echo "TEST FAIL"
+	exit
+fi
+
+echo "TEST SUCCEEDED"
+exit 0
