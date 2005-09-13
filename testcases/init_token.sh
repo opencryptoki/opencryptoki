5288c29d3651004f33294ad27043a9a9f692c91d
mhalcrow <mhalcrow>
2005-09-13 18:43:13 +0000

    Update PIN settings for new values in 2.2.x

--- testcases/init_token.sh
+++ testcases/init_token.sh
@@ -2,7 +2,7 @@
 
 set timeout 5
 
-spawn /usr/lib/pkcs11/methods/pkcsconf -c [lindex $argv 0] -I
+spawn pkcsconf -c [lindex $argv 0] -I
 expect "Enter the SO PIN: "
 sleep .1
 send "87654321\r"
@@ -13,7 +13,7 @@ send "ibmtest\r"
 sleep .1
 expect eof
 
-spawn /usr/lib/pkcs11/methods/pkcsconf -c [lindex $argv 0] -P
+spawn pkcsconf -c [lindex $argv 0] -P
 expect "Enter the SO PIN: "
 sleep .1
 send "87654321\r"
@@ -28,47 +28,32 @@ send "76543210\r"
 sleep .1
 expect eof
 
-spawn /usr/lib/pkcs11/methods/pkcsconf -c [lindex $argv 0] -P
+spawn pkcsconf -c [lindex $argv 0] -u
 expect "Enter the SO PIN: "
 sleep .1
 send "76543210\r"
-sleep .1
-expect "Enter the new SO PIN: "
-sleep .1
-send "87654321\r"
-sleep .1
-expect "Re-enter the new SO PIN: "
-sleep .1
-send "87654321\r"
-sleep .1
-expect eof
-
-spawn /usr/lib/pkcs11/methods/pkcsconf -c [lindex $argv 0] -u
-expect "Enter the SO PIN: "
-sleep .1
-send "87654321\r"
 sleep .1
 expect "Enter the new user PIN: "
 sleep .1
-send "01234567\r"
+send "12345678\r"
 sleep .1
 expect "Re-enter the new user PIN: "
 sleep .1
-send "01234567\r"
+send "12345678\r"
 sleep .1
 expect eof
 
-spawn /usr/lib/pkcs11/methods/pkcsconf -c [lindex $argv 0] -p
+spawn pkcsconf -c [lindex $argv 0] -p
 expect "Enter user PIN: "
 sleep .1
-send "01234567\r"
+send "12345678\r"
 sleep .1
 expect "Enter the new user PIN: "
 sleep .1
-send "12345678\r"
+send "01234567\r"
 sleep .1
 expect "Re-enter the new user PIN: "
 sleep .1
-send "12345678\r"
+send "01234567\r"
 sleep .1
 expect eof
