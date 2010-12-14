b4744379e6413a0e28a281b9927ebd0d6363420d
Klaus Heinrich Kiwi <klausk@linux.vnet.ibm.com>
2010-12-13 23:50:18 -0200

    Introducing ock_log_err(OCK_E_PARALLEL_SESS)
    Signed-off-by: Klaus Heinrich Kiwi <klausk@linux.vnet.ibm.com>

--- usr/lib/pkcs11/tpm_stdll/new_host.c
+++ usr/lib/pkcs11/tpm_stdll/new_host.c
@@ -1259,7 +1259,7 @@ CK_RV SC_OpenSession( CK_SLOT_ID             sid,
    }
 
    if ((flags & CKF_SERIAL_SESSION) == 0) {
-      st_err_log(41, __FILE__, __LINE__); 
+      ock_log_err(OCK_E_PARALLEL_SESS);
       rc = CKR_SESSION_PARALLEL_NOT_SUPPORTED;
       goto done;
    }
