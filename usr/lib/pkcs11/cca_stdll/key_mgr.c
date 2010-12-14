35da93d0482b5dda46d0a2c3ba2d89aaacc5544e
Klaus Heinrich Kiwi <klausk@linux.vnet.ibm.com>
2010-12-14 17:51:48 -0200

    Introducing ock_log_err(OCK_E_TDES_WRAP_GET_DATA)
    Signed-off-by: Klaus Heinrich Kiwi <klausk@linux.vnet.ibm.com>

--- usr/lib/pkcs11/cca_stdll/key_mgr.c
+++ usr/lib/pkcs11/cca_stdll/key_mgr.c
@@ -901,7 +901,7 @@ key_mgr_wrap_key( SESSION           * sess,
       case CKK_DES3:
          rc = des3_wrap_get_data( key2_obj->template, length_only, &data, &data_len );
          if (rc != CKR_OK){
-            st_err_log(93, __FILE__, __LINE__);
+            ock_log_err(OCK_E_TDES_WRAP_GET_DATA);
             return rc;
          }
          break;
