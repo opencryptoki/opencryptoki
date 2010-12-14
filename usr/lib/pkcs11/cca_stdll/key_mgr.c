ba79c3d44382ed886ae629d9f8d9a1e223b3e5f5
Klaus Heinrich Kiwi <klausk@linux.vnet.ibm.com>
2010-12-14 17:52:46 -0200

    Introducing ock_log_err(OCK_E_DSA_WRAP_GET_DATA)
    Signed-off-by: Klaus Heinrich Kiwi <klausk@linux.vnet.ibm.com>

--- usr/lib/pkcs11/cca_stdll/key_mgr.c
+++ usr/lib/pkcs11/cca_stdll/key_mgr.c
@@ -918,7 +918,7 @@ key_mgr_wrap_key( SESSION           * sess,
       case CKK_DSA:
          rc = dsa_priv_wrap_get_data( key2_obj->template, length_only, &data, &data_len );
          if (rc != CKR_OK){
-            st_err_log(95, __FILE__, __LINE__);
+            ock_log_err(OCK_E_DSA_WRAP_GET_DATA);
             return rc;
          }
          break;
