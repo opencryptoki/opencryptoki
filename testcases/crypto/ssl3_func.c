5af403023cb8a02dc83b1157c3115443bf932be6
Joy Latten <latten@linux.vnet.ibm.com>
2013-03-26 11:29:34 -0500

    Refactored ssl3 testcases in ssl3_func.c
    Signed-off-by: Joy Latten <latten@linux.vnet.ibm.com>

--- testcases/crypto/ssl3_func.c
+++ testcases/crypto/ssl3_func.c
@@ -223,134 +223,102 @@ CK_RV do_SignVerify_SSL3_MD5_MAC( void )
 
 //
 //
-CK_RV do_SignVerify_SSL3_SHA1_MAC( void )
+CK_RV do_SignVerify_SSL3_SHA1_MAC(CK_SESSION_HANDLE session)
 {
-	CK_SESSION_HANDLE session;
-	CK_SLOT_ID        slot_id;
 	CK_MECHANISM      mech;
-	CK_ULONG          flags;
 	CK_ULONG          mac_size;
 	CK_ULONG          i;
 	CK_RV             rc;
 
+	CK_OBJECT_HANDLE  h_key;
+	CK_OBJECT_CLASS   key_class = CKO_SECRET_KEY;
+	CK_KEY_TYPE       key_type = CKK_GENERIC_SECRET;
+	CK_BBOOL          false = FALSE;
+	CK_BYTE           hash[SHA1_HASH_LEN];
+	CK_BYTE           data[50];
+	CK_BYTE           key_data[48];
+	CK_ULONG          hash_len;
+	CK_ULONG          data_len;
+	CK_ATTRIBUTE      key_attribs[] =
+	{
+		{CKA_CLASS, &key_class, sizeof(key_class)},
+		{CKA_KEY_TYPE, &key_type, sizeof(key_type)},
+		{CKA_TOKEN, &false, sizeof(false)},
+		{CKA_VALUE, &key_data, sizeof(key_data)}
+	};
 
-
-	printf("do_SignVerify_SSL3_SHA1_MAC...\n");
-
-	slot_id = SLOT_ID;
-	flags = CKF_SERIAL_SESSION | CKF_RW_SESSION;
-	rc = funcs->C_OpenSession( slot_id, flags, NULL, NULL, &session );
-	if (rc != CKR_OK) {
-		show_error("   C_OpenSession #1", rc );
-		return rc;
-	}
+	testcase_begin("starting do_SignVerify_SSL3_SHA1_MAC...\n");
 
 	mac_size = 8;
 
-	mech.mechanism      = CKM_SSL3_SHA1_MAC;
+	mech.mechanism = CKM_SSL3_SHA1_MAC;
 	mech.ulParameterLen = sizeof(CK_ULONG);
-	mech.pParameter     = &mac_size;
+	mech.pParameter = &mac_size;
 
-	{
-		CK_OBJECT_HANDLE  h_key;
-		CK_OBJECT_CLASS   key_class  = CKO_SECRET_KEY;
-		CK_KEY_TYPE       key_type   = CKK_GENERIC_SECRET;
-		CK_BBOOL          false      = FALSE;
-		CK_BYTE           hash[SHA1_HASH_LEN];
-		CK_BYTE           data[50];
-		CK_BYTE           key_data[48];
-		CK_ULONG          hash_len;
-		CK_ULONG          data_len;
-		CK_ATTRIBUTE      key_attribs[] =
-		{
-			{CKA_CLASS,       &key_class,        sizeof(key_class)    },
-			{CKA_KEY_TYPE,    &key_type,         sizeof(key_type)     },
-			{CKA_TOKEN,       &false,            sizeof(false)        },
-			{CKA_VALUE,       &key_data,         sizeof(key_data)     }
-		};
+	for (i=0; i < 48; i++)
+		key_data[i] = i;
 
-		for (i=0; i < 48; i++)
-			key_data[i] = i;
+	memset(data, 0xb, 50);
+	data_len = 50;
 
-		memset( data, 0xb, 50 );
-		data_len = 50;
-
-		rc = funcs->C_CreateObject( session, key_attribs, 4, &h_key );
-		if (rc != CKR_OK) {
-			show_error("   C_CreateObject #1", rc );
-			return rc;
-		}
-
-		rc = funcs->C_SignInit( session, &mech, h_key );
-		if (rc != CKR_OK) {
-			show_error("   C_SignInit #1", rc );
-			return rc;
-		}
-
-		hash_len = sizeof(hash);
-		rc = funcs->C_Sign( session, data, data_len, hash, &hash_len );
-		if (rc != CKR_OK) {
-			show_error("   C_Sign #1", rc );
-			return rc;
-		}
-
-		if (hash_len != mac_size) {
-			PRINT_ERR("   Error:  C_Sign #1 generated bad MAC length\n");
-			return -1;
-		}
-
-		rc = funcs->C_VerifyInit( session, &mech, h_key );
-		if (rc != CKR_OK) {
-			show_error("   C_VerifyInit #1", rc );
-			return rc;
-		}
-
-		rc = funcs->C_Verify( session, data, data_len, hash, hash_len );
-		if (rc != CKR_OK) {
-			show_error("   C_Verify #1", rc );
-			return rc;
-		}
-
-		rc = funcs->C_DestroyObject( session, h_key );
-		if (rc != CKR_OK) {
-			show_error("   C_DestroyObject #1", rc );
-			return rc;
-		}
-	}
-
-	rc = funcs->C_CloseAllSessions( slot_id );
+	rc = funcs->C_CreateObject(session, key_attribs, 4, &h_key);
 	if (rc != CKR_OK) {
-		show_error("   C_CloseAllSessions #1", rc );
+		testcase_error("C_CreateObject() rc = %s", p11_get_ckr(rc));
 		return rc;
 	}
 
-	printf("Looks okay...\n");
+	testcase_new_assertion();
+	rc = funcs->C_SignInit(session, &mech, h_key);
+	if (rc != CKR_OK) {
+		testcase_fail("C_SignInit() rc = %s", p11_get_ckr(rc));
+		goto done;
+	}
+
+	hash_len = sizeof(hash);
+	rc = funcs->C_Sign(session, data, data_len, hash, &hash_len);
+	if (rc != CKR_OK) {
+		testcase_fail("C_Sign() rc = %s", p11_get_ckr(rc));
+		goto done;
+	}
+
+	if (hash_len != mac_size) {
+		testcase_fail("Error: C_Sign generated bad MAC length\n");
+		goto done;
+	} else
+		testcase_pass("Successfully signed.");
+
+	testcase_new_assertion();
+	rc = funcs->C_VerifyInit(session, &mech, h_key);
+	if (rc != CKR_OK) {
+		testcase_error("C_VerifyInit() rc = %s", p11_get_ckr(rc));
+		goto done;
+	}
+
+	rc = funcs->C_Verify(session, data, data_len, hash, hash_len);
+	if (rc != CKR_OK)
+		testcase_fail("C_Verify() rc = %s", p11_get_ckr(rc));
+	else
+		testcase_pass("Successfully verified signature.");
+
+done:
+	if (funcs->C_DestroyObject(session, h_key) != CKR_OK)
+		testcase_error("C_DestroyObject() rc = %s", p11_get_ckr(rc));
+
 	return rc;
 }
 
 
 //
 //
-CK_RV do_SSL3_PreMasterKeyGen( void )
+CK_RV do_SSL3_PreMasterKeyGen(CK_SESSION_HANDLE session)
 {
-	CK_SESSION_HANDLE session;
-	CK_SLOT_ID        slot_id;
 	CK_MECHANISM      mech;
 	CK_VERSION        version;
 	CK_OBJECT_HANDLE  h_key;
-	CK_ULONG          flags;
 	CK_RV             rc;
 
 
-	printf("do_SSL3_PreMasterKeyGen...\n");
-
-	slot_id = SLOT_ID;
-	flags = CKF_SERIAL_SESSION | CKF_RW_SESSION;
-	rc = funcs->C_OpenSession( slot_id, flags, NULL, NULL, &session );
-	if (rc != CKR_OK) {
-		show_error("   C_OpenSession #1", rc );
-		return rc;
-	}
+	testcase_begin("starting do_SSL3_PreMasterKeyGen...\n");
 
 	version.major = 3;
 	version.minor = 0;
@@ -360,116 +328,135 @@ CK_RV do_SSL3_PreMasterKeyGen( void )
 	mech.ulParameterLen = sizeof(CK_VERSION);
 
 
-	rc = funcs->C_GenerateKey( session, &mech, NULL, 0, &h_key );
-	if (rc != CKR_OK) {
-		show_error("   C_GenerateKey #1", rc );
-		return rc;
-	}
+	testcase_new_assertion();
+	rc = funcs->C_GenerateKey(session, &mech, NULL, 0, &h_key);
+	if (rc != CKR_OK)
+		testcase_fail("C_GenerateKey() rc = %s", p11_get_ckr(rc));
+	else
+		testcase_pass("Successfully generated a generic secret key.");
 
-	rc = funcs->C_CloseAllSessions( slot_id );
-	if (rc != CKR_OK) {
-		show_error("   C_CloseAllSessions #1", rc );
-		return rc;
-	}
+	if (funcs->C_DestroyObject(session, h_key) != CKR_OK)
+		testcase_error("C_DestroyObject rc=%s", p11_get_ckr(rc));
 
-	printf("Looks okay...\n");
 	return rc;
 }
 
 
 //
 //
-CK_RV do_SSL3_MasterKeyDerive( void )
+CK_RV do_SSL3_MasterKeyDerive(CK_SESSION_HANDLE session)
 {
-	CK_SESSION_HANDLE session;
-	CK_SLOT_ID        slot_id;
-	CK_MECHANISM      mech;
-	CK_OBJECT_HANDLE  h_pm_secret;
-	CK_OBJECT_HANDLE  h_mk;
-	CK_ULONG          flags;
-	CK_RV             rc;
+	CK_MECHANISM mech;
+	CK_OBJECT_HANDLE h_pm_secret;
+	CK_OBJECT_HANDLE h_mk;
+	CK_RV rc;
 
+	CK_VERSION version = { 3, 0 };
+	CK_ATTRIBUTE pm_tmpl[] =
+	{
+		{CKA_SENSITIVE, &false, sizeof(CK_BBOOL)},
+		{CKA_EXTRACTABLE, &true, sizeof(CK_BBOOL)}
+	};
 
-	printf("do_SSL3_MasterKeyDerive...\n");
+	CK_BYTE  client_random_data[256];
+	CK_BYTE  server_random_data[256];
+	CK_ATTRIBUTE  m_tmpl[] =
+	{
+		{CKA_SENSITIVE, &true, sizeof(CK_BBOOL)},
+		{CKA_EXTRACTABLE, &false, sizeof(CK_BBOOL)}
+	};
 
-	slot_id = SLOT_ID;
-	flags = CKF_SERIAL_SESSION | CKF_RW_SESSION;
-	rc = funcs->C_OpenSession( slot_id, flags, NULL, NULL, &session );
-	if (rc != CKR_OK) {
-		show_error("   C_OpenSession #1", rc );
-		return rc;
-	}
+	CK_SSL3_MASTER_KEY_DERIVE_PARAMS  mk_params;
+	CK_ULONG i;
+
+	testcase_begin("starting do_SSL3_MasterKeyDerive...\n");
 
 	// generate the pre-master secret key
 	//
-	{
-		CK_VERSION    version    = { 3, 0 };
-		CK_ATTRIBUTE  pm_tmpl[] =
-		{
-			{CKA_SENSITIVE,   &false, sizeof(CK_BBOOL) },
-			{CKA_EXTRACTABLE, &true,  sizeof(CK_BBOOL) }
-		};
+	mech.mechanism      = CKM_SSL3_PRE_MASTER_KEY_GEN;
+	mech.pParameter     = &version;
+	mech.ulParameterLen = sizeof(CK_VERSION);
 
-		mech.mechanism      = CKM_SSL3_PRE_MASTER_KEY_GEN;
-		mech.pParameter     = &version;
-		mech.ulParameterLen = sizeof(CK_VERSION);
-
-		rc = funcs->C_GenerateKey( session, &mech, pm_tmpl, 2, &h_pm_secret );
-		if (rc != CKR_OK) {
-			show_error("   C_GenerateKey #1", rc );
-			return rc;
-		}
-	}
+	testcase_new_assertion();
+	rc = funcs->C_GenerateKey(session, &mech, pm_tmpl, 2, &h_pm_secret);
+	if (rc != CKR_OK) {
+		testcase_fail("C_GenerateKey() rc= %s", p11_get_ckr(rc));
+		goto done;
+	} else
+		testcase_pass("Successfully generated a generic secret key.");
 
 	// derive a master key
 	//
-	{
-		CK_BYTE  client_random_data[256];
-		CK_BYTE  server_random_data[256];
 
-		CK_VERSION                        version = {3, 0};
-		CK_SSL3_MASTER_KEY_DERIVE_PARAMS  mk_params;
-		CK_ULONG i;
-
-		for (i=0; i < 256; i++) {
-			client_random_data[i] = i;
-			server_random_data[i] = 256 - i;
-		}
-
-		mk_params.pVersion = &version;
-
-		mk_params.RandomInfo.pClientRandom     = client_random_data;
-		mk_params.RandomInfo.pServerRandom     = server_random_data;
-		mk_params.RandomInfo.ulClientRandomLen = 256;
-		mk_params.RandomInfo.ulServerRandomLen = 256;
-
-		mech.mechanism      = CKM_SSL3_MASTER_KEY_DERIVE;
-		mech.pParameter     = &mk_params;
-		mech.ulParameterLen = sizeof(CK_SSL3_MASTER_KEY_DERIVE_PARAMS);
-
-		rc = funcs->C_DeriveKey( session, &mech, h_pm_secret, NULL, 0, &h_mk );
-		if (rc != CKR_OK) {
-			show_error("   C_Derive #1", rc );
-			return rc;
-		}
+	for (i=0; i < 32; i++) {
+		client_random_data[i] = i;
+		server_random_data[i] = 32 - i;
 	}
 
-	rc = funcs->C_CloseAllSessions( slot_id );
+	mk_params.pVersion = &version;
+
+	mk_params.RandomInfo.pClientRandom = client_random_data;
+	mk_params.RandomInfo.pServerRandom = server_random_data;
+	mk_params.RandomInfo.ulClientRandomLen = 32;
+	mk_params.RandomInfo.ulServerRandomLen = 32;
+
+	mech.mechanism = CKM_SSL3_MASTER_KEY_DERIVE;
+	mech.pParameter = &mk_params;
+	mech.ulParameterLen = sizeof(CK_SSL3_MASTER_KEY_DERIVE_PARAMS);
+
+	testcase_new_assertion();
+	rc = funcs->C_DeriveKey(session, &mech, h_pm_secret, m_tmpl, 2, &h_mk);
 	if (rc != CKR_OK) {
-		show_error("   C_CloseAllSessions #1", rc );
-		return rc;
-	}
+		testcase_fail("C_DeriveKey() rc= %s", p11_get_ckr(rc));
+		goto done;
+	} else
+		testcase_pass("Successfully derived a key from pre-master.");
 
-	printf("Looks okay...\n");
+	if (funcs->C_DestroyObject(session, h_mk) != CKR_OK)
+		testcase_error("C_DestroyObject rc=%s", p11_get_ckr(rc));
+
+done:
+	if (funcs->C_DestroyObject(session, h_pm_secret) != CKR_OK)
+		testcase_error("C_DestroyObject rc=%s", p11_get_ckr(rc));
+
 	return rc;
 }
 
 CK_RV ssl3_functions()
 {
+	CK_RV rc;
 	SYSTEMTIME t1, t2;
-	CK_RV        rc;
+	CK_SLOT_ID slot_id = SLOT_ID;
+	CK_SESSION_HANDLE session;
+	CK_FLAGS flags;
+	CK_BYTE user_pin[PKCS11_MAX_PIN_LEN];
+	CK_ULONG user_pin_len;
 
+	testcase_rw_session();
+	testcase_user_login();
 
+	GetSystemTime(&t1);
+	rc = do_SSL3_PreMasterKeyGen(session);
+	if (rc && !no_stop)
+		goto testcase_cleanup;
+	GetSystemTime(&t2);
+	process_time( t1, t2 );
+
+	GetSystemTime(&t1);
+	rc = do_SSL3_MasterKeyDerive(session);
+	if (rc && !no_stop)
+		goto testcase_cleanup;
+	GetSystemTime(&t2);
+	process_time( t1, t2 );
+
+	GetSystemTime(&t1);
+	rc = do_SignVerify_SSL3_SHA1_MAC(session);
+	if (rc && !no_stop)
+		goto testcase_cleanup;
+	GetSystemTime(&t2);
+	process_time( t1, t2 );
+
+/*
 	GetSystemTime(&t1);
 	rc = do_SignVerify_SSL3_MD5_MAC();
 	if (rc && !no_stop)
@@ -484,19 +471,15 @@ CK_RV ssl3_functions()
 	GetSystemTime(&t2);
 	process_time( t1, t2 );
 
-	GetSystemTime(&t1);
-	rc = do_SSL3_PreMasterKeyGen();
-	if (rc && !no_stop)
-		return rc;
-	GetSystemTime(&t2);
-	process_time( t1, t2 );
+*/
 
-	GetSystemTime(&t1);
-	rc = do_SSL3_MasterKeyDerive();
-	if (rc && !no_stop)
-		return rc;
-	GetSystemTime(&t2);
-	process_time( t1, t2 );
+testcase_cleanup:
+	testcase_user_logout();
+	rc = funcs->C_CloseAllSessions(slot_id);
+	if (rc != CKR_OK) {
+		testcase_error("C_CloseSessions rc=%s", p11_get_ckr(rc));
+	}
+	return rc;
 
 	return rc;
 }
@@ -541,6 +524,8 @@ int main(int argc, char **argv)
 	}
 
 	rv = ssl3_functions();
+	testcase_print_result();
+
 	/* make sure we return non-zero if rv is non-zero */
 	return ((rv==0) || (rv % 256) ? rv : -1);
 }
