43d950cbb344bb576afa52554f5c4f11ec1ef65f
Joy Latten <latten@linux.vnet.ibm.com>
2013-03-26 11:28:46 -0500

    Refactored the Diffie Helman testcase in dh_func.c
    Signed-off-by: Joy Latten <latten@linux.vnet.ibm.com>

--- testcases/crypto/dh_func.c
+++ testcases/crypto/dh_func.c
@@ -309,271 +309,263 @@
 #include "regress.h"
 
 
-// These values were obtained from IPsec second oakley group. These values are in big-endian
-// format.  These are required for generating DH keys and secrets.
+// These values were obtained from IPsec second oakley group.
+// These values are in big-endian format.
+// These are required for generating DH keys and secrets.
 
-CK_BYTE DH_PUBL_PRIME[128] =
+CK_BYTE DH_PUBL_PRIME[128] =  {	0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xC9,
+				0x0F,0xDA,0xA2,0x21,0x68,0xC2,0x34,0xC4,0xC6,
+				0x62,0x8B,0x80,0xDC,0x1C,0xD1,0x29,0x02,0x4E,
+				0x08,0x8A,0x67,0xCC,0x74,0x02,0x0B,0xBE,0xA6,
+				0x3B,0x13,0x9B,0x22,0x51,0x4A,0x08,0x79,0x8E,
+				0x34,0x04,0xDD,0xEF,0x95,0x19,0xB3,0xCD,0x3A,
+				0x43,0x1B,0x30,0x2B,0x0A,0x6D,0xF2,0x5F,0x14,
+				0x37,0x4F,0xE1,0x35,0x6D,0x6D,0x51,0xC2,0x45,
+				0xE4,0x85,0xB5,0x76,0x62,0x5E,0x7E,0xC6,0xF4,
+				0x4C,0x42,0xE9,0xA6,0x37,0xED,0x6B,0x0B,0xFF,
+				0x5C,0xB6,0xF4,0x06,0xB7,0xED,0xEE,0x38,0x6B,
+				0xFB,0x5A,0x89,0x9F,0xA5,0xAE,0x9F,0x24,0x11,
+				0x7C,0x4B,0x1F,0xE6,0x49,0x28,0x66,0x51,0xEC,
+				0xE6,0x53,0x81,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+				0xFF,0xFF };
+
+CK_BYTE DH_PUBL_BASE[128] =   {	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+				0x00,0x02 };
+
+/*
+ * Generate DH key-pairs for parties A and B.
+ * Derive keys based on Diffie Hellman key agreement defined in PKCS#3.
+ *
+ */
+CK_RV do_DeriveDHKey(void)
 {
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xC9, 0x0F, 0xDA, 0xA2, 0x21, 0x68, 0xC2,
-	0x34, 0xC4, 0xC6, 0x62, 0x8B, 0x80, 0xDC, 0x1C, 0xD1, 0x29, 0x02, 0x4E, 0x08, 0x8A, 0x67,
-	0xCC, 0x74, 0x02, 0x0B, 0xBE, 0xA6, 0x3B, 0x13, 0x9B, 0x22, 0x51, 0x4A, 0x08, 0x79, 0x8E,
-	0x34, 0x04, 0xDD, 0xEF, 0x95, 0x19, 0xB3, 0xCD, 0x3A, 0x43, 0x1B, 0x30, 0x2B, 0x0A, 0x6D,
-	0xF2, 0x5F, 0x14, 0x37, 0x4F, 0xE1, 0x35, 0x6D, 0x6D, 0x51, 0xC2, 0x45, 0xE4, 0x85, 0xB5,
-	0x76, 0x62, 0x5E, 0x7E, 0xC6, 0xF4, 0x4C, 0x42, 0xE9, 0xA6, 0x37, 0xED, 0x6B, 0x0B, 0xFF,
-	0x5C, 0xB6, 0xF4, 0x06, 0xB7, 0xED, 0xEE, 0x38, 0x6B, 0xFB, 0x5A, 0x89, 0x9F, 0xA5, 0xAE,
-	0x9F, 0x24, 0x11, 0x7C, 0x4B, 0x1F, 0xE6, 0x49, 0x28, 0x66, 0x51, 0xEC, 0xE6, 0x53, 0x81,
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-} ;
-
-CK_BYTE DH_PUBL_BASE[128] =
-{
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02
-
-};
-
-//
-//
-CK_RV do_GenerateDHKeyPair( void )
-{
-	CK_SLOT_ID          slot_id;
 	CK_SESSION_HANDLE   session;
 	CK_MECHANISM        mech;
 	CK_OBJECT_HANDLE    publ_key, priv_key;
+	CK_OBJECT_HANDLE    peer_publ_key, peer_priv_key;
+	CK_OBJECT_HANDLE    secret_key, peer_secret_key;
 	CK_FLAGS            flags;
 	CK_BYTE             user_pin[PKCS11_MAX_PIN_LEN];
 	CK_ULONG            user_pin_len;
-	CK_RV               rc, loc_rc;
+	CK_RV               rc = CKR_OK, loc_rc = CKR_OK;
 
 	CK_OBJECT_CLASS     pub_key_class  = CKO_PUBLIC_KEY ;
-	CK_KEY_TYPE         pub_key_type   = CKK_DH ;
+	CK_KEY_TYPE         key_type   = CKK_DH ;
+	CK_UTF8CHAR	    publ_label[] = "A DH public key object";
 	CK_OBJECT_CLASS     priv_key_class = CKO_PRIVATE_KEY ;
-	CK_KEY_TYPE         priv_key_type   = CKK_DH ;
-	CK_BBOOL            true = TRUE ;
+        CK_UTF8CHAR	    priv_label[] = "A DH private key object";
+
+	CK_ULONG secret_key_size = sizeof(DH_PUBL_PRIME);
+	CK_OBJECT_CLASS secret_key_class = CKO_SECRET_KEY;
+	CK_KEY_TYPE secret_key_type = CKK_GENERIC_SECRET;
+	CK_UTF8CHAR secret_label[] = "A generic secret key object";
+
+	CK_BYTE key1_value[sizeof(DH_PUBL_PRIME)*2];
+	CK_BYTE key2_value[sizeof(DH_PUBL_PRIME)*2];
 
 	CK_ATTRIBUTE  publ_tmpl[] =
 	{
-		{CKA_CLASS,    &pub_key_class,   sizeof(pub_key_class)    },
-		{CKA_KEY_TYPE, &pub_key_type,    sizeof(pub_key_type)     },
-		{CKA_MODIFIABLE, &true,          sizeof(true)             },
-		{CKA_PRIME,    DH_PUBL_PRIME,    sizeof(DSA_PUBL_PRIME)   },
-		{CKA_BASE,     DH_PUBL_BASE,     sizeof(DSA_PUBL_BASE)    }
+		{CKA_CLASS, &pub_key_class, sizeof(pub_key_class)},
+		{CKA_KEY_TYPE, &key_type, sizeof(key_type)},
+		{CKA_LABEL, publ_label, sizeof(publ_label)-1},
+		{CKA_PRIME, DH_PUBL_PRIME, sizeof(DH_PUBL_PRIME)},
+		{CKA_BASE, DH_PUBL_BASE, sizeof(DH_PUBL_BASE)}
 	};
 
 	CK_ATTRIBUTE  priv_tmpl[] =
 	{
-		{CKA_CLASS,    &priv_key_class,   sizeof(priv_key_class)    },
-		{CKA_KEY_TYPE, &priv_key_type,    sizeof(priv_key_type)     },
-		{CKA_MODIFIABLE, &true,          sizeof(true)             }
-	};
-
-	slot_id = SLOT_ID;
-
-	if (get_user_pin(user_pin))
-		return CKR_FUNCTION_FAILED;
-	user_pin_len = (CK_ULONG)strlen((char *)user_pin);
-
-	mech.mechanism      = CKM_DH_PKCS_KEY_PAIR_GEN;
-	mech.ulParameterLen = 0;
-	mech.pParameter     = NULL;
-
-	flags = CKF_SERIAL_SESSION;
-	rc = funcs->C_OpenSession( slot_id, flags, NULL, NULL, &session );
-	if (rc != CKR_OK) {
-		show_error("   C_OpenSession #3", rc );
-		return rc;
-	}
-
-	rc = funcs->C_Login( session, CKU_USER, user_pin, user_pin_len );
-	if (rc != CKR_OK) {
-		show_error("   C_Login #1", rc );
-		goto error;
-	}
-
-	printf("do_GenerateDHKeyPair...\n");
-
-	rc = funcs->C_GenerateKeyPair( session,   &mech,
-			publ_tmpl,  5,
-			priv_tmpl,  3,
-			&publ_key, &priv_key );
-	if (rc != CKR_OK) {
-		show_error("   C_GenerateKeyPair #1", rc );
-		goto error;
-	}
-
-	rc = funcs->C_CloseSession( session );
-	if (rc != CKR_OK) {
-		show_error("   C_CloseSession #3", rc );
-		return rc;
-	}
-
-	printf("Looks okay...\n");
-	return 0;
-
-error:
-	loc_rc = funcs->C_CloseSession (session);
-	if (loc_rc != CKR_OK)
-		show_error ("   C_CloseSession", loc_rc);
-
-	return rc;
-}
-
-
-CK_RV do_DeriveDHKey( void )
-{
-	CK_SLOT_ID          slot_id;
-	CK_SESSION_HANDLE   session;
-	CK_MECHANISM        mech;
-	CK_OBJECT_HANDLE    publ_key, priv_key, secret_key;
-	CK_FLAGS            flags;
-	CK_BYTE             user_pin[PKCS11_MAX_PIN_LEN];
-	CK_ULONG            user_pin_len;
-	CK_RV               rc, loc_rc;
-
-	CK_OBJECT_CLASS     pub_key_class  = CKO_PUBLIC_KEY ;
-	CK_KEY_TYPE         pub_key_type   = CKK_DH ;
-	CK_OBJECT_CLASS     priv_key_class = CKO_PRIVATE_KEY ;
-	CK_KEY_TYPE         priv_key_type   = CKK_DH ;
-	CK_BYTE             peer_pub_value[128];
-	CK_ULONG            count ;
-	CK_BBOOL            true = TRUE ;
-	CK_ULONG            cka_value_bits = 180 ;
-
-	CK_OBJECT_CLASS     secret_key_class  = CKO_SECRET_KEY ;
-	CK_KEY_TYPE         secret_key_type   = CKK_GENERIC_SECRET ;
-	CK_ULONG            secret_key_value_len = 128 ;
-
-	CK_ATTRIBUTE  publ_tmpl[] =
-	{
-		{CKA_CLASS,    &pub_key_class,   sizeof(pub_key_class)    },
-		{CKA_KEY_TYPE, &pub_key_type,    sizeof(pub_key_type)     },
-		{CKA_MODIFIABLE, &true,          sizeof(true)             },
-		{CKA_PRIME,    DH_PUBL_PRIME,    sizeof(DSA_PUBL_PRIME)   },
-		{CKA_BASE,     DH_PUBL_BASE,     sizeof(DSA_PUBL_BASE)    }
-	};
-
-	CK_ATTRIBUTE  priv_tmpl[] =
-	{
-		{CKA_CLASS,    &priv_key_class,   sizeof(priv_key_class)    },
-		{CKA_KEY_TYPE, &priv_key_type,    sizeof(priv_key_type)     },
-		{CKA_MODIFIABLE, &true,          sizeof(true)             },
-		{CKA_VALUE_BITS, &cka_value_bits, sizeof(cka_value_bits) }                                                   
+		{CKA_CLASS, &priv_key_class, sizeof(priv_key_class)},
+		{CKA_KEY_TYPE, &key_type, sizeof(key_type)},
+		{CKA_LABEL, priv_label, sizeof(priv_label)-1}
 	};
 
 	CK_ATTRIBUTE  secret_tmpl[] =
 	{
-		{CKA_CLASS,    &secret_key_class,   sizeof(secret_key_class) },
-		{CKA_KEY_TYPE, &secret_key_type,    sizeof(secret_key_type)  },
-		{CKA_VALUE_LEN, &secret_key_value_len, sizeof(secret_key_value_len) }
+		{CKA_CLASS, &secret_key_class, sizeof(secret_key_class)},
+		{CKA_KEY_TYPE, &secret_key_type, sizeof(secret_key_type)},
+		{CKA_VALUE_LEN, &secret_key_size, sizeof(secret_key_size)},
+		{CKA_LABEL, secret_label, sizeof(secret_label)-1}
 	};
 
-	printf("do_DeriveDHKey...\n");
+	CK_ATTRIBUTE  extr1_tmpl[] =
+	{
+		{CKA_VALUE, key1_value, sizeof(key1_value)}
+	};
 
-	slot_id = SLOT_ID;
+	CK_ATTRIBUTE  extr2_tmpl[] =
+	{
+		{CKA_VALUE, key2_value, sizeof(key2_value)}
+	};
 
-	if (get_user_pin(user_pin))
-		return CKR_FUNCTION_FAILED;
-	user_pin_len = (CK_ULONG)strlen((char *)user_pin);
+	testcase_begin("starting do_DeriveDHKey...");
+	testcase_rw_session();
+	testcase_user_login();
 
+	// Testcase #1 - Generate 2 DH key pairs.
 
-	flags = CKF_SERIAL_SESSION | CKF_RW_SESSION;
-	rc = funcs->C_OpenSession( slot_id, flags, NULL, NULL, &session );
-	if (rc != CKR_OK) {
-		show_error("   C_OpenSession #3", rc );
-		return rc;
-	}
-
-	rc = funcs->C_Login( session, CKU_USER, user_pin, user_pin_len );
-	if (rc != CKR_OK) {
-		show_error("   C_Login #1", rc );
-		goto error;
-	}
-
-	// First, generate the DH key Pair
-
-	mech.mechanism      = CKM_DH_PKCS_KEY_PAIR_GEN;
+	// First, generate the DH key Pair for Party A
+	mech.mechanism = CKM_DH_PKCS_KEY_PAIR_GEN;
 	mech.ulParameterLen = 0;
-	mech.pParameter     = NULL;
+	mech.pParameter = NULL;
 
-	rc = funcs->C_GenerateKeyPair( session,   &mech,
-			publ_tmpl,  5,
-			priv_tmpl,  4,
-			&publ_key, &priv_key );
+	rc = funcs->C_GenerateKeyPair(session, &mech, publ_tmpl, 5,
+					priv_tmpl, 3, &publ_key, &priv_key);
 	if (rc != CKR_OK) {
-		show_error("   C_GenerateKeyPair #1", rc );
-		goto error;
+		testcase_fail("C_GenerateKeyPair #1: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
 	}
 
-	// Now, Use the key pair derived above to derive the DH key
+	// Now generate a key-pair for party B (the peer)
+	mech.mechanism = CKM_DH_PKCS_KEY_PAIR_GEN;
+	mech.ulParameterLen = 0;
+	mech.pParameter = NULL;
 
-	// Fill in dummy value for peer's public key
-	for (count=0; count<128; count++)
-		peer_pub_value[count] = 0x22 ;
-
-	mech.mechanism      = CKM_DH_PKCS_DERIVE;
-	mech.pParameter     = peer_pub_value ;
-	mech.ulParameterLen = sizeof(peer_pub_value);
-
-	rc = funcs->C_DeriveKey( session, &mech,
-			priv_key, secret_tmpl,
-			3, &secret_key ) ;
+	rc = funcs->C_GenerateKeyPair(session, &mech, publ_tmpl, 5,
+				priv_tmpl, 3, &peer_publ_key, &peer_priv_key);
 	if (rc != CKR_OK) {
-		PRINT_ERR("DeriveKey Failed\n") ;
-		show_error("   C_DeriveKey #1", rc );
-		goto error;
+		testcase_fail("C_GenerateKeyPair #2: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
 	}
 
-
-	rc = funcs->C_CloseSession( session );
+	// Extract the peer's public key
+	rc = funcs->C_GetAttributeValue(session, peer_publ_key, extr1_tmpl, 1);
 	if (rc != CKR_OK) {
-		show_error("   C_CloseSession #3", rc );
-		return rc;
+		testcase_error("C_GetAttributeValue #1: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
 	}
 
-	printf("Looks okay...\n");
-	return 0;
+	// Make sure peer's key is the right size
+	if ((extr1_tmpl[0].ulValueLen != sizeof(DH_PUBL_PRIME)) &&
+	    (extr1_tmpl[0].ulValueLen != sizeof(DH_PUBL_PRIME)-1)) {
+		testcase_fail("ERROR:size error peer's key %ld\n",extr1_tmpl[0].ulValueLen );
+		goto testcase_cleanup;
+	}
 
-error:
-	loc_rc = funcs->C_CloseSession (session);
+	// Testcase #2 - Now derive the secrets...
+
+	/* Now, derive a generic secret key using party A's private key
+	 * and peer's public key
+	 */
+	mech.mechanism  = CKM_DH_PKCS_DERIVE;
+	mech.ulParameterLen = extr1_tmpl[0].ulValueLen ;
+	mech.pParameter = key1_value;
+
+	rc = funcs->C_DeriveKey(session, &mech, priv_key, secret_tmpl,
+			4, &secret_key) ;
+	if (rc != CKR_OK) {
+		testcase_fail("C_DeriveKey #1: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
+	}
+
+	// Do the same for the peer
+
+	// Extract party A's public key
+	rc = funcs->C_GetAttributeValue(session, publ_key, extr2_tmpl, 1);
+	if (rc != CKR_OK) {
+		testcase_error("C_GetAttributeValue #2: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
+	}
+
+	// Make sure party A's key is the right size
+	if ((extr2_tmpl[0].ulValueLen != sizeof(DH_PUBL_PRIME)) &&
+	    (extr2_tmpl[0].ulValueLen != sizeof(DH_PUBL_PRIME)-1)) {
+		testcase_fail("ERROR:size error party A's key %ld\n",extr2_tmpl[0].ulValueLen );
+		goto testcase_cleanup;
+	}
+
+	// Now, derive a generic secret key using peer's private key
+	// and A's public key
+	mech.mechanism = CKM_DH_PKCS_DERIVE;
+	mech.ulParameterLen = extr2_tmpl[0].ulValueLen;
+	mech.pParameter = key2_value;
+
+	rc = funcs->C_DeriveKey(session, &mech, peer_priv_key,
+                            secret_tmpl, 4, &peer_secret_key);
+	if (rc != CKR_OK) {
+		testcase_fail("C_DeriveKey #2: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
+	}
+
+	// Extract the derived keys and compare them
+
+	memset(key1_value,0,sizeof(key1_value));
+	extr1_tmpl[0].ulValueLen= sizeof(key1_value);
+
+	rc = funcs->C_GetAttributeValue(session, secret_key, extr1_tmpl, 1);
+	if (rc != CKR_OK) {
+		testcase_error("C_GetAttributeValue #3: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
+	}
+
+	if (extr1_tmpl[0].ulValueLen != sizeof(DH_PUBL_PRIME) || *((int*)extr1_tmpl[0].pValue) == 0) {
+		testcase_fail("ERROR:derived key #1 length or value %ld\n", extr1_tmpl[0].ulValueLen );
+		goto testcase_cleanup;
+	}
+
+	memset(key2_value,0,sizeof(key2_value));
+	extr2_tmpl[0].ulValueLen= sizeof(key2_value);
+
+	rc = funcs->C_GetAttributeValue(session, peer_secret_key, extr2_tmpl,1);
+	if (rc != CKR_OK) {
+		testcase_error("C_GetAttributeValue #4: rc = %s", p11_get_ckr(rc));
+		goto testcase_cleanup;
+	}
+
+	if (extr2_tmpl[0].ulValueLen != sizeof(DH_PUBL_PRIME) || *((int*)extr2_tmpl[0].pValue) == 0) {
+		testcase_fail("ERROR:derived key #2 length or value %ld\n", extr2_tmpl[0].ulValueLen );
+		goto testcase_cleanup;
+	}
+
+	if (memcmp(key1_value, key2_value, sizeof(DH_PUBL_PRIME)) != 0) {
+		testcase_fail("ERROR:derived key mismatch\n");
+		goto testcase_cleanup;
+	}
+
+	testcase_pass("Generating DH key pairs and deriving secrets");
+
+testcase_cleanup:
+	funcs->C_DestroyObject(session, publ_key);
+	funcs->C_DestroyObject(session, priv_key);
+	funcs->C_DestroyObject(session, peer_priv_key);
+	funcs->C_DestroyObject(session, peer_publ_key);
+	funcs->C_DestroyObject(session, secret_key);
+	funcs->C_DestroyObject(session, peer_secret_key);
+
+	loc_rc = funcs->C_CloseSession(session);
 	if (loc_rc != CKR_OK)
-		show_error ("   C_CloseSession", loc_rc);
-
+		testcase_error("C_CloseSession, loc_rc = %s", p11_get_ckr(loc_rc));
 	return rc;
 } /* end do_DeriveDHKey() */
 
 CK_RV dh_functions()
 {
-	SYSTEMTIME t1, t2;
-	CK_RV        rc;
+	CK_RV  rv, rv2;
+	CK_MECHANISM_INFO mechinfo;
 
-	GetSystemTime(&t1);
-	rc = do_GenerateDHKeyPair();
-	if (rc) {
-		PRINT_ERR("ERROR do_GenerateDHKeyPair failed, rc = 0x%lx\n", rc);
-		if (!no_stop)
-			return rc;
-	}
-	GetSystemTime(&t2);
-	process_time( t1, t2 );
+	/** get mech info **/
+	rv = funcs->C_GetMechanismInfo(SLOT_ID, CKM_DH_PKCS_KEY_PAIR_GEN,
+					&mechinfo);
+	rv2 = funcs->C_GetMechanismInfo(SLOT_ID, CKM_DH_PKCS_DERIVE, &mechinfo);
 
-	GetSystemTime(&t1);
-	rc = do_DeriveDHKey();
-	if (rc) {
-		PRINT_ERR("ERROR do_DeriveDHKey failed, rc = 0x%lx\n", rc);
-		if (!no_stop)
-			return rc;
-	}
-	GetSystemTime(&t2);
-	process_time( t1, t2 );
+	if ((rv == CKR_OK) && (rv2 == CKR_OK))
+		rv = do_DeriveDHKey();
+	else
+		rv = CKR_MECHANISM_INVALID;
 
-	return 0;
+	return rv;
 }
 
 int main(int argc, char **argv)
