869ba187ba62d1a91e576f15ff9dc84d4163d384
Paulo Vital <pvital@linux.vnet.ibm.com>
2017-03-09 12:48:18 -0300

    Add CPL-1.0 license header into Shell Script files.
    Signed-off-by: Paulo Vital <pvital@linux.vnet.ibm.com>

--- misc/migrate_from_2.1_to_2.2.sh
+++ misc/migrate_from_2.1_to_2.2.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# COPYRIGHT (c) International Business Machines Corp. 2006-2017
+#
+# This program is provided under the terms of the Common Public License,
+# version 1.0 (CPL-1.0). Any use, reproduction or distribution for this software
+# constitutes recipient's acceptance of CPL-1.0 terms which can be found
+# in the file LICENSE file or at https://opensource.org/licenses/cpl1.0.php
+#
 
 # This script should be run after installing openCryptoki version 2.2.x on a
 # machine where openCryptoki version 2.1.x has already been installed.
