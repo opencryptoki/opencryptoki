61fff14d42189acc05f72cdc6e6b283372f73a79
Ingo Tuchscherer <ingo.tuchscherer@linux.vnet.ibm.com>
2017-08-18 11:04:15 +0200

    MTI [PATCH 22/85]: Adapt wrap crypto functions
    Adapt wrap related cryptographic functions with respect to the newly
    introduced token data storage.
    
    Signed-off-by: Ingo Tuchscherer <ingo.tuchscherer@linux.vnet.ibm.com>

--- usr/lib/pkcs11/ep11_stdll/ep11_specific.c
+++ usr/lib/pkcs11/ep11_stdll/ep11_specific.c
@@ -3159,9 +3159,10 @@ CK_RV ep11tok_decrypt_init(SESSION *session, CK_MECHANISM_PTR mech,
 }
 
 
-CK_RV ep11tok_wrap_key(SESSION *session, CK_MECHANISM_PTR mech,
-		       CK_OBJECT_HANDLE wrapping_key, CK_OBJECT_HANDLE key,
-		       CK_BYTE_PTR wrapped_key, CK_ULONG_PTR p_wrapped_key_len)
+CK_RV ep11tok_wrap_key(STDLL_TokData_t *tokdata, SESSION *session,
+		       CK_MECHANISM_PTR mech, CK_OBJECT_HANDLE wrapping_key,
+		       CK_OBJECT_HANDLE key, CK_BYTE_PTR wrapped_key,
+		       CK_ULONG_PTR p_wrapped_key_len)
 {
 	CK_RV rc;
 	CK_BYTE *wrapping_blob;
