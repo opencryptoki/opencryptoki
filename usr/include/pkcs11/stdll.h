65b27069f4e3e48d5fe2bba5584692e537f5fe27
Ingo Tuchscherer <ingo.tuchscherer@linux.vnet.ibm.com>
2017-09-15 23:15:42 +0200

    MTI [PATCH 61/85]: Adapt object destroy
    Adapt the destroy object helper function with respect to
    the newly introduced token data storage.
    
    Signed-off-by: Ingo Tuchscherer <ingo.tuchscherer@linux.vnet.ibm.com>

--- usr/include/pkcs11/stdll.h
+++ usr/include/pkcs11/stdll.h
@@ -127,7 +127,7 @@ typedef CK_RV	(CK_PTR  ST_C_CopyObject)
 						 CK_OBJECT_HANDLE hObject, CK_ATTRIBUTE_PTR pTemplate,
 						 CK_ULONG usCount, CK_OBJECT_HANDLE_PTR phNewObject);
 typedef CK_RV (CK_PTR  ST_C_DestroyObject)
-                       (ST_SESSION_T *hSession, CK_OBJECT_HANDLE hObject);
+                       (STDLL_TokData_t *tokdata, ST_SESSION_T *hSession, CK_OBJECT_HANDLE hObject);
 typedef CK_RV(CK_PTR  ST_C_GetObjectSize)
                        (STDLL_TokData_t *tokdata, ST_SESSION_T *hSession, CK_OBJECT_HANDLE hObject,
                         CK_ULONG_PTR pusSize);
--- usr/lib/pkcs11/ep11_stdll/new_host.c
+++ usr/lib/pkcs11/ep11_stdll/new_host.c
@@ -1124,12 +1124,13 @@ done:
 }
 
 
-CK_RV SC_DestroyObject(ST_SESSION_HANDLE *sSession, CK_OBJECT_HANDLE hObject)
+CK_RV SC_DestroyObject(STDLL_TokData_t *tokdata, ST_SESSION_HANDLE *sSession,
+		       CK_OBJECT_HANDLE hObject)
 {
 	SESSION *sess = NULL;
 	CK_RV rc = CKR_OK;
 
-	if (initialized == FALSE) {
+	if (tokdata->initialized == FALSE) {
 		TRACE_ERROR("%s\n", ock_err(ERR_CRYPTOKI_NOT_INITIALIZED));
 		rc = CKR_CRYPTOKI_NOT_INITIALIZED;
 		goto done;
@@ -1148,7 +1149,7 @@ CK_RV SC_DestroyObject(ST_SESSION_HANDLE *sSession, CK_OBJECT_HANDLE hObject)
 		goto done;
 	}
 
-	rc = object_mgr_destroy_object(sess, hObject);
+	rc = object_mgr_destroy_object(tokdata, sess, hObject);
 	if (rc != CKR_OK)
 		TRACE_DEVEL("*_destroy_object() failed\n");
 done:
