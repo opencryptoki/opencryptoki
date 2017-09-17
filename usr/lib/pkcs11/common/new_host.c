45bc2e6593b4265562f84f0bb3cd770144bf43ec
Ingo Tuchscherer <ingo.tuchscherer@linux.vnet.ibm.com>
2017-09-17 06:02:59 +0200

    MTI [PATCH 65/85]: Load/save master key
    Adapt the load/save masterkey helper functions with respect to
    the newly introduced token data storage.
    
    Signed-off-by: Ingo Tuchscherer <ingo.tuchscherer@linux.vnet.ibm.com>

--- usr/lib/pkcs11/common/new_host.c
+++ usr/lib/pkcs11/common/new_host.c
@@ -555,7 +555,7 @@ CK_RV SC_InitPIN(STDLL_TokData_t *tokdata, ST_SESSION_HANDLE *sSession,
 		TRACE_DEVEL("Failed to save token data.\n");
 		goto done;
 	}
-	rc = save_masterkey_user();
+	rc = save_masterkey_user(tokdata);
 	if (rc != CKR_OK)
 		TRACE_DEVEL("Failed to save user's masterkey.\n");
 
@@ -656,7 +656,7 @@ CK_RV SC_SetPIN(STDLL_TokData_t *tokdata, ST_SESSION_HANDLE *sSession,
 			TRACE_DEVEL("Failed to save token data.\n");
 			goto done;
 		}
-		rc = save_masterkey_user();
+		rc = save_masterkey_user(tokdata);
 	} else if (sess->session_info.state == CKS_RW_SO_FUNCTIONS) {
 		if (memcmp(nv_token_data->so_pin_sha, old_hash_sha,
 			   SHA1_HASH_SIZE) != 0) {
@@ -694,7 +694,7 @@ CK_RV SC_SetPIN(STDLL_TokData_t *tokdata, ST_SESSION_HANDLE *sSession,
 			TRACE_DEVEL("Failed to save token data.\n");
 			goto done;
 		}
-		rc = save_masterkey_so();
+		rc = save_masterkey_so(tokdata);
 		if (rc != CKR_OK)
 			TRACE_DEVEL("Failed to save SO's masterkey.\n");
 	} else {
@@ -1015,7 +1015,7 @@ CK_RV SC_Login(STDLL_TokData_t *tokdata, ST_SESSION_HANDLE *sSession,
 			     tokdata->nv_token_data->user_pin_md5 );
 		memset(tokdata->nv_token_data->so_pin_md5, 0x0, MD5_HASH_SIZE);
 
-		rc = load_masterkey_user();
+		rc = load_masterkey_user(tokdata);
 		if (rc != CKR_OK){
 			TRACE_DEVEL("Failed to load user's masterkey.\n");
 			goto done;
@@ -1068,7 +1068,7 @@ CK_RV SC_Login(STDLL_TokData_t *tokdata, ST_SESSION_HANDLE *sSession,
 			    tokdata->nv_token_data->so_pin_md5);
 		memset(tokdata->nv_token_data->user_pin_md5, 0x0, MD5_HASH_SIZE);
 
-		rc = load_masterkey_so();
+		rc = load_masterkey_so(tokdata);
 		if (rc != CKR_OK)
 			TRACE_DEVEL("Failed to load SO's masterkey.\n");
 	}
