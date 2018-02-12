a333ae426976c9f3dd4be5465d452c8a0825c49a
Eduardo Barretto <ebarretto@linux.vnet.ibm.com>
2018-02-12 15:21:23 -0200

    pkcsep11_migrate: Fix coding style
    Signed-off-by: Eduardo Barretto <ebarretto@linux.vnet.ibm.com>

--- usr/sbin/pkcsep11_migrate/ep11adm.h
+++ usr/sbin/pkcsep11_migrate/ep11adm.h
@@ -31,52 +31,52 @@
 #endif
 
 #if !defined(EP11_ADMCTR_BYTES)
-#define  EP11_ADMCTR_BYTES          (128/8)    /* admin transaction ctrs */
+#define  EP11_ADMCTR_BYTES          (128/8) /* admin transaction ctrs */
 #endif
 
 #if !defined(EP11_ADM_REENCRYPT)
-#define  EP11_ADM_REENCRYPT         25    /* transform blobs to next WK */
+#define  EP11_ADM_REENCRYPT         25      /* transform blobs to next WK */
 #endif
 
 #if !defined(CK_IBM_EP11Q_DOMAIN)
-#define  CK_IBM_EP11Q_DOMAIN        3     /* list domain's WK hashes */
+#define  CK_IBM_EP11Q_DOMAIN        3       /* list domain's WK hashes */
 #endif
 
 #if !defined(CK_IBM_DOM_COMMITTED_NWK)
-#define  CK_IBM_DOM_COMMITTED_NWK   8     /* next WK is active(committed) */
+#define  CK_IBM_DOM_COMMITTED_NWK   8       /* next WK is active(committed) */
 #endif
 
 
 typedef struct ep11_admresp {
-	uint32_t fn;
-	uint32_t domain;
-	uint32_t domainInst;
+    uint32_t fn;
+    uint32_t domain;
+    uint32_t domainInst;
 
-	/* module ID || module instance */
-	unsigned char  module[ EP11_SERIALNR_CHARS + EP11_SERIALNR_CHARS ];
-	unsigned char   modNr[ EP11_SERIALNR_CHARS ];
-	unsigned char modInst[ EP11_SERIALNR_CHARS ];
+    /* module ID || module instance */
+    unsigned char module[EP11_SERIALNR_CHARS + EP11_SERIALNR_CHARS];
+    unsigned char modNr[EP11_SERIALNR_CHARS];
+    unsigned char modInst[EP11_SERIALNR_CHARS];
 
-	unsigned char    tctr[ EP11_ADMCTR_BYTES ];    /* transaction counter */
+    unsigned char tctr[EP11_ADMCTR_BYTES];      /* transaction counter */
 
-	CK_RV rv;
-	uint32_t reason;
+    CK_RV rv;
+    uint32_t reason;
 
-	// points to original response; NULL if no payload
-	// make sure it's copied if used after releasing response block
-	//
-	const unsigned char *payload;
-	size_t pllen;
+    // points to original response; NULL if no payload
+    // make sure it's copied if used after releasing response block
+    //
+    const unsigned char *payload;
+    size_t pllen;
 } *ep11_admresp_t;
 
 
 #if !defined(__XCP_H__)
 typedef struct CK_IBM_DOMAIN_INFO {
-	CK_ULONG    domain;
-	CK_BYTE     wk[ EP11_KEYCSUM_BYTES ];
- 	CK_BYTE nextwk[ EP11_KEYCSUM_BYTES ];
-	CK_ULONG  flags;
-	CK_BYTE   mode[ 8 ];
+    CK_ULONG domain;
+    CK_BYTE wk[EP11_KEYCSUM_BYTES];
+    CK_BYTE nextwk[EP11_KEYCSUM_BYTES];
+    CK_ULONG flags;
+    CK_BYTE mode[8];
 } CK_IBM_DOMAIN_INFO;
 #endif
 
@@ -89,11 +89,12 @@ typedef struct CK_IBM_DOMAIN_INFO {
  *   *minf used for module ID and transaction counter
  *  ignored for commands where those fields are ignored
  */
-long ep11a_cmdblock(unsigned char *blk, size_t blen,
+long ep11a_cmdblock(unsigned char *blk,
+                    size_t blen,
                     unsigned int fn,
-              const struct ep11_admresp *minf,
-              const unsigned char *tctr,    /* EP11_ADMCTR_BYTES */
-              const unsigned char *payload, size_t plen);
+                    const struct ep11_admresp *minf,
+                    const unsigned char *tctr,       /* EP11_ADMCTR_BYTES */
+                    const unsigned char *payload, size_t plen);
 
 
 /*----------------------------------------------------------------------
@@ -104,7 +105,7 @@ long ep11a_cmdblock(unsigned char *blk, size_t blen,
  *  failed)
  */
 long ep11a_internal_rv(const unsigned char *rsp, size_t rlen,
-		       struct ep11_admresp *rspblk, CK_RV *rv);
+                       struct ep11_admresp *rspblk, CK_RV *rv);
 
 
 /*----------------------------------------------------------------------
@@ -117,8 +118,7 @@ long ep11a_internal_rv(const unsigned char *rsp, size_t rlen,
  */
 CK_RV m_get_ep11_info(CK_VOID_PTR pinfo, CK_ULONG_PTR infbytes,
                       unsigned int query,
-                      unsigned int subquery,
-                      uint64_t target);
+                      unsigned int subquery, uint64_t target);
 
 
-#endif /* !defined(__EP11ADM_H__) */
+#endif                          /* !defined(__EP11ADM_H__) */
