30372a9a55d65d4cc65fe8c2c1546d217e8944d4
Eduardo Barretto <ebarretto@linux.vnet.ibm.com>
2018-02-20 16:45:14 -0300

    TPM: Fix coding style
    Signed-off-by: Eduardo Barretto <ebarretto@linux.vnet.ibm.com>

--- usr/lib/pkcs11/tpm_stdll/tok_struct.h
+++ usr/lib/pkcs11/tpm_stdll/tok_struct.h
@@ -15,106 +15,106 @@
 #ifndef CONFIG_PATH
 #warning CONFIG_PATH not set, using default (/usr/local/var/lib/opencryptoki)
 #define CONFIG_PATH "/usr/local/var/lib/opencryptoki"
-#endif // #ifndef CONFIG_PATH
+#endif                          // #ifndef CONFIG_PATH
 
 #define TPM_CONFIG_PATH CONFIG_PATH "/tpm"
-#endif // #ifndef TPM_CONFIG_PATH
+#endif                          // #ifndef TPM_CONFIG_PATH
 
 struct token_specific_struct token_specific = {
-	TPM_CONFIG_PATH,
-	"tpm",
-	0,
-	// Token data info:
-	{
-		TRUE,			// Use per guest data store
-		TRUE,			// Use master key
-		CKM_AES_CBC,		// Data store encryption
-		NULL,			// Default initialization vector for pins
-		")#%&!*)^!()$&!&N",	// Default initialization vector for objects
-	},
-	token_specific_creatlock,
-	NULL,			// attach_shm
-	&token_specific_init,
-	&token_specific_init_token_data,
-	NULL,			// load_token_data
-	NULL,			// save_token_data
-	&token_specific_rng,
-	&token_specific_final,
-	&token_specific_init_token,
-	&token_specific_login,
-	&token_specific_logout,
-	&token_specific_init_pin,
-	&token_specific_set_pin,
-	// DES
-	&token_specific_des_key_gen,
-	&token_specific_des_ecb,
-	&token_specific_des_cbc,
-	// Triple DES
-	&token_specific_tdes_ecb,
-	&token_specific_tdes_cbc,
-	NULL,			// tdes_ofb
-	NULL,			// tdes_cfb
-	NULL,			// tdes_mac
-	// RSA
-	&token_specific_rsa_decrypt,
-	&token_specific_rsa_encrypt,
-	&token_specific_rsa_sign,
-	&token_specific_rsa_verify,
-	&token_specific_rsa_verify_recover,
-	NULL,			// rsa_x509_decrypt
-	NULL,			// rsa_x509_encrypt
-	NULL,			// rsa_x509_sign
-	NULL,			// rsa_x509_verify
-	NULL,			// rsa_x509_verify_recover
-        NULL,                   // rsa_oaep_decrypt
-        NULL,                   // rsa_oaep_encrypt
-        NULL,                   // rsa_pss_sign
-        NULL,                   // rsa_pss_verify
-	&token_specific_rsa_generate_keypair,
-	// Elliptic Curve
-	NULL,			// ec_sign
-	NULL,			// ec_verify
-	NULL,			// ec_generate_keypair
-    NULL,            // ecdh_derive
+    TPM_CONFIG_PATH,
+    "tpm",
+    0,
+    // Token data info:
+    {
+        TRUE,                      // Use per guest data store
+        TRUE,                      // Use master key
+        CKM_AES_CBC,               // Data store encryption
+        NULL,                      // Default initialization vector for pins
+        ")#%&!*)^!()$&!&N",        // Default initialization vector for objects
+    },
+    token_specific_creatlock,
+    NULL,                       // attach_shm
+    &token_specific_init,
+    &token_specific_init_token_data,
+    NULL,                       // load_token_data
+    NULL,                       // save_token_data
+    &token_specific_rng,
+    &token_specific_final,
+    &token_specific_init_token,
+    &token_specific_login,
+    &token_specific_logout,
+    &token_specific_init_pin,
+    &token_specific_set_pin,
+    // DES
+    &token_specific_des_key_gen,
+    &token_specific_des_ecb,
+    &token_specific_des_cbc,
+    // Triple DES
+    &token_specific_tdes_ecb,
+    &token_specific_tdes_cbc,
+    NULL,                       // tdes_ofb
+    NULL,                       // tdes_cfb
+    NULL,                       // tdes_mac
+    // RSA
+    &token_specific_rsa_decrypt,
+    &token_specific_rsa_encrypt,
+    &token_specific_rsa_sign,
+    &token_specific_rsa_verify,
+    &token_specific_rsa_verify_recover,
+    NULL,                       // rsa_x509_decrypt
+    NULL,                       // rsa_x509_encrypt
+    NULL,                       // rsa_x509_sign
+    NULL,                       // rsa_x509_verify
+    NULL,                       // rsa_x509_verify_recover
+    NULL,                       // rsa_oaep_decrypt
+    NULL,                       // rsa_oaep_encrypt
+    NULL,                       // rsa_pss_sign
+    NULL,                       // rsa_pss_verify
+    &token_specific_rsa_generate_keypair,
+    // Elliptic Curve
+    NULL,                       // ec_sign
+    NULL,                       // ec_verify
+    NULL,                       // ec_generate_keypair
+    NULL,                       // ecdh_derive
 #ifndef NODH
-	&token_specific_dh_pkcs_derive,
-	&token_specific_dh_pkcs_key_pair_gen,
+    &token_specific_dh_pkcs_derive,
+    &token_specific_dh_pkcs_key_pair_gen,
 #else
-	NULL,
-	NULL,
+    NULL,
+    NULL,
 #endif
-	// SHA
-	NULL,			// sha_init
-	NULL,			// sha
-	NULL,			// sha_update
-	NULL,			// sha_final
-	// HMAC
-	NULL,			// hmac_sign_init
-	NULL,			// hmac_sign
-	NULL,			// hmac_sign_update
-	NULL,			// hmac_sign_final
-	NULL,			// hmac_verify_init
-	NULL,			// hmac_verify
-	NULL,			// hmac_verify_update
-	NULL,			// hmac_verify_final
-        NULL,			// generic_secret_key_gen
-	// AES
-	&token_specific_aes_key_gen,
-	&token_specific_aes_ecb,
-	&token_specific_aes_cbc,
-	NULL,			// aes_ctr
-	NULL,			// aes_gcm_init
-	NULL,			// aes_gcm
-	NULL,			// aes_gcm_update
-	NULL,			// aes_gcm_final
-	NULL,			// aes_ofb
-	NULL,			// aes_cfb
-	NULL,			// aes_mac
-	// DSA
-	NULL,			// dsa_generate_keypair
-	NULL,			// dsa_sign
-	NULL,			// dsa_verify
-	&token_specific_get_mechanism_list,
-	&token_specific_get_mechanism_info,
-	NULL			// object_add
+    // SHA
+    NULL,                       // sha_init
+    NULL,                       // sha
+    NULL,                       // sha_update
+    NULL,                       // sha_final
+    // HMAC
+    NULL,                       // hmac_sign_init
+    NULL,                       // hmac_sign
+    NULL,                       // hmac_sign_update
+    NULL,                       // hmac_sign_final
+    NULL,                       // hmac_verify_init
+    NULL,                       // hmac_verify
+    NULL,                       // hmac_verify_update
+    NULL,                       // hmac_verify_final
+    NULL,                       // generic_secret_key_gen
+    // AES
+    &token_specific_aes_key_gen,
+    &token_specific_aes_ecb,
+    &token_specific_aes_cbc,
+    NULL,                       // aes_ctr
+    NULL,                       // aes_gcm_init
+    NULL,                       // aes_gcm
+    NULL,                       // aes_gcm_update
+    NULL,                       // aes_gcm_final
+    NULL,                       // aes_ofb
+    NULL,                       // aes_cfb
+    NULL,                       // aes_mac
+    // DSA
+    NULL,                       // dsa_generate_keypair
+    NULL,                       // dsa_sign
+    NULL,                       // dsa_verify
+    &token_specific_get_mechanism_list,
+    &token_specific_get_mechanism_info,
+    NULL                        // object_add
 };
--- usr/lib/pkcs11/tpm_stdll/tpm_specific.h
+++ usr/lib/pkcs11/tpm_stdll/tpm_specific.h
@@ -14,38 +14,38 @@
 #include <openssl/rsa.h>
 
 /* TSS key type helper */
-#define TPMTOK_TSS_KEY_TYPE_MASK	0x000000F0
-#define TPMTOK_TSS_KEY_TYPE(x)		(x & TPMTOK_TSS_KEY_TYPE_MASK)
-#define TPMTOK_TSS_KEY_MIG_TYPE(x)	(x & TSS_KEY_MIGRATABLE)
+#define TPMTOK_TSS_KEY_TYPE_MASK    0x000000F0
+#define TPMTOK_TSS_KEY_TYPE(x)      (x & TPMTOK_TSS_KEY_TYPE_MASK)
+#define TPMTOK_TSS_KEY_MIG_TYPE(x)  (x & TSS_KEY_MIGRATABLE)
 
-#define TPMTOK_TSS_MAX_ERROR		0x00000FFF
-#define TPMTOK_TSS_ERROR_CODE(x)	(x & TPMTOK_TSS_MAX_ERROR)
+#define TPMTOK_TSS_MAX_ERROR        0x00000FFF
+#define TPMTOK_TSS_ERROR_CODE(x)    (x & TPMTOK_TSS_MAX_ERROR)
 
 /* key types in the TPM token */
-#define TPMTOK_PRIVATE_ROOT_KEY	1
-#define TPMTOK_PRIVATE_LEAF_KEY	2
-#define TPMTOK_PUBLIC_ROOT_KEY	3
-#define TPMTOK_PUBLIC_LEAF_KEY	4
+#define TPMTOK_PRIVATE_ROOT_KEY     1
+#define TPMTOK_PRIVATE_LEAF_KEY     2
+#define TPMTOK_PUBLIC_ROOT_KEY      3
+#define TPMTOK_PUBLIC_LEAF_KEY      4
 
 /* key identifiers for the PKCS#11 objects */
-#define TPMTOK_PRIVATE_ROOT_KEY_ID	"PRIVATE ROOT KEY"
-#define TPMTOK_PRIVATE_LEAF_KEY_ID	"PRIVATE LEAF KEY"
-#define TPMTOK_PUBLIC_ROOT_KEY_ID	"PUBLIC ROOT KEY"
-#define TPMTOK_PUBLIC_LEAF_KEY_ID	"PUBLIC LEAF KEY"
+#define TPMTOK_PRIVATE_ROOT_KEY_ID  "PRIVATE ROOT KEY"
+#define TPMTOK_PRIVATE_LEAF_KEY_ID  "PRIVATE LEAF KEY"
+#define TPMTOK_PUBLIC_ROOT_KEY_ID   "PUBLIC ROOT KEY"
+#define TPMTOK_PUBLIC_LEAF_KEY_ID   "PUBLIC LEAF KEY"
 
-#define TPMTOK_PRIVATE_ROOT_KEY_ID_SIZE	strlen(TPMTOK_PRIVATE_ROOT_KEY_ID)
-#define TPMTOK_PRIVATE_LEAF_KEY_ID_SIZE	strlen(TPMTOK_PRIVATE_LEAF_KEY_ID)
-#define TPMTOK_PUBLIC_ROOT_KEY_ID_SIZE	strlen(TPMTOK_PUBLIC_ROOT_KEY_ID)
-#define TPMTOK_PUBLIC_LEAF_KEY_ID_SIZE	strlen(TPMTOK_PUBLIC_LEAF_KEY_ID)
+#define TPMTOK_PRIVATE_ROOT_KEY_ID_SIZE strlen(TPMTOK_PRIVATE_ROOT_KEY_ID)
+#define TPMTOK_PRIVATE_LEAF_KEY_ID_SIZE strlen(TPMTOK_PRIVATE_LEAF_KEY_ID)
+#define TPMTOK_PUBLIC_ROOT_KEY_ID_SIZE  strlen(TPMTOK_PUBLIC_ROOT_KEY_ID)
+#define TPMTOK_PUBLIC_LEAF_KEY_ID_SIZE  strlen(TPMTOK_PUBLIC_LEAF_KEY_ID)
 
-#define TPMTOK_PUB_ROOT_KEY_FILE	"PUBLIC_ROOT_KEY.pem"
-#define TPMTOK_PRIV_ROOT_KEY_FILE	"PRIVATE_ROOT_KEY.pem"
+#define TPMTOK_PUB_ROOT_KEY_FILE    "PUBLIC_ROOT_KEY.pem"
+#define TPMTOK_PRIV_ROOT_KEY_FILE   "PRIVATE_ROOT_KEY.pem"
 
 /* TPM token specific return codes */
-#define CKR_KEY_NOT_FOUND	CKR_VENDOR_DEFINED + 0x0f000000
-#define CKR_FILE_NOT_FOUND	CKR_VENDOR_DEFINED + 0x0f000001
+#define CKR_KEY_NOT_FOUND   CKR_VENDOR_DEFINED + 0x0f000000
+#define CKR_FILE_NOT_FOUND  CKR_VENDOR_DEFINED + 0x0f000001
 
-#define TPMTOK_MASTERKEY_PRIVATE	"MK_PRIVATE"
+#define TPMTOK_MASTERKEY_PRIVATE "MK_PRIVATE"
 
 #ifdef DEBUG
 #define DEBUG_openssl_print_errors()    openssl_print_errors()
@@ -59,31 +59,31 @@
 RSA *openssl_gen_key();
 int openssl_write_key(RSA *, char *, CK_BYTE *);
 CK_RV openssl_read_key(char *, CK_BYTE *, RSA **);
-int openssl_get_modulus_and_prime(RSA *, unsigned int *, unsigned char *, unsigned int *, unsigned char *);
+int openssl_get_modulus_and_prime(RSA *, unsigned int *, unsigned char *,
+                                  unsigned int *, unsigned char *);
 int util_set_file_mode(char *, mode_t);
 CK_BYTE *util_create_id(int);
 CK_RV util_set_username(char **);
 unsigned int util_get_keysize_flag(CK_ULONG);
 CK_ULONG util_check_public_exponent(TEMPLATE *);
 
-#define NULL_HKEY	0
-#define NULL_HENCDATA	0
-#define NULL_HPOLICY	0
-#define NULL_HCONTEXT	0
-#define NULL_HPCRS	0
+#define NULL_HKEY       0
+#define NULL_HENCDATA   0
+#define NULL_HPOLICY    0
+#define NULL_HCONTEXT   0
+#define NULL_HPCRS      0
 
-/* CKA_ENC_AUTHDATA will be used to store the encrypted SHA-1 hashes of auth data
- * passed in for TPM keys. The authdata will be encrypted using either the public
- * leaf key or the private leaf key */
+/* CKA_ENC_AUTHDATA will be used to store the encrypted SHA-1 hashes of auth
+ * data passed in for TPM keys. The authdata will be encrypted using either the
+ * public leaf key or the private leaf key */
 #define CKA_ENC_AUTHDATA        CKA_VENDOR_DEFINED + 0x01000001
 
 #define MK_SIZE (AES_KEY_SIZE_256)
 
-struct srk_info
-{
-	char *secret;
-	int mode;
-	int len;
+struct srk_info {
+    char *secret;
+    int mode;
+    int len;
 };
 
 int get_srk_info(struct srk_info *srk);
