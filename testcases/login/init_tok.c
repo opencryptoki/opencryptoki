b748c91966a7e769fdaf6e39de34ca8264076bf0
Eduardo Barretto <ebarretto@linux.vnet.ibm.com>
2018-04-30 11:43:44 -0300

    Testcases/login: Fix coding style
    Signed-off-by: Eduardo Barretto <ebarretto@linux.vnet.ibm.com>

--- testcases/login/init_tok.c
+++ testcases/login/init_tok.c
@@ -24,98 +24,95 @@
 #include "regress.h"
 #include "common.c"
 
-CK_FUNCTION_LIST  *funcs;
-int  do_GetInfo(void);
+CK_FUNCTION_LIST *funcs;
+int do_GetInfo(void);
 
-CK_RV C_GetFunctionList( CK_FUNCTION_LIST ** ) ;
+CK_RV C_GetFunctionList(CK_FUNCTION_LIST **);
 int do_GetFunctionList(void);
 
-CK_SLOT_ID  SLOT_ID;
+CK_SLOT_ID SLOT_ID;
 
-int do_inittoken( CK_FUNCTION_LIST *funcs, CK_BYTE *sopass )
+int do_inittoken(CK_FUNCTION_LIST * funcs, CK_BYTE * sopass)
 {
-   CK_BYTE           label[32];
-   int               len;
-   CK_RV             rc;
+    CK_BYTE label[32];
+    int len;
+    CK_RV rc;
 
-   memcpy( label,   "L13                                   ", 32 );
+    memcpy(label, "L13                                   ", 32);
 
-   for (len = 0; len <31;len++){
-      if (label[len] == '\0'){
-         label[len] = ' ';
-         break;
-      }
-   }
+    for (len = 0; len < 31; len++) {
+        if (label[len] == '\0') {
+            label[len] = ' ';
+            break;
+        }
+    }
 
-   rc = funcs->C_InitToken( SLOT_ID, NULL, strlen((char *)sopass), label );
-   if (rc != CKR_ARGUMENTS_BAD) {
-      show_error(" C_InitToken Fail #1",rc);
-      goto done;
-   }
+    rc = funcs->C_InitToken(SLOT_ID, NULL, strlen((char *) sopass), label);
+    if (rc != CKR_ARGUMENTS_BAD) {
+        show_error(" C_InitToken Fail #1", rc);
+        goto done;
+    }
 
-   rc = funcs->C_InitToken( SLOT_ID, sopass, strlen((char *)sopass), NULL );
-   if (rc != CKR_ARGUMENTS_BAD) {
-      show_error(" C_InitToken Fail #2",rc);
-      goto done;
-   }
+    rc = funcs->C_InitToken(SLOT_ID, sopass, strlen((char *) sopass), NULL);
+    if (rc != CKR_ARGUMENTS_BAD) {
+        show_error(" C_InitToken Fail #2", rc);
+        goto done;
+    }
 
-   rc = funcs->C_InitToken( SLOT_ID, sopass, strlen((char *)sopass), label );
-   if (rc != CKR_OK) {
-      show_error("   C_InitToken #1", rc );
-      goto done;
-   }
+    rc = funcs->C_InitToken(SLOT_ID, sopass, strlen((char *) sopass), label);
+    if (rc != CKR_OK) {
+        show_error("   C_InitToken #1", rc);
+        goto done;
+    }
 
 done:
-   return rc;
+    return rc;
 }
 
 
-//
-//
-int
-main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-	CK_BYTE		   *pass = NULL;
-	int rc;
-	int i;
+    CK_BYTE *pass = NULL;
+    int rc;
+    int i;
 
-	SLOT_ID = 0;
+    SLOT_ID = 0;
 
-	for (i=1; i < argc; i++) {
-		if (strcmp(argv[i], "-slot") == 0) {
-			SLOT_ID = atoi(argv[i+1]);
-			i++;
-		} else if (strcmp(argv[i], "-pass") == 0) {
-                        pass = (CK_BYTE_PTR)strdup(argv[i+1]);
-			i++;
-		} else {
-			printf("usage:  %s [-slot <num>] [-h] -pass pass\n\n", argv[0] );
-			printf("By default, Slot 0 is used\n\n");
-			return -1;
-		}
-	}
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-slot") == 0) {
+            SLOT_ID = atoi(argv[i + 1]);
+            i++;
+        } else if (strcmp(argv[i], "-pass") == 0) {
+            pass = (CK_BYTE_PTR) strdup(argv[i + 1]);
+            i++;
+        } else {
+            printf("usage:  %s [-slot <num>] [-h] -pass pass\n\n", argv[0]);
+            printf("By default, Slot 0 is used\n\n");
+            return -1;
+        }
+    }
 
-	if (!pass) {
-		printf("usage:  %s [-slot <num>] [-h] [-pass pass]\n\n", argv[0] );
-		printf("By default, Slot 0 is used\n\n");
-		return -1;
-	}
+    if (!pass) {
+        printf("usage:  %s [-slot <num>] [-h] [-pass pass]\n\n", argv[0]);
+        printf("By default, Slot 0 is used\n\n");
+        return -1;
+    }
 
-	printf("Using slot #%lu...\n\n", SLOT_ID );
+    printf("Using slot #%lu...\n\n", SLOT_ID);
 
-	rc = do_GetFunctionList();
-	if ((rc != TRUE) || (funcs == NULL)) {
-		printf("do_GetFunctionList failed.\n");
-		return rc;
-	}
+    rc = do_GetFunctionList();
+    if ((rc != TRUE) || (funcs == NULL)) {
+        printf("do_GetFunctionList failed.\n");
+        return rc;
+    }
 
-	funcs->C_Initialize( NULL );
+    funcs->C_Initialize(NULL);
 
-	rc = do_inittoken(funcs, pass);
+    rc = do_inittoken(funcs, pass);
 
-	free(pass);
+    free(pass);
 
-	funcs->C_Finalize( NULL );
+    funcs->C_Finalize(NULL);
 
-	return rc;
+    return rc;
 }
--- testcases/login/login.c
+++ testcases/login/login.c
@@ -1,4 +1,3 @@
-
 /*
  * COPYRIGHT (c) International Business Machines Corp. 2005-2017
  *
@@ -21,103 +20,102 @@
 #include "regress.h"
 #include "common.c"
 
-CK_FUNCTION_LIST  *funcs;
+CK_FUNCTION_LIST *funcs;
 int do_GetFunctionList(void);
 
-int
-do_LoginLogout(CK_FUNCTION_LIST *funcs, CK_SLOT_ID slot_id, CK_USER_TYPE userType, char *pass)
+int do_LoginLogout(CK_FUNCTION_LIST * funcs, CK_SLOT_ID slot_id,
+                   CK_USER_TYPE userType, char *pass)
 {
-	CK_RV rc;
-	CK_SESSION_HANDLE session;
-	CK_FLAGS flags = CKF_SERIAL_SESSION;
+    CK_RV rc;
+    CK_SESSION_HANDLE session;
+    CK_FLAGS flags = CKF_SERIAL_SESSION;
 
-	if (userType == CKU_SO) {
-		flags |= CKF_RW_SESSION;
-	}
+    if (userType == CKU_SO) {
+        flags |= CKF_RW_SESSION;
+    }
 
-	rc = funcs->C_OpenSession(slot_id, flags, NULL, NULL, &session);
-	if (rc != CKR_OK) {
-		show_error("C_OpenSession", rc);
-		return rc;
-	}
-	rc = funcs->C_Login(session, userType, (CK_CHAR_PTR)pass, strlen(pass));
-	if (rc != CKR_OK) {
-		show_error("C_Login", rc);
-		return rc;
-	}
+    rc = funcs->C_OpenSession(slot_id, flags, NULL, NULL, &session);
+    if (rc != CKR_OK) {
+        show_error("C_OpenSession", rc);
+        return rc;
+    }
+    rc = funcs->C_Login(session, userType, (CK_CHAR_PTR) pass, strlen(pass));
+    if (rc != CKR_OK) {
+        show_error("C_Login", rc);
+        return rc;
+    }
 
-	printf("Logged in successfully, logging out...\n");
+    printf("Logged in successfully, logging out...\n");
 
-	rc = funcs->C_Logout(session);
-	if (rc != CKR_OK) {
-		show_error("C_Logout", rc);
-		return rc;
-	}
+    rc = funcs->C_Logout(session);
+    if (rc != CKR_OK) {
+        show_error("C_Logout", rc);
+        return rc;
+    }
 
-	printf("Logged out.\n");
+    printf("Logged out.\n");
 
-	rc = funcs->C_CloseSession(session);
-	if (rc != CKR_OK) {
-		show_error("C_CloseSession", rc);
-		return (int)rc;
-	}
+    rc = funcs->C_CloseSession(session);
+    if (rc != CKR_OK) {
+        show_error("C_CloseSession", rc);
+        return (int) rc;
+    }
 
-	return rc;
+    return rc;
 }
 
-//
-//
-int
-main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-	CK_C_INITIALIZE_ARGS	cinit_args;
-	CK_USER_TYPE		userType = CKU_USER;
-	CK_RV			rc = 0;
-	int			i;
-	char			*pass = NULL;
-	int			slot_id = 0;
+    CK_C_INITIALIZE_ARGS cinit_args;
+    CK_USER_TYPE userType = CKU_USER;
+    CK_RV rc = 0;
+    int i;
+    char *pass = NULL;
+    int slot_id = 0;
 
-	for (i=1; i < argc; i++) {
-		if (strcmp(argv[i], "-pass") == 0) {
-			++i;
-			pass = argv[i];
-		} else if (strcmp(argv[i], "-slot") == 0) {
-			++i;
-			slot_id = atoi(argv[i]);
-		} else if (strcmp(argv[i], "-user") == 0) {
-			userType = CKU_USER;
-			if (pass == NULL)
-				pass = "12345678";
-		} else if (strcmp(argv[i], "-so") == 0) {
-			userType = CKU_SO;
-			if (pass == NULL)
-				pass = "87654321";
-		} else {
-			printf("usage:  %s [-slot <num>] [-h] [-pass passwd] [-user|-so]\n\n", argv[0] );
-			printf("By default, Slot #%lu is used, as user\n\n", SLOT_ID);
-			return -1;
-		}
-	}
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-pass") == 0) {
+            ++i;
+            pass = argv[i];
+        } else if (strcmp(argv[i], "-slot") == 0) {
+            ++i;
+            slot_id = atoi(argv[i]);
+        } else if (strcmp(argv[i], "-user") == 0) {
+            userType = CKU_USER;
+            if (pass == NULL)
+                pass = "12345678";
+        } else if (strcmp(argv[i], "-so") == 0) {
+            userType = CKU_SO;
+            if (pass == NULL)
+                pass = "87654321";
+        } else {
+            printf
+                ("usage:  %s [-slot <num>] [-h] [-pass passwd] [-user|-so]\n\n",
+                 argv[0]);
+            printf("By default, Slot #%lu is used, as user\n\n", SLOT_ID);
+            return -1;
+        }
+    }
 
-	if (slot_id != SLOT_ID)
-		printf("Using user specified slot %d.\n", slot_id);
+    if (slot_id != SLOT_ID)
+        printf("Using user specified slot %d.\n", slot_id);
 
-	rc = do_GetFunctionList();
-	if (funcs == NULL)
-		return -1;
+    rc = do_GetFunctionList();
+    if (funcs == NULL)
+        return -1;
 
-	memset( &cinit_args, 0, sizeof(cinit_args) );
-	cinit_args.flags = CKF_OS_LOCKING_OK;
+    memset(&cinit_args, 0, sizeof(cinit_args));
+    cinit_args.flags = CKF_OS_LOCKING_OK;
 
-	rc = funcs->C_Initialize( &cinit_args );
-	if (rc != CKR_OK) {
-		show_error("C_Initialize", rc);
-		return -1;
-	}
+    rc = funcs->C_Initialize(&cinit_args);
+    if (rc != CKR_OK) {
+        show_error("C_Initialize", rc);
+        return -1;
+    }
 
-	rc = do_LoginLogout(funcs, slot_id, userType, pass);
+    rc = do_LoginLogout(funcs, slot_id, userType, pass);
 
-	funcs->C_Finalize( NULL );
+    funcs->C_Finalize(NULL);
 
-	return rc;
+    return rc;
 }
