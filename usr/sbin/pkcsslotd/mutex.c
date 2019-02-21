9a58a68e063acf1bd42fa58bfa95a2ac23a52e2e
Ingo Franzki <ifranzki@linux.ibm.com>
2019-02-21 17:33:07 +0100

    Fix coverty scan findings for stat/open
    A stat() before open() is not required and can cause timing
    ralted errors. The code can directly react on the result of
    open(). Flag O_CREAT will create the file only if it is not
    existent, so we don't need to know upfront if the file exists
    already or not.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/sbin/pkcsslotd/mutex.c
+++ usr/sbin/pkcsslotd/mutex.c
@@ -26,37 +26,31 @@ int CreateXProcLock(void)
 {
     struct group *grp;
     mode_t mode = (S_IRUSR | S_IRGRP);
-    struct stat statbuf;
 
     if (xplfd == -1) {
-        if (stat(OCK_API_LOCK_FILE, &statbuf) == 0) {
-            xplfd = open(OCK_API_LOCK_FILE, O_RDONLY, mode);
-        } else {
-            xplfd = open(OCK_API_LOCK_FILE, O_CREAT | O_RDONLY, mode);
+        xplfd = open(OCK_API_LOCK_FILE, O_CREAT | O_RDONLY, mode);
 
-            if (xplfd != -1) {
-                if (fchmod(xplfd, mode) == -1) {
-                    DbgLog(DL0, "%s:fchmod(%s):%s\n",
-                           __func__, OCK_API_LOCK_FILE, strerror(errno));
-                    goto error;
-                }
-
-                grp = getgrnam("pkcs11");
-                if (grp != NULL) {
-                    if (fchown(xplfd, -1, grp->gr_gid) == -1) {
-                        DbgLog(DL0, "%s:fchown(%s):%s\n",
-                               __func__,
-                               OCK_API_LOCK_FILE, strerror(errno));
-                        goto error;
-                    }
-                } else {
-                    DbgLog(DL0, "%s:getgrnam():%s\n",
-                           __func__, strerror(errno));
-                    goto error;
-                }
+        if (xplfd != -1) {
+            if (fchmod(xplfd, mode) == -1) {
+                DbgLog(DL0, "%s:fchmod(%s):%s\n",
+                       __func__, OCK_API_LOCK_FILE, strerror(errno));
+                goto error;
             }
-        }
-        if (xplfd == -1) {
+
+            grp = getgrnam("pkcs11");
+            if (grp != NULL) {
+                if (fchown(xplfd, -1, grp->gr_gid) == -1) {
+                    DbgLog(DL0, "%s:fchown(%s):%s\n",
+                           __func__,
+                           OCK_API_LOCK_FILE, strerror(errno));
+                    goto error;
+                }
+            } else {
+                DbgLog(DL0, "%s:getgrnam():%s\n",
+                       __func__, strerror(errno));
+                goto error;
+            }
+        } else  {
             DbgLog(DL0, "open(%s): %s\n", OCK_API_LOCK_FILE, strerror(errno));
             return FALSE;
         }
