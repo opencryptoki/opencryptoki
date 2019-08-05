460e3af391cbec7d414d94df6295a0f6472e114b
Ingo Franzki <ifranzki@linux.ibm.com>
2019-08-05 10:35:21 +0200

    COMMON: Fix per-user data store option
    Opencryptoki allows that a token uses a per-user data store. That is, the token
    directory then contains the user name, e.g. /var/lib/opencryptoki/tpm/root.
    Currently only the TPM token makes use of this, all other tokens share
    the data store across all, users.
    
    Support for per-user data store got lost with commit 'MTI [PATCH 04/85]:
    Initialize token' 39a7685295a7fe3688dd66cbed687deefe06bee6.
    
    Fixes: https://github.com/opencryptoki/opencryptoki/issues/242
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/common/loadsave.c
+++ usr/lib/common/loadsave.c
@@ -1651,6 +1651,7 @@ void init_data_store(char *directory, char *data_store)
         pk_dir = (char *) malloc(strlen(pkdir) + 1024);
         memset(pk_dir, 0, strlen(pkdir) + 1024);
         sprintf(pk_dir, "%s/%s", pkdir, SUB_DIR);
+        get_pk_dir(data_store);
         return;
     }
 
@@ -1658,13 +1659,12 @@ void init_data_store(char *directory, char *data_store)
         pk_dir = (char *) malloc(strlen(directory) + 25);
         memset(pk_dir, 0, strlen(directory) + 25);
         sprintf(pk_dir, "%s", directory);
-        memcpy(data_store, pk_dir, strlen(directory) + 25);
     } else {
         pk_dir = (char *) malloc(strlen(PK_DIR) + 25);
         memset(pk_dir, 0, strlen(PK_DIR) + 25);
         sprintf(pk_dir, "%s", PK_DIR);
-        memcpy(data_store, pk_dir, strlen(PK_DIR) + 25);
     }
+    get_pk_dir(data_store);
 
     return;
 }
