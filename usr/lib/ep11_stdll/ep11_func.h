fbea81f47364e609df106d254123f98c61cde6d6
Ingo Franzki <ifranzki@linux.ibm.com>
2019-08-12 15:30:32 +0200

    EP11: make target handling more type save
    Avoid all these casts by making the EP11 target field type save.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/ep11_stdll/ep11_func.h
+++ usr/lib/ep11_stdll/ep11_func.h
@@ -9,69 +9,69 @@
  */
 
 typedef CK_RV (*m_GenerateRandom_t) (CK_BYTE_PTR rnd, CK_ULONG len,
-                                     uint64_t target);
+                                     target_t target);
 typedef CK_RV (*m_SeedRandom_t) (CK_BYTE_PTR pSeed, CK_ULONG ulSeedLen,
-                                 uint64_t target);
+                                 target_t target);
 typedef CK_RV (*m_Digest_t) (const unsigned char *state, size_t slen,
                              CK_BYTE_PTR data, CK_ULONG len,
                              CK_BYTE_PTR digest, CK_ULONG_PTR dglen,
-                             uint64_t target);
+                             target_t target);
 typedef CK_RV (*m_DigestInit_t) (unsigned char *state, size_t * len,
                                  const CK_MECHANISM_PTR pmech,
-                                 uint64_t target);
+                                 target_t target);
 typedef CK_RV (*m_DigestUpdate_t) (unsigned char *state, size_t slen,
                                    CK_BYTE_PTR data, CK_ULONG dlen,
-                                   uint64_t target);
+                                   target_t target);
 typedef CK_RV (*m_DigestKey_t) (unsigned char *state, size_t slen,
                                 const unsigned char *key, size_t klen,
-                                uint64_t target);
+                                target_t target);
 typedef CK_RV (*m_DigestFinal_t) (const unsigned char *state,
                                   size_t slen, CK_BYTE_PTR digest,
-                                  CK_ULONG_PTR dlen, uint64_t target);
+                                  CK_ULONG_PTR dlen, target_t target);
 typedef CK_RV (*m_DigestSingle_t) (CK_MECHANISM_PTR pmech,
                                    CK_BYTE_PTR data, CK_ULONG len,
                                    CK_BYTE_PTR digest, CK_ULONG_PTR dlen,
-                                   uint64_t target);
+                                   target_t target);
 typedef CK_RV (*m_EncryptInit_t) (unsigned char *state, size_t * slen,
                                   CK_MECHANISM_PTR pmech,
                                   const unsigned char *key, size_t klen,
-                                  uint64_t target);
+                                  target_t target);
 typedef CK_RV (*m_DecryptInit_t) (unsigned char *state, size_t * slen,
                                   CK_MECHANISM_PTR pmech,
                                   const unsigned char *key, size_t klen,
-                                  uint64_t target);
+                                  target_t target);
 typedef CK_RV (*m_EncryptUpdate_t) (unsigned char *state, size_t slen,
                                     CK_BYTE_PTR plain, CK_ULONG plen,
                                     CK_BYTE_PTR cipher,
-                                    CK_ULONG_PTR clen, uint64_t target);
+                                    CK_ULONG_PTR clen, target_t target);
 typedef CK_RV (*m_DecryptUpdate_t) (unsigned char *state, size_t slen,
                                     CK_BYTE_PTR cipher, CK_ULONG clen,
                                     CK_BYTE_PTR plain, CK_ULONG_PTR plen,
-                                    uint64_t target);
+                                    target_t target);
 typedef CK_RV (*m_Encrypt_t) (const unsigned char *state, size_t slen,
                               CK_BYTE_PTR plain, CK_ULONG plen,
                               CK_BYTE_PTR cipher, CK_ULONG_PTR clen,
-                              uint64_t target);
+                              target_t target);
 typedef CK_RV (*m_Decrypt_t) (const unsigned char *state, size_t slen,
                               CK_BYTE_PTR cipher, CK_ULONG clen,
                               CK_BYTE_PTR plain, CK_ULONG_PTR plen,
-                              uint64_t target);
+                              target_t target);
 typedef CK_RV (*m_EncryptFinal_t) (const unsigned char *state,
                                    size_t slen, CK_BYTE_PTR output,
-                                   CK_ULONG_PTR len, uint64_t target);
+                                   CK_ULONG_PTR len, target_t target);
 typedef CK_RV (*m_DecryptFinal_t) (const unsigned char *state,
                                    size_t slen, CK_BYTE_PTR output,
-                                   CK_ULONG_PTR len, uint64_t target);
+                                   CK_ULONG_PTR len, target_t target);
 typedef CK_RV (*m_EncryptSingle_t) (const unsigned char *key,
                                     size_t klen, CK_MECHANISM_PTR mech,
                                     CK_BYTE_PTR plain, CK_ULONG plen,
                                     CK_BYTE_PTR cipher,
-                                    CK_ULONG_PTR clen, uint64_t target);
+                                    CK_ULONG_PTR clen, target_t target);
 typedef CK_RV (*m_DecryptSingle_t) (const unsigned char *key,
                                     size_t klen, CK_MECHANISM_PTR mech,
                                     CK_BYTE_PTR cipher, CK_ULONG clen,
                                     CK_BYTE_PTR plain, CK_ULONG_PTR plen,
-                                    uint64_t target);
+                                    target_t target);
 typedef CK_RV (*m_ReencryptSingle_t) (const unsigned char *dkey,
                                       size_t dklen,
                                       const unsigned char *ekey,
@@ -80,14 +80,14 @@ typedef CK_RV (*m_ReencryptSingle_t) (const unsigned char *dkey,
                                       CK_MECHANISM_PTR pencrmech,
                                       CK_BYTE_PTR in, CK_ULONG ilen,
                                       CK_BYTE_PTR out, CK_ULONG_PTR olen,
-                                      uint64_t target);
+                                      target_t target);
 typedef CK_RV (*m_GenerateKey_t) (CK_MECHANISM_PTR pmech,
                                   CK_ATTRIBUTE_PTR ptempl,
                                   CK_ULONG templcount,
                                   const unsigned char *pin,
                                   size_t pinlen, unsigned char *key,
                                   size_t * klen, unsigned char *csum,
-                                  size_t * clen, uint64_t target);
+                                  size_t * clen, target_t target);
 typedef CK_RV (*m_GenerateKeyPair_t) (CK_MECHANISM_PTR pmech,
                                       CK_ATTRIBUTE_PTR ppublic,
                                       CK_ULONG pubattrs,
@@ -97,45 +97,45 @@ typedef CK_RV (*m_GenerateKeyPair_t) (CK_MECHANISM_PTR pmech,
                                       size_t pinlen, unsigned char *key,
                                       size_t * klen,
                                       unsigned char *pubkey,
-                                      size_t * pklen, uint64_t target);
+                                      size_t * pklen, target_t target);
 typedef CK_RV (*m_SignInit_t) (unsigned char *state, size_t * slen,
                                CK_MECHANISM_PTR alg,
                                const unsigned char *key, size_t klen,
-                               uint64_t target);
+                               target_t target);
 typedef CK_RV (*m_VerifyInit_t) (unsigned char *state, size_t * slen,
                                  CK_MECHANISM_PTR alg,
                                  const unsigned char *key, size_t klen,
-                                 uint64_t target);
+                                 target_t target);
 typedef CK_RV (*m_SignUpdate_t) (unsigned char *state, size_t slen,
                                  CK_BYTE_PTR data, CK_ULONG dlen,
-                                 uint64_t target);
+                                 target_t target);
 typedef CK_RV (*m_VerifyUpdate_t) (unsigned char *state, size_t slen,
                                    CK_BYTE_PTR data, CK_ULONG dlen,
-                                   uint64_t target);
+                                   target_t target);
 typedef CK_RV (*m_SignFinal_t) (const unsigned char *state, size_t stlen,
                                 CK_BYTE_PTR sig, CK_ULONG_PTR siglen,
-                                uint64_t target);
+                                target_t target);
 typedef CK_RV (*m_VerifyFinal_t) (const unsigned char *state,
                                   size_t stlen, CK_BYTE_PTR sig,
-                                  CK_ULONG siglen, uint64_t target);
+                                  CK_ULONG siglen, target_t target);
 typedef CK_RV (*m_Sign_t) (const unsigned char *state, size_t stlen,
                            CK_BYTE_PTR data, CK_ULONG dlen,
                            CK_BYTE_PTR sig, CK_ULONG_PTR siglen,
-                            uint64_t target);
+                            target_t target);
 typedef CK_RV (*m_Verify_t) (const unsigned char *state, size_t stlen,
                              CK_BYTE_PTR data, CK_ULONG dlen,
                              CK_BYTE_PTR sig, CK_ULONG siglen,
-                             uint64_t target);
+                             target_t target);
 typedef CK_RV (*m_SignSingle_t) (const unsigned char *key, size_t klen,
                                  CK_MECHANISM_PTR pmech,
                                  CK_BYTE_PTR data, CK_ULONG dlen,
                                  CK_BYTE_PTR sig, CK_ULONG_PTR slen,
-                                 uint64_t target);
+                                 target_t target);
 typedef CK_RV (*m_VerifySingle_t) (const unsigned char *key, size_t klen,
                                    CK_MECHANISM_PTR pmech,
                                    CK_BYTE_PTR data, CK_ULONG dlen,
                                    CK_BYTE_PTR sig, CK_ULONG slen,
-                                   uint64_t target);
+                                   target_t target);
 
 /* mackey is NULL for PKCS#11 formats, not for authenticated ones */
 typedef CK_RV (*m_WrapKey_t) (const unsigned char *key, size_t keylen,
@@ -143,7 +143,7 @@ typedef CK_RV (*m_WrapKey_t) (const unsigned char *key, size_t keylen,
                               const unsigned char *mackey, size_t mklen,
                               const CK_MECHANISM_PTR pmech,
                               CK_BYTE_PTR wrapped, CK_ULONG_PTR wlen,
-                               uint64_t target);
+                               target_t target);
  /**/
 /* mackey is NULL for PKCS#11 formats, not for authenticated ones */
 typedef CK_RV (*m_UnwrapKey_t) (const CK_BYTE_PTR wrapped, CK_ULONG wlen,
@@ -156,7 +156,7 @@ typedef CK_RV (*m_UnwrapKey_t) (const CK_BYTE_PTR wrapped, CK_ULONG wlen,
                                 CK_ULONG pcount,
                                 unsigned char *unwrapped, size_t * uwlen,
                                 CK_BYTE_PTR csum, CK_ULONG * cslen,
-                                uint64_t target);
+                                target_t target);
 
 typedef CK_RV (*m_DeriveKey_t) (CK_MECHANISM_PTR pderivemech,
                                 CK_ATTRIBUTE_PTR ptempl,
@@ -167,36 +167,36 @@ typedef CK_RV (*m_DeriveKey_t) (CK_MECHANISM_PTR pderivemech,
                                 const unsigned char *pin, size_t pinlen,
                                 unsigned char *newkey, size_t * nklen,
                                 unsigned char *csum, size_t * cslen,
-                                uint64_t target);
+                                target_t target);
 
 typedef CK_RV (*m_GetMechanismList_t) (CK_SLOT_ID slot,
                                        CK_MECHANISM_TYPE_PTR mechs,
                                        CK_ULONG_PTR count,
-                                       uint64_t target);
+                                       target_t target);
 typedef CK_RV (*m_GetMechanismInfo_t) (CK_SLOT_ID slot,
                                        CK_MECHANISM_TYPE mech,
                                        CK_MECHANISM_INFO_PTR pmechinfo,
-                                       uint64_t target);
+                                       target_t target);
 typedef CK_RV (*m_GetAttributeValue_t) (const unsigned char *obj,
                                         size_t olen,
                                         CK_ATTRIBUTE_PTR pTemplate,
                                         CK_ULONG ulCount,
-                                        uint64_t target);
+                                        target_t target);
 typedef CK_RV (*m_SetAttributeValue_t) (unsigned char *obj, size_t olen,
                                         CK_ATTRIBUTE_PTR pTemplate,
                                         CK_ULONG ulCount,
-                                        uint64_t target);
+                                        target_t target);
 typedef CK_RV (*m_Login_t) (CK_UTF8CHAR_PTR pin, CK_ULONG pinlen,
                             const unsigned char *nonce, size_t nlen,
                             unsigned char *pinblob, size_t * pinbloblen,
-                            uint64_t target);
+                            target_t target);
 typedef CK_RV (*m_Logout_t) (const unsigned char *pin, size_t len,
-                             uint64_t target);
+                             target_t target);
 typedef CK_RV (*m_admin_t) (unsigned char *response1, size_t * r1len,
                             unsigned char *response2, size_t * r2len,
                             const unsigned char *cmd, size_t clen,
                             const unsigned char *sigs, size_t slen,
-                            uint64_t target);
+                            target_t target);
 typedef int (*m_add_backend_t) (const char *name, unsigned int port);
 typedef int (*m_init_t) (void);
 typedef int (*m_shutdown_t) (void);
@@ -335,7 +335,7 @@ typedef long (*xcpa_internal_rv_t) (const unsigned char *rsp, size_t rlen,
 
 typedef CK_RV (*m_get_xcp_info_t)(CK_VOID_PTR pinfo, CK_ULONG_PTR infbytes,
                                 unsigned int query, unsigned int subquery,
-                                uint64_t target);
+                                target_t target);
 
 #ifndef CK_IBM_XCP_HOSTQ_IDX
 
