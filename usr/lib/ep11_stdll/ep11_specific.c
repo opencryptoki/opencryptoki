396c5337280ffc10a08b9d5febbcb86bda40b378
Patrick Steuer <patrick.steuer@de.ibm.com>
2019-11-14 16:43:27 +0100

    ep11_specific.c: Appease -Wmissing-field-initializers
    Signed-off-by: Patrick Steuer <patrick.steuer@de.ibm.com>

--- usr/lib/ep11_stdll/ep11_specific.c
+++ usr/lib/ep11_stdll/ep11_specific.c
@@ -465,7 +465,7 @@ static void free_ep11_target_for_apqn(target_t target);
 /* mechanisms provided by this token will be generated from the underlaying
  * crypto adapter. Anyway to be conform to the generic mech_list handling
  * we need to define these dummies */
-MECH_LIST_ELEMENT mech_list[] = {{0}};
+MECH_LIST_ELEMENT mech_list[] = {{0, {0, 0, 0}}};
 
 CK_ULONG mech_list_len = 0;
 
