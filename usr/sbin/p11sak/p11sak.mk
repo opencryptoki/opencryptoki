6897ed693cd0e6c1776bc43d715b16c8b41684fd
Matthias Reumann <matthias.reumann1@ibm.com>
2020-07-28 12:23:25 +0200

    Added new command remove-key to the p11sak tool to remove key objects by cipher type. Optionally, the label can be given to remove key objects with the given label.
    Signed-off-by: Matthias Reumann <matthias.reumann1@ibm.com>

--- usr/sbin/p11sak/p11sak.mk
+++ usr/sbin/p11sak/p11sak.mk
@@ -5,8 +5,9 @@ usr_sbin_p11sak_p11sak_LDFLAGS = -ldl
 
 usr_sbin_p11sak_p11sak_CFLAGS  =			\
 	-DLINUX -DPROGRAM_NAME=\"$(@)\"					\
-	-I${srcdir}/usr/include \
+	-I${srcdir}/usr/include  -I${srcdir}/usr/lib/common\
 	-I${srcdir}/usr/sbin/p11sak
 
 usr_sbin_p11sak_p11sak_SOURCES =			\
-	usr/sbin/p11sak/p11sak.c
+	usr/lib/common/p11util.c \
+	usr/sbin/p11sak/p11sak.c 
