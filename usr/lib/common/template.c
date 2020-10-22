opencryptoki/opencryptoki

commit 07ee26eeef66e2ac334bfc361173f305d7d33011

Conform to PKCS 11 v3.0 Baseline Provider profile - part 2
Support unique id attribute.

Signed-off-by: Patrick Steuer <patrick.steuer@de.ibm.com>

usr/lib/common/template.c
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <openssl/rand.h>
 
 #include "pkcs11types.h"
 #include "defs.h"
@@ -40,6 +41,23 @@
 #include "pkcs32.h"
 #include "p11util.h"
 #include "trace.h"
+
+/* Random 32 byte string is unique with overwhelming probability. */
+#define UNIQUE_ID_LEN 32
+
+static CK_RV get_unique_id_str(char unique_id_str[2 * UNIQUE_ID_LEN + 1])
+{
+    unsigned char buf[UNIQUE_ID_LEN];
+    size_t i;
+
+    if (RAND_bytes(buf, sizeof(buf)) != 1)
+        return CKR_FUNCTION_FAILED;
+
+    for (i = 0; i < sizeof(buf); i++)
+        sprintf(&unique_id_str[i * 2], "%02x", buf[i]);
+
+    return CKR_OK;
+}
 
 /* template_add_attributes()
  *
@@ -470,9 +488,17 @@ CK_RV template_copy(TEMPLATE *dest, TEMPLATE *src)
             TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
             return CKR_HOST_MEMORY;
         }
-        memcpy(new_attr, attr, len);
 
+        memcpy(new_attr, attr, len);
         new_attr->pValue = (CK_BYTE *) new_attr + sizeof(CK_ATTRIBUTE);
+
+        if (attr->type == CKA_UNIQUE_ID) {
+            if (get_unique_id_str(new_attr->pValue) != CKR_OK) {
+                free(new_attr);
+                TRACE_ERROR("%s\n", ock_err(ERR_FUNCTION_FAILED));
+                return CKR_FUNCTION_FAILED;
+	    }
+	}
 
         dest->attribute_list = dlist_add_as_first(dest->attribute_list,
                                                   new_attr);
@@ -952,10 +978,15 @@ CK_RV template_merge(TEMPLATE *dest, TEMPLATE **src)
  */
 CK_RV template_set_default_common_attributes(TEMPLATE *tmpl)
 {
+    char unique_id_str[2 * UNIQUE_ID_LEN + 1];
     CK_ATTRIBUTE *token_attr;
     CK_ATTRIBUTE *priv_attr;
     CK_ATTRIBUTE *mod_attr;
     CK_ATTRIBUTE *label_attr;
+    CK_ATTRIBUTE *unique_id_attr;
+
+    if (get_unique_id_str(unique_id_str) != CKR_OK)
+        return CKR_FUNCTION_FAILED;
 
     /* add the default common attributes */
     token_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE)
@@ -965,8 +996,9 @@ CK_RV template_set_default_common_attributes(TEMPLATE *tmpl)
     mod_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE)
                                        + sizeof(CK_BBOOL));
     label_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + 0);
+    unique_id_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + UNIQUE_ID_LEN * 2);
 
-    if (!token_attr || !priv_attr || !mod_attr || !label_attr) {
+    if (!token_attr || !priv_attr || !mod_attr || !label_attr || !unique_id_attr) {
         if (token_attr)
             free(token_attr);
         if (priv_attr)
@@ -975,6 +1007,8 @@ CK_RV template_set_default_common_attributes(TEMPLATE *tmpl)
             free(mod_attr);
         if (label_attr)
             free(label_attr);
+        if (unique_id_attr)
+            free(unique_id_attr);
 
         TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
         return CKR_HOST_MEMORY;
@@ -999,10 +1033,16 @@ CK_RV template_set_default_common_attributes(TEMPLATE *tmpl)
     label_attr->ulValueLen = 0; // empty string
     label_attr->pValue = NULL;
 
+    unique_id_attr->type = CKA_UNIQUE_ID;
+    unique_id_attr->ulValueLen = UNIQUE_ID_LEN * 2;
+    unique_id_attr->pValue = (CK_BYTE *) unique_id_attr + sizeof(CK_ATTRIBUTE);
+    memcpy(unique_id_attr->pValue, unique_id_str, UNIQUE_ID_LEN * 2);
+
     template_update_attribute(tmpl, token_attr);
     template_update_attribute(tmpl, priv_attr);
     template_update_attribute(tmpl, mod_attr);
     template_update_attribute(tmpl, label_attr);
+    template_update_attribute(tmpl, unique_id_attr);
 
     /* the TEMPLATE 'owns' the attributes now.
      * it is responsible for freeing them upon deletion...
@@ -1247,6 +1287,12 @@ CK_RV template_validate_base_attribute(TEMPLATE *tmpl, CK_ATTRIBUTE *attr,
         if ((mode & (MODE_CREATE | MODE_COPY | MODE_DERIVE | MODE_KEYGEN |
                      MODE_UNWRAP)) != 0)
             return CKR_OK;
+        break;
+    case CKA_UNIQUE_ID:
+        if ((mode & (MODE_CREATE | MODE_COPY | MODE_DERIVE | MODE_KEYGEN |
+                     MODE_UNWRAP)) != 0)
+        TRACE_ERROR("%s\n", ock_err(ERR_ATTRIBUTE_READ_ONLY));
+        return CKR_ATTRIBUTE_READ_ONLY;
         break;
     default:
         TRACE_ERROR("%s\n", ock_err(ERR_TEMPLATE_INCONSISTENT));

testcases/misc_tests/obj_mgmt.c
@@ -193,7 +193,7 @@ CK_RV do_CopyObject(void)
 
     CK_BYTE false = FALSE;
 
-    CK_OBJECT_HANDLE h_data;
+    CK_OBJECT_HANDLE h_data, h_data2;
     CK_OBJECT_CLASS data_class = CKO_DATA;
     CK_BYTE data_application[] = "Test Application";
     CK_BYTE data_value[] = "1234567890abcedfghijklmnopqrstuvwxyz";
@@ -201,6 +201,13 @@ CK_RV do_CopyObject(void)
         {CKA_CLASS, &data_class, sizeof(data_class)},
         {CKA_TOKEN, &false, sizeof(false)},
         {CKA_VALUE, &data_value, sizeof(data_value)}
+    };
+    /* Invalid: coannot set CKA_UNIQUE_ID */
+    CK_ATTRIBUTE data_attribs2[] = {
+        {CKA_CLASS, &data_class, sizeof(data_class)},
+        {CKA_TOKEN, &false, sizeof(false)},
+        {CKA_VALUE, &data_value, sizeof(data_value)},
+        {CKA_UNIQUE_ID, &data_value, sizeof(data_value)}
     };
 
     CK_OBJECT_HANDLE h_copy;
@@ -216,6 +223,16 @@ CK_RV do_CopyObject(void)
     CK_BYTE buf2[100];
     CK_ATTRIBUTE prime_attribs[] = {
         {CKA_PRIME, &buf2, sizeof(buf2)}
+    };
+
+    CK_BYTE buf3[100];
+    CK_ATTRIBUTE unique_id_attribs[] = {
+        {CKA_UNIQUE_ID, &buf3, sizeof(buf3)}
+    };
+
+    CK_BYTE buf4[100];
+    CK_ATTRIBUTE unique_id_attribs2[] = {
+        {CKA_UNIQUE_ID, &buf4, sizeof(buf4)}
     };
 
     testcase_begin("starting...");
@@ -245,6 +262,13 @@ CK_RV do_CopyObject(void)
     /* create the object */
     rc = funcs->C_CreateObject(h_session, data_attribs, 3, &h_data);
     if (rc != CKR_OK) {
+        testcase_fail("C_CreateObject() rc = %s", p11_get_ckr(rc));
+        return rc;
+    }
+
+    /* try to create invalid object */
+    rc = funcs->C_CreateObject(h_session, data_attribs2, 4, &h_data2);
+    if (rc != CKR_ATTRIBUTE_READ_ONLY) {
         testcase_fail("C_CreateObject() rc = %s", p11_get_ckr(rc));
         return rc;
     }
@@ -299,6 +323,26 @@ CK_RV do_CopyObject(void)
         testcase_fail("C_GetAttributeValue() rc = %s (expected "
                       "CKR_OBJECT_HANDLE_INVALID)", p11_get_ckr(rc));
         return rc;
+    }
+
+    /* try to extract the CKA_UNIQUE_ID attribute from the original */
+    unique_id_attribs[0].ulValueLen = sizeof(buf3);
+    rc = funcs->C_GetAttributeValue(h_session, h_data, unique_id_attribs, 1);
+    if (rc != CKR_OK) {
+        testcase_fail("C_GetAttributeValue() rc=%s", p11_get_ckr(rc));
+        return rc;
+    }
+    /* now, try to extract the CKA_UNIQUE_IDattribute from the copy */
+    unique_id_attribs2[0].ulValueLen = sizeof(buf4);
+    rc = funcs->C_GetAttributeValue(h_session, h_copy, unique_id_attribs2, 1);
+    if (rc != CKR_OK) {
+        testcase_fail("C_GetAttributeValue() rc=%s", p11_get_ckr(rc));
+        return rc;
+    }
+    if (memcmp(unique_id_attribs[0].pValue,
+               unique_id_attribs2[0].pValue, 64) == 0) {
+        testcase_fail("unique id attributes match");
+        return -1;
     }
 
     /* now, get the size of the original object */
@@ -381,6 +425,7 @@ CK_RV do_SetAttributeValues(void)
     CK_BYTE cert_id[] = "Certificate ID";
     CK_BYTE cert_value[] =
         "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    CK_BYTE unique_id[] = "deadbeef";
 
     CK_ATTRIBUTE cert_attribs[] = {
         {CKA_CLASS, &cert_class, sizeof(cert_class)},
@@ -398,6 +443,10 @@ CK_RV do_SetAttributeValues(void)
         {CKA_SERIAL_NUMBER, &cert_ser_no, sizeof(cert_ser_no)},
         {CKA_ISSUER, &cert_issuer, sizeof(cert_issuer)},
         {CKA_ID, &cert_id2, sizeof(cert_id2)}
+    };
+    /* Invalid: update CKA_UNIQUE_ID */
+    CK_ATTRIBUTE update_attr2[] = {
+        {CKA_UNIQUE_ID, &unique_id, sizeof(unique_id)}
     };
 
     CK_BYTE cert_value2[] = "Invalid Value";
@@ -434,6 +483,13 @@ CK_RV do_SetAttributeValues(void)
     rc = funcs->C_CreateObject(h_session, cert_attribs, 6, &h_cert);
     if (rc != CKR_OK) {
         testcase_fail("C_CreateObject() rc = %s", p11_get_ckr(rc));
+        return rc;
+    }
+
+    /* Try to change the CKA_UNIQUE_ID */
+    rc = funcs->C_SetAttributeValue(h_session, h_cert, update_attr2, 1);
+    if (rc !=  CKR_ATTRIBUTE_READ_ONLY) {
+        testcase_fail("C_SetAttributeValue() rc = %s", p11_get_ckr(rc));
         return rc;
     }
 

usr/lib/common/p11util.c
@@ -167,6 +167,7 @@ CK_BBOOL is_attribute_defined(CK_ATTRIBUTE_TYPE type)
     case CKA_ALWAYS_SENSITIVE:
     case CKA_ALWAYS_AUTHENTICATE:
     case CKA_MODIFIABLE:
+    case CKA_UNIQUE_ID:
     case CKA_ECDSA_PARAMS:
     case CKA_EC_POINT:
     case CKA_HW_FEATURE_TYPE:

commit d7b2ee8f4eb9b4ad55a08800bb03a76145181b09

Fix buffer overrun in C_CopyObject
When copying attribute CKA_UNIQUE_ID a buffer overrun by 1 byte
occurs when generating a new unique id for the copied object.

Fixes: 07ee26eeef66e2ac334bfc361173f305d7d33011

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/common/template.c
@@ -484,6 +484,7 @@ CK_BBOOL template_compare(CK_ATTRIBUTE *t1, CK_ULONG ulCount, TEMPLATE *t2)
  */
 CK_RV template_copy(TEMPLATE *dest, TEMPLATE *src)
 {
+    char unique_id_str[2 * UNIQUE_ID_LEN + 1];
     DL_NODE *node;
 
     if (!dest || !src) {
@@ -509,12 +510,19 @@ CK_RV template_copy(TEMPLATE *dest, TEMPLATE *src)
         new_attr->pValue = (CK_BYTE *) new_attr + sizeof(CK_ATTRIBUTE);
 
         if (attr->type == CKA_UNIQUE_ID) {
-            if (get_unique_id_str(new_attr->pValue) != CKR_OK) {
+            if (attr->ulValueLen < 2 * UNIQUE_ID_LEN) {
+                free(new_attr);
+                TRACE_ERROR("%s\n", ock_err(ERR_ATTRIBUTE_VALUE_INVALID));
+                return CKR_ATTRIBUTE_VALUE_INVALID;
+            }
+            if (get_unique_id_str(unique_id_str) != CKR_OK) {
                 free(new_attr);
                 TRACE_ERROR("%s\n", ock_err(ERR_FUNCTION_FAILED));
                 return CKR_FUNCTION_FAILED;
-	    }
-	}
+            }
+            memcpy(new_attr->pValue, unique_id_str, 2 * UNIQUE_ID_LEN);
+            new_attr->ulValueLen = 2 * UNIQUE_ID_LEN;
+        }
 
         dest->attribute_list = dlist_add_as_first(dest->attribute_list,
                                                   new_attr);