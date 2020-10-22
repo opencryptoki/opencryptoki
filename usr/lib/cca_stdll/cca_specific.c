opencryptoki/opencryptoki

commit 8ca2cce225218af74abd8e0064fa3f711bebbb77

COMMON: Make symmetric key size of secure key tokens variable length
The key blob size of symmetric keys is not always the same for keys
of different algorithms. As of today, field token_keysize in struct
token_specific_struct sets a fixed size of DES, DES3, and AES key
blobs for secure key tokens.

Allow the token specific key generation function to generate an opaque key
blob of any length. For clear key tokens, the generated key size must still
be the expected clear key size of the used algorithm, but for secure key
tokens, the opaque key blob can be of any size.

Also let the token specific key generation function decide if the returned
key blob is opaque or not. Some key tokens may only use secure keys for
certain algorithms, but use clear keys for others.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/cca_stdll/cca_specific.c
@@ -508,22 +508,25 @@ static CK_RV cca_key_gen(enum cca_key_type type, CK_BYTE * key,
     return CKR_OK;
 }
 
-CK_RV token_specific_des_key_gen(STDLL_TokData_t * tokdata, CK_BYTE * des_key,
-                                 CK_ULONG len, CK_ULONG keysize)
+CK_RV token_specific_des_key_gen(STDLL_TokData_t *tokdata, CK_BYTE **des_key,
+                                 CK_ULONG *len, CK_ULONG keysize,
+                                 CK_BBOOL *is_opaque)
 {
     unsigned char key_form[CCA_KEYWORD_SIZE];
     unsigned char key_type_1[CCA_KEYWORD_SIZE];
 
     UNUSED(tokdata);
 
-    /* make sure key is the right size for the token */
-    if (len != CCA_KEY_ID_SIZE)
-        return CKR_FUNCTION_FAILED;
+    *des_key = malloc(CCA_KEY_ID_SIZE);
+    if (*des_key == NULL)
+        return CKR_HOST_MEMORY;
+    *len = CCA_KEY_ID_SIZE;
+    *is_opaque = TRUE;
 
     memcpy(key_form, "OP      ", (size_t) CCA_KEYWORD_SIZE);
     memcpy(key_type_1, "DATA    ", (size_t) CCA_KEYWORD_SIZE);
 
-    return cca_key_gen(CCA_DES_KEY, des_key, key_form, key_type_1, keysize);
+    return cca_key_gen(CCA_DES_KEY, *des_key, key_form, key_type_1, keysize);
 }
 
 
@@ -1202,8 +1205,9 @@ CK_RV token_specific_rsa_verify(STDLL_TokData_t * tokdata,
 
 
 #ifndef NOAES
-CK_RV token_specific_aes_key_gen(STDLL_TokData_t * tokdata, CK_BYTE * aes_key,
-                                 CK_ULONG len, CK_ULONG key_size)
+CK_RV token_specific_aes_key_gen(STDLL_TokData_t *tokdata, CK_BYTE **aes_key,
+                                 CK_ULONG *len, CK_ULONG key_size,
+                                 CK_BBOOL *is_opaque)
 {
     long return_code, reason_code;
     unsigned char key_token[CCA_KEY_ID_SIZE] = { 0, };
@@ -1218,9 +1222,11 @@ CK_RV token_specific_aes_key_gen(STDLL_TokData_t * tokdata, CK_BYTE * aes_key,
 
     UNUSED(tokdata);
 
-    /* make sure key is the right size for the token */
-    if (len != CCA_KEY_ID_SIZE)
-        return CKR_FUNCTION_FAILED;
+    *aes_key = malloc(CCA_KEY_ID_SIZE);
+    if (*aes_key == NULL)
+        return CKR_HOST_MEMORY;
+    *len = CCA_KEY_ID_SIZE;
+    *is_opaque = TRUE;
 
     memcpy(rule_array, "INTERNALAES     NO-KEY  ",
            (size_t) (CCA_KEYWORD_SIZE * 3));
@@ -1263,9 +1269,9 @@ CK_RV token_specific_aes_key_gen(STDLL_TokData_t * tokdata, CK_BYTE * aes_key,
     }
     memcpy(key_form, "OP      ", (size_t) CCA_KEYWORD_SIZE);
     memcpy(key_type, "AESTOKEN", (size_t) CCA_KEYWORD_SIZE);
-    memcpy(aes_key, key_token, (size_t) CCA_KEY_ID_SIZE);
+    memcpy(*aes_key, key_token, (size_t) CCA_KEY_ID_SIZE);
 
-    return cca_key_gen(CCA_AES_KEY, aes_key, key_form, key_type, key_size);
+    return cca_key_gen(CCA_AES_KEY, *aes_key, key_form, key_type, key_size);
 }
 
 CK_RV token_specific_aes_ecb(STDLL_TokData_t * tokdata,

usr/lib/common/loadsave.c
@@ -1328,15 +1328,16 @@ CK_RV generate_master_key_old(STDLL_TokData_t *tokdata, CK_BYTE *key)
 {
     CK_RV rc = CKR_OK;
     CK_ULONG key_len = 0L;
-    CK_ULONG master_key_len = 0L;
+    CK_ULONG master_key_len;
+    CK_BYTE *master_key = NULL;
+    CK_BBOOL is_opaque = FALSE;
 
     /* Skip it if master key is not needed. */
     if (!token_specific.data_store.use_master_key)
         return CKR_OK;
 
-    if ((rc = get_encryption_info_for_clear_key(&key_len, NULL)) != CKR_OK ||
-        (rc = get_encryption_info(&master_key_len, NULL)) != CKR_OK)
-        return ERR_FUNCTION_FAILED;
+    if ((rc = get_encryption_info_for_clear_key(&key_len, NULL)) != CKR_OK)
+        return CKR_FUNCTION_FAILED;
 
     /* For secure key tokens, object encrypt/decrypt uses
      * software(openssl), not token. So generate masterkey via RNG.
@@ -1349,14 +1350,32 @@ CK_RV generate_master_key_old(STDLL_TokData_t *tokdata, CK_BYTE *key)
      */
     switch (token_specific.data_store.encryption_algorithm) {
     case CKM_DES3_CBC:
-        return token_specific.t_des_key_gen(tokdata, key,
-                                            master_key_len, key_len);
+        rc = token_specific.t_des_key_gen(tokdata, &master_key,
+                                          &master_key_len, key_len,
+                                          &is_opaque);
+        break;
     case CKM_AES_CBC:
-        return token_specific.t_aes_key_gen(tokdata, key,
-                                            master_key_len, key_len);
+        rc = token_specific.t_aes_key_gen(tokdata, &master_key,
+                                          &master_key_len, key_len,
+                                          &is_opaque);
+        break;
+    default:
+        return CKR_MECHANISM_INVALID;
     }
 
-    return ERR_MECHANISM_INVALID;
+    if (rc != CKR_OK)
+        return rc;
+
+    if (master_key_len != key_len) {
+        TRACE_ERROR("Invalid master key size: %lu\n", master_key_len);
+        free(master_key);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    memcpy(key, master_key, master_key_len);
+    free(master_key);
+
+    return CKR_OK;
 }
 
 CK_RV restore_private_token_object_old(STDLL_TokData_t *tokdata, CK_BYTE *data,

usr/lib/common/mech_aes.c
@@ -3360,12 +3360,11 @@ CK_RV ckm_aes_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     CK_ATTRIBUTE *local_attr = NULL;
     CK_ATTRIBUTE *val_len_attr = NULL;
     CK_BYTE *aes_key = NULL;
-    CK_BYTE dummy_key[AES_KEY_SIZE_256] = { 0, };
     CK_ULONG rc;
     CK_ULONG key_size;
     CK_ULONG token_keysize;
     CK_BBOOL found = FALSE;
-
+    CK_BBOOL is_opaque = FALSE;
 
     found = template_attribute_find(tmpl, CKA_VALUE_LEN, &val_len_attr);
     if (found == FALSE)
@@ -3382,26 +3381,14 @@ CK_RV ckm_aes_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
         return CKR_MECHANISM_INVALID;
     }
 
-    if (is_secure_key_token())
-        token_keysize = token_specific.token_keysize;
-    else
-        token_keysize = key_size;
-
-    if ((aes_key = (CK_BYTE *) calloc(1, token_keysize)) == NULL) {
-        TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
-        return CKR_HOST_MEMORY;
-    }
-
-    rc = token_specific.t_aes_key_gen(tokdata, aes_key, token_keysize,
-                                      key_size);
+    rc = token_specific.t_aes_key_gen(tokdata, &aes_key, &token_keysize,
+                                      key_size, &is_opaque);
 
     if (rc != CKR_OK)
         goto err;
 
-    /* For secure-key keys put in CKA_IBM_OPAQUE
-     * and put dummy_key in CKA_VALUE.
-     */
-    if (is_secure_key_token()) {
+    /* For opaque keys put in CKA_IBM_OPAQUE and put dummy_key in CKA_VALUE. */
+    if (is_opaque) {
         opaque_attr =
             (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + token_keysize);
         if (!opaque_attr) {
@@ -3414,6 +3401,11 @@ CK_RV ckm_aes_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
         opaque_attr->pValue = (CK_BYTE *) opaque_attr + sizeof(CK_ATTRIBUTE);
         memcpy(opaque_attr->pValue, aes_key, token_keysize);
         template_update_attribute(tmpl, opaque_attr);
+    } else {
+        if (token_keysize != key_size) {
+            TRACE_ERROR("Invalid key size: %lu\n", token_keysize);
+            return CKR_FUNCTION_FAILED;
+        }
     }
 
     value_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + key_size);
@@ -3442,8 +3434,8 @@ CK_RV ckm_aes_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     value_attr->type = CKA_VALUE;
     value_attr->ulValueLen = key_size;
     value_attr->pValue = (CK_BYTE *) value_attr + sizeof(CK_ATTRIBUTE);
-    if (is_secure_key_token())
-        memcpy(value_attr->pValue, dummy_key, key_size);
+    if (is_opaque)
+        memset(value_attr->pValue, 0, key_size);
     else
         memcpy(value_attr->pValue, aes_key, key_size);
     free(aes_key);

usr/lib/common/mech_des.c
@@ -1225,33 +1225,22 @@ CK_RV ckm_des_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     CK_ATTRIBUTE *class_attr = NULL;
     CK_ATTRIBUTE *local_attr = NULL;
     CK_BYTE *des_key = NULL;
-    CK_BYTE dummy_key[DES_KEY_SIZE] = { 0, };
     CK_ULONG rc;
-    CK_ULONG keysize;
+    CK_ULONG keysize = 0;
+    CK_BBOOL is_opaque = FALSE;
 
     if (token_specific.t_des_key_gen == NULL) {
         TRACE_ERROR("%s\n", ock_err(ERR_MECHANISM_INVALID));
         return CKR_MECHANISM_INVALID;
     }
 
-    if (is_secure_key_token())
-        keysize = token_specific.token_keysize;
-    else
-        keysize = DES_KEY_SIZE;
-
-    if ((des_key = (CK_BYTE *) calloc(1, keysize)) == NULL) {
-        TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
-        return CKR_HOST_MEMORY;
-    }
-
-    rc = token_specific.t_des_key_gen(tokdata, des_key, keysize, DES_KEY_SIZE);
+    rc = token_specific.t_des_key_gen(tokdata, &des_key, &keysize,
+                                      DES_KEY_SIZE, &is_opaque);
     if (rc != CKR_OK)
         goto err;
 
-    /* For secure-key keys put in CKA_IBM_OPAQUE
-     * and put dummy_key in CKA_VALUE.
-     */
-    if (is_secure_key_token()) {
+    /* For opaque keys put in CKA_IBM_OPAQUE and put dummy_key in CKA_VALUE. */
+    if (is_opaque) {
         opaque_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + keysize);
         if (!opaque_attr) {
             TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
@@ -1263,6 +1252,11 @@ CK_RV ckm_des_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
         opaque_attr->pValue = (CK_BYTE *) opaque_attr + sizeof(CK_ATTRIBUTE);
         memcpy(opaque_attr->pValue, des_key, keysize);
         template_update_attribute(tmpl, opaque_attr);
+    } else {
+        if (keysize != DES_KEY_SIZE) {
+            TRACE_ERROR("Invalid key size: %lu\n", keysize);
+            return CKR_FUNCTION_FAILED;
+        }
     }
 
     value_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + DES_KEY_SIZE);
@@ -1291,8 +1285,8 @@ CK_RV ckm_des_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     value_attr->type = CKA_VALUE;
     value_attr->ulValueLen = DES_KEY_SIZE;
     value_attr->pValue = (CK_BYTE *) value_attr + sizeof(CK_ATTRIBUTE);
-    if (is_secure_key_token())
-        memcpy(value_attr->pValue, dummy_key, DES_KEY_SIZE);
+    if (is_opaque)
+        memset(value_attr->pValue, 0, DES_KEY_SIZE);
     else
         memcpy(value_attr->pValue, des_key, DES_KEY_SIZE);
     free(des_key);

usr/lib/common/mech_des3.c
@@ -2708,34 +2708,22 @@ CK_RV ckm_des3_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     CK_ATTRIBUTE *class_attr = NULL;
     CK_ATTRIBUTE *local_attr = NULL;
     CK_BYTE *des_key = NULL;
-    CK_BYTE dummy_key[3 * DES_KEY_SIZE] = { 0, };
     CK_ULONG rc;
-    CK_ULONG keysize;
+    CK_ULONG keysize = 0;
+    CK_BBOOL is_opaque = FALSE;
 
     if (token_specific.t_des_key_gen == NULL) {
         TRACE_ERROR("%s\n", ock_err(ERR_MECHANISM_INVALID));
         return CKR_MECHANISM_INVALID;
     }
 
-    if (is_secure_key_token())
-        keysize = token_specific.token_keysize;
-    else
-        keysize = (3 * DES_KEY_SIZE);
-
-    if ((des_key = (CK_BYTE *) calloc(1, keysize)) == NULL) {
-        TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
-        return CKR_HOST_MEMORY;
-    }
-
-    rc = token_specific.t_des_key_gen(tokdata, des_key, keysize,
-                                      3 * DES_KEY_SIZE);
+    rc = token_specific.t_des_key_gen(tokdata, &des_key, &keysize,
+                                      3 * DES_KEY_SIZE, &is_opaque);
     if (rc != CKR_OK)
         goto err;
 
-    /* For secure-key keys put in CKA_IBM_OPAQUE
-     * and put dummy_key in CKA_VALUE.
-     */
-    if (is_secure_key_token()) {
+    /* For opaque keys put in CKA_IBM_OPAQUE and put dummy_key in CKA_VALUE. */
+    if (is_opaque) {
         opaque_attr = (CK_ATTRIBUTE *) malloc(sizeof(CK_ATTRIBUTE) + keysize);
         if (!opaque_attr) {
             TRACE_ERROR("%s\n", ock_err(ERR_HOST_MEMORY));
@@ -2747,6 +2735,11 @@ CK_RV ckm_des3_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
         opaque_attr->pValue = (CK_BYTE *) opaque_attr + sizeof(CK_ATTRIBUTE);
         memcpy(opaque_attr->pValue, des_key, keysize);
         template_update_attribute(tmpl, opaque_attr);
+    } else {
+        if (keysize != 3 * DES_KEY_SIZE) {
+            TRACE_ERROR("Invalid key size: %lu\n", keysize);
+            return CKR_FUNCTION_FAILED;
+        }
     }
 
     value_attr =
@@ -2776,8 +2769,8 @@ CK_RV ckm_des3_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     value_attr->type = CKA_VALUE;
     value_attr->ulValueLen = 3 * DES_KEY_SIZE;
     value_attr->pValue = (CK_BYTE *) value_attr + sizeof(CK_ATTRIBUTE);
-    if (is_secure_key_token())
-        memcpy(value_attr->pValue, dummy_key, 3 * DES_KEY_SIZE);
+    if (is_opaque)
+        memset(value_attr->pValue, 0, 3 * DES_KEY_SIZE);
     else
         memcpy(value_attr->pValue, des_key, 3 * DES_KEY_SIZE);
     free(des_key);

usr/lib/common/tok_spec_struct.h
@@ -82,7 +82,8 @@ struct token_specific_struct {
     CK_RV(*t_set_pin) (STDLL_TokData_t *, SESSION *, CK_CHAR_PTR, CK_ULONG,
                        CK_CHAR_PTR, CK_ULONG);
 
-    CK_RV(*t_des_key_gen) (STDLL_TokData_t *, CK_BYTE *, CK_ULONG, CK_ULONG);
+    CK_RV(*t_des_key_gen) (STDLL_TokData_t *, CK_BYTE **, CK_ULONG *, CK_ULONG,
+                           CK_BBOOL *);
     CK_RV(*t_des_ecb) (STDLL_TokData_t *, CK_BYTE *, CK_ULONG,
                        CK_BYTE *, CK_ULONG *, OBJECT *, CK_BYTE);
     CK_RV(*t_des_cbc) (STDLL_TokData_t *, CK_BYTE *, CK_ULONG,
@@ -208,7 +209,8 @@ struct token_specific_struct {
     CK_RV(*t_generic_secret_key_gen) (STDLL_TokData_t *, TEMPLATE *);
 
     // Token Specific AES functions
-    CK_RV(*t_aes_key_gen) (STDLL_TokData_t *, CK_BYTE *, CK_ULONG, CK_ULONG);
+    CK_RV(*t_aes_key_gen) (STDLL_TokData_t *, CK_BYTE **, CK_ULONG *, CK_ULONG,
+                           CK_BBOOL *);
 
     CK_RV(*t_aes_ecb) (STDLL_TokData_t *tokdata, CK_BYTE *, CK_ULONG,
                        CK_BYTE *, CK_ULONG *, OBJECT *, CK_BYTE);

usr/lib/common/tok_specific.h
@@ -49,8 +49,8 @@ CK_RV token_specific_init_pin(STDLL_TokData_t *, SESSION *, CK_CHAR_PTR,
 CK_RV token_specific_set_pin(STDLL_TokData_t *, SESSION *, CK_CHAR_PTR,
                              CK_ULONG, CK_CHAR_PTR, CK_ULONG);
 
-CK_RV token_specific_des_key_gen(STDLL_TokData_t *, CK_BYTE *, CK_ULONG,
-                                 CK_ULONG);
+CK_RV token_specific_des_key_gen(STDLL_TokData_t *, CK_BYTE **, CK_ULONG *,
+                                 CK_ULONG, CK_BBOOL *);
 
 CK_RV token_specific_des_ecb(STDLL_TokData_t *,
                              CK_BYTE *,
@@ -234,7 +234,7 @@ CK_RV token_specific_generic_secret_key_gen(STDLL_TokData_t *,
 
 #ifndef NOAES
 CK_RV token_specific_aes_key_gen(STDLL_TokData_t *,
-                                 CK_BYTE *, CK_ULONG, CK_ULONG);
+                                 CK_BYTE **, CK_ULONG *, CK_ULONG, CK_BBOOL *);
 
 CK_RV token_specific_aes_ecb(STDLL_TokData_t *,
                              CK_BYTE *,

usr/lib/ica_s390_stdll/ica_specific.c
@@ -360,22 +360,27 @@ static void adjust_des_key_parity_bits(CK_BYTE *des_key, CK_ULONG key_size,
     }
 }
 
-CK_RV token_specific_des_key_gen(STDLL_TokData_t *tokdata, CK_BYTE *des_key,
-                                 CK_ULONG len, CK_ULONG keysize)
+CK_RV token_specific_des_key_gen(STDLL_TokData_t *tokdata, CK_BYTE **des_key,
+                                 CK_ULONG *len, CK_ULONG keysize,
+                                 CK_BBOOL *is_opaque)
 {
-    UNUSED(keysize);
+    *des_key = malloc(keysize);
+    if (*des_key == NULL)
+        return CKR_HOST_MEMORY;
+    *len = keysize;
+    *is_opaque = FALSE;
 
     // Nothing different to do for DES or TDES here as this is just
     // random data...  Validation handles the rest
     // Only check for weak keys when DES.
-    if (len == (3 * DES_KEY_SIZE)) {
-        rng_generate(tokdata, des_key, len);
-        adjust_des_key_parity_bits(des_key, len, ODD_PARITY);
+    if (keysize == (3 * DES_KEY_SIZE)) {
+        rng_generate(tokdata, *des_key, keysize);
+        adjust_des_key_parity_bits(*des_key, keysize, ODD_PARITY);
     } else {
         do {
-            rng_generate(tokdata, des_key, len);
-            adjust_des_key_parity_bits(des_key, len, ODD_PARITY);
-        } while (des_check_weak_key(des_key) == TRUE);
+            rng_generate(tokdata, *des_key, keysize);
+            adjust_des_key_parity_bits(*des_key, keysize, ODD_PARITY);
+        } while (des_check_weak_key(*des_key) == TRUE);
     }
 
 
@@ -2861,12 +2866,17 @@ done:
 
 #ifndef NOAES
 
-CK_RV token_specific_aes_key_gen(STDLL_TokData_t *tokdata, CK_BYTE *key,
-                                 CK_ULONG len, CK_ULONG keysize)
+CK_RV token_specific_aes_key_gen(STDLL_TokData_t *tokdata, CK_BYTE **key,
+                                 CK_ULONG *len, CK_ULONG keysize,
+                                 CK_BBOOL *is_opaque)
 {
-    UNUSED(keysize);
+    *key = malloc(keysize);
+    if (*key == NULL)
+        return CKR_HOST_MEMORY;
+    *len = keysize;
+    *is_opaque = FALSE;
 
-    return rng_generate(tokdata, key, len);
+    return rng_generate(tokdata, *key, keysize);
 }
 
 CK_RV token_specific_aes_ecb(STDLL_TokData_t *tokdata, CK_BYTE *in_data,

commit fa1b3102a5f8479f4248c977de4df0718d13721f

CCA: Zeroize key buffer to avoid CCA 8/32 error
Fixes: 8ca2cce225218af74abd8e0064fa3f711bebbb77

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/cca_stdll/cca_specific.c
@@ -526,7 +526,7 @@ CK_RV token_specific_des_key_gen(STDLL_TokData_t *tokdata, CK_BYTE **des_key,
 
     UNUSED(tokdata);
 
-    *des_key = malloc(CCA_KEY_ID_SIZE);
+    *des_key = calloc(CCA_KEY_ID_SIZE, 1);
     if (*des_key == NULL)
         return CKR_HOST_MEMORY;
     *len = CCA_KEY_ID_SIZE;
@@ -1734,7 +1734,7 @@ CK_RV token_specific_aes_key_gen(STDLL_TokData_t *tokdata, CK_BYTE **aes_key,
 
     UNUSED(tokdata);
 
-    *aes_key = malloc(CCA_KEY_ID_SIZE);
+    *aes_key = calloc(CCA_KEY_ID_SIZE, 1);
     if (*aes_key == NULL)
         return CKR_HOST_MEMORY;
     *len = CCA_KEY_ID_SIZE;