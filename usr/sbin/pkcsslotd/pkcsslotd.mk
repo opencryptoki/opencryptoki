5dac2baac2efa30164669fb8ee17db836ba749f0
Juergen Christ <jchrist@linux.ibm.com>
2021-02-11 16:22:13 +0100

    BUILD: Fix out of tree builds
    During parser rewrite I broke out of tree builds for the generated parser
    header.  I also did not take care of the distribution bundle.  Fixed this by
    rewriting part of the automake file.
    
    Signed-off-by: Juergen Christ <jchrist@linux.ibm.com>

--- usr/sbin/pkcsslotd/pkcsslotd.mk
+++ usr/sbin/pkcsslotd/pkcsslotd.mk
@@ -3,20 +3,23 @@ noinst_HEADERS +=							\
 	usr/sbin/pkcsslotd/err.h usr/sbin/pkcsslotd/garbage_linux.h	\
 	usr/sbin/pkcsslotd/log.h usr/sbin/pkcsslotd/pkcsslotd.h
 
-BUILT_SOURCES += usr/lib/common/parser.h
 EXTRA_DIST += usr/sbin/pkcsslotd/opencryptoki.conf
 CLEANFILES += usr/lib/common/parser.c usr/lib/common/parser.h	\
 	usr/lib/common/parser.output usr/lib/common/lexer.c
 
 usr_sbin_pkcsslotd_pkcsslotd_LDFLAGS = -lpthread -lcrypto
 
-usr_sbin_pkcsslotd_pkcsslotd_CFLAGS =							\
-	-DPROGRAM_NAME=\"$(@)\" -I${srcdir}/usr/include -I${srcdir}/usr/lib/common
+usr_sbin_pkcsslotd_pkcsslotd_CFLAGS = -DPROGRAM_NAME=\"$(@)\"	\
+	-I${srcdir}/usr/include -I${srcdir}/usr/lib/common	\
+	-I${top_builddir}/usr/lib/common
 
 usr_sbin_pkcsslotd_pkcsslotd_SOURCES =							\
 	usr/sbin/pkcsslotd/slotmgr.c usr/sbin/pkcsslotd/shmem.c				\
 	usr/sbin/pkcsslotd/signal.c usr/sbin/pkcsslotd/mutex.c usr/sbin/pkcsslotd/err.c	\
 	usr/sbin/pkcsslotd/log.c usr/sbin/pkcsslotd/daemon.c				\
 	usr/sbin/pkcsslotd/garbage_linux.c usr/sbin/pkcsslotd/pkcsslotd_util.c		\
-	usr/sbin/pkcsslotd/socket_server.c usr/lib/common/parser.y			\
-	usr/lib/common/lexer.l
+	usr/sbin/pkcsslotd/socket_server.c
+
+nodist_usr_sbin_pkcsslotd_pkcsslotd_SOURCES = \
+	usr/lib/common/parser.h usr/lib/common/parser.c	usr/lib/common/lexer.c
+usr/sbin/pkcsslotd/slotmgr.$(OBJEXT): usr/lib/common/parser.h
