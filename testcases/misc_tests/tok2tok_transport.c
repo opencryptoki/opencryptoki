060b315332df81c2269b928f1d516f0b6f48d77b
Ingo Franzki <ifranzki@linux.ibm.com>
2021-02-23 17:32:06 +0100

    TESTCASE: tok2tok_transport: Enhance error handling
    EP11 may not support unwrap of an EC private key with CKM_DES3_CBC,
    and returns different return codes dependent on the firmware level.
    Detect all this errors and skip the testcase in case of error.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- testcases/misc_tests/tok2tok_transport.c
+++ testcases/misc_tests/tok2tok_transport.c
@@ -805,9 +805,10 @@ CK_RV do_wrap_key_test(struct wrapped_mech_info *tsuite,
                 goto testcase_cleanup;
             }
         }
-        if (rc == CKR_MECHANISM_INVALID && is_ep11_token(slot_id2) &&
-            wrap_mech->mechanism == CKM_DES3_CBC) {
-            testcase_skip("EP11 does not support unwrap with DES3 CBC");
+        if ((rc == CKR_MECHANISM_INVALID || rc == CKR_KEY_TYPE_INCONSISTENT) &&
+            is_ep11_token(slot_id2) && wrap_mech->mechanism == CKM_DES3_CBC &&
+            key_type == CKK_EC) {
+            testcase_skip("EP11 does not support unwrap of EC keys with DES3 CBC");
             rc = CKR_OK;
             goto testcase_cleanup;
         }
