f50affe3c5e6f761a4bd0a94b02ebf15c7b89f62
Ingo Franzki <ifranzki@linux.ibm.com>
2021-07-26 17:04:35 +0200

    CI: Perform cleanup of left over EP11 sessions
    Allow to cleanup left over EP11 sessions for CI runs via
    'make ci-cleanup'. This is to be called by the CI after
    'make ci-installcheck'.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- testcases/testcases.mk
+++ testcases/testcases.mk
@@ -12,9 +12,9 @@ include testcases/misc_tests/misc_tests.mk
 include testcases/pkcs11/pkcs11.mk
 include testcases/build/build.mk
 
-noinst_SCRIPTS += testcases/ock_tests.sh testcases/init_token.sh testcases/init_vhsm.exp
-CLEANFILES += testcases/ock_tests.sh testcases/init_token.sh testcases/init_vhsm.exp
-EXTRA_DIST += testcases/ock_tests.sh.in testcases/init_token.sh.in testcases/init_vhsm.exp.in
+noinst_SCRIPTS += testcases/ock_tests.sh testcases/init_token.sh testcases/init_vhsm.exp testcases/cleanup_vhsm.exp
+CLEANFILES += testcases/ock_tests.sh testcases/init_token.sh testcases/init_vhsm.exp testcases/cleanup_vhsm.exp
+EXTRA_DIST += testcases/ock_tests.sh.in testcases/init_token.sh.in testcases/init_vhsm.exp.in testcases/cleanup_vhsm.exp.in
 
 testcases/ock_tests.sh: testcases/ock_tests.sh.in
 	@SED@	-e s!\@sysconfdir\@!"@sysconfdir@"!g			\
@@ -31,6 +31,13 @@ testcases/init_token.sh: testcases/init_token.sh.in
 	mv $@-t $@
 
 testcases/init_vhsm.exp: testcases/init_vhsm.exp.in
+	@SED@	-e s!\@localstatedir\@!"@localstatedir@"!g		\
+		-e s!\@sbindir\@!"@sbindir@"!g				\
+		-e s!\@libdir\@!"@libdir@"!g < $< > $@-t
+	@CHMOD@ a+x $@-t
+	mv $@-t $@
+
+testcases/cleanup_vhsm.exp: testcases/cleanup_vhsm.exp.in
 	@SED@	-e s!\@localstatedir\@!"@localstatedir@"!g		\
 		-e s!\@sbindir\@!"@sbindir@"!g				\
 		-e s!\@libdir\@!"@libdir@"!g < $< > $@-t
