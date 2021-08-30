88d110daef508d6ed45ec661089ac03a40f881bc
Juergen Christ <jchrist@linux.ibm.com>
2021-08-30 16:39:33 +0200

    [COMMON,ICSF] Fix out-of-sourcetree builds.
    When building outside the source tree, generated files should not be generated
    inside the source tree, but in the separate build tree.  Unfortunately, the
    ylwrap utility from the autotools does not correctly do that when called from
    autotools generic prefix rule for make.  Write explicit make rules to fix
    this.
    
    Signed-off-by: Juergen Christ <jchrist@linux.ibm.com>

--- usr/lib/common/common.mk
+++ usr/lib/common/common.mk
@@ -8,3 +8,9 @@ noinst_HEADERS +=							\
 	usr/lib/common/list.h usr/lib/common/tok_specific.h
 
 usr/lib/common/lexer.c: usr/lib/common/parser.h
+
+usr/lib/common/parser.c usr/lib/common/parser.output: usr/lib/common/parser.y
+	$(AM_V_YACC)$(am__skipyacc) $(SHELL) $(YLWRAP) $< parser.tab.c usr/lib/common/parser.c parser.tab.h usr/lib/common/parser.h parser.output usr/lib/common/parser.output -- $(YACCCOMPILE)
+
+usr/lib/common/lexer.c usr/lib/common/lexer.h: usr/lib/common/lexer.l
+	$(AM_V_LEX)$(am__skiplex) $(SHELL) $(YLWRAP) $< lex.yy.c usr/lib/common/lexer.c lex.yy.h usr/lib/common/lexer.h -- $(LEXCOMPILE)
