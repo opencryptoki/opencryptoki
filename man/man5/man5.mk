bd5aef0a55c18012c63320be4d74f00c4b3146a1
Juergen Christ <jchrist@linux.ibm.com>
2021-09-10 14:09:11 +0200

    MAN: Add config manpages
    Added man pages for policy and strength configuration.
    
    Signed-off-by: Juergen Christ <jchrist@linux.ibm.com>

--- man/man5/man5.mk
+++ man/man5/man5.mk
@@ -1,4 +1,4 @@
-man5_MANS += man/man5/opencryptoki.conf.5
+man5_MANS += man/man5/opencryptoki.conf.5 man/man5/strength.conf.5 man/man5/policy.conf.5
 
 if ENABLE_P11SAK
 man5_MANS += man/man5/p11sak_defined_attrs.conf.5
