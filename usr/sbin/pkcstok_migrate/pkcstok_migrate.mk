bc0f89118db6fd7521d526d83bed4826d03f376e
Ingo Franzki <ifranzki@linux.ibm.com>
2021-10-01 15:52:45 +0200

    pkcstok_migrate: Use new generic config file parser
    Make use of the generic config file parser from usr/lib/config/.
    
    Also ensure that pkcstok_migrate can only be run as root.
    The opencryptoki.conf file is usually owned by root:root, and
    thus can only be updated by root.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/sbin/pkcstok_migrate/pkcstok_migrate.mk
+++ usr/sbin/pkcstok_migrate/pkcstok_migrate.mk
@@ -14,16 +14,20 @@ usr_sbin_pkcstok_migrate_pkcstok_migrate_CFLAGS  =		\
 	-I${srcdir}/usr/lib/common				\
 	-I${srcdir}/usr/sbin/pkcstok_migrate			\
 	-I${srcdir}/usr/lib/api					\
-	-I${top_builddir}/usr/lib/api
+	-I${top_builddir}/usr/lib/api				\
+	-I${srcdir}/usr/lib/config				\
+	-I${top_builddir}/usr/lib/config
 
 usr_sbin_pkcstok_migrate_pkcstok_migrate_SOURCES =		\
-	usr/lib/common/p11util.c 			\
-	usr/lib/common/sw_crypt.c			\
-	usr/lib/common/trace.c 				\
-	usr/lib/common/pkcs_utils.c			\
-	usr/sbin/pkcstok_migrate/pkcstok_migrate.c	\
-	usr/lib/common/parser.y				\
-	usr/lib/common/lexer.l
+	usr/lib/common/p11util.c 				\
+	usr/lib/common/sw_crypt.c				\
+	usr/lib/common/trace.c 					\
+	usr/lib/common/pkcs_utils.c				\
+	usr/sbin/pkcstok_migrate/pkcstok_migrate.c		\
+	usr/lib/config/configuration.c				\
+	usr/lib/config/cfgparse.y 				\
+	usr/lib/config/cfglex.l
 
-nodist_usr_sbin_pkcstok_migrate_pkcstok_migrate_SOURCES =	\
+nodist_usr_sbin_pkcstok_migrate_pkcstok_migrate_SOURCES = \
 	usr/lib/api/mechtable.c
+usr/sbin/pkcstok_migrate/pkcstok_migrate.$(OBJEXT): usr/lib/config/cfgparse.h
