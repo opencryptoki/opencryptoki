82c13dedf5aaee95eddbad39428835d1519ad375
Ingo Franzki <ifranzki@linux.ibm.com>
2021-10-01 17:00:50 +0200

    common: remove old config file parser
    No longer used, remove.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/common/common.mk
+++ usr/lib/common/common.mk
@@ -6,11 +6,3 @@ noinst_HEADERS +=							\
 	usr/lib/common/sw_crypt.h usr/lib/common/defs.h			\
 	usr/lib/common/p11util.h usr/lib/common/event_client.h		\
 	usr/lib/common/list.h usr/lib/common/tok_specific.h
-
-usr/lib/common/lexer.c: usr/lib/common/parser.h
-
-usr/lib/common/parser.c usr/lib/common/parser.output: usr/lib/common/parser.y
-	$(AM_V_YACC)$(am__skipyacc) $(SHELL) $(YLWRAP) $< parser.tab.c usr/lib/common/parser.c parser.tab.h usr/lib/common/parser.h parser.output usr/lib/common/parser.output -- $(YACCCOMPILE)
-
-usr/lib/common/lexer.c usr/lib/common/lexer.h: usr/lib/common/lexer.l
-	$(AM_V_LEX)$(am__skiplex) $(SHELL) $(YLWRAP) $< lex.yy.c usr/lib/common/lexer.c lex.yy.h usr/lib/common/lexer.h -- $(LEXCOMPILE)
