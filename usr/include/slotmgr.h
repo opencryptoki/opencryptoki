08eaca02385e61759743a912f2f3bb7f262d78cd
Ingo Franzki <ifranzki@linux.ibm.com>
2022-01-20 11:43:25 +0100

    Change /var/run to /run/opencryptoki for pid file and sockets
    The usage of /var/run is deprecated. Also introduce a sub-directory under /run
    because we have more than one run-time file.
    
    Change was suggested by Mark Post in https://github.com/opencryptoki/opencryptoki/pull/403
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/include/slotmgr.h
+++ usr/include/slotmgr.h
@@ -30,10 +30,10 @@
 #define TOK_PATH  SBIN_PATH "/pkcsslotd"
 #define OCK_API_LOCK_FILE LOCKDIR_PATH "/LCK..APIlock"
 
-#define PROC_SOCKET_FILE_PATH "/var/run/pkcsslotd.socket"
-#define ADMIN_SOCKET_FILE_PATH "/var/run/pkcsslotd.admin.socket"
+#define PROC_SOCKET_FILE_PATH "/run/pkcsslotd.socket"
+#define ADMIN_SOCKET_FILE_PATH "/run/pkcsslotd.admin.socket"
 
-#define PID_FILE_PATH "/var/run/pkcsslotd.pid"
+#define PID_FILE_PATH "/run/pkcsslotd.pid"
 #define OCK_CONFIG OCK_CONFDIR "/opencryptoki.conf"
 
 #ifndef CK_BOOL
