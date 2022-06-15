58eaeeb3b4ba32a1dc3f4491a1f54ec6f2e37b07
Holger Dengler <dengler@linux.ibm.com>
2022-06-15 18:03:46 +0200

    p11sak: add PKCS#11 URI support
    Print the PKCS#11 URI in the list-key function. The PKCS#11 URI is only
    displayed in the long output (--long).
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

--- usr/sbin/p11sak/p11sak.mk
+++ usr/sbin/p11sak/p11sak.mk
@@ -11,6 +11,7 @@ usr_sbin_p11sak_p11sak_CFLAGS = -DLINUX -DPROGRAM_NAME=\"$(@)\"		\
 
 usr_sbin_p11sak_p11sak_SOURCES = usr/lib/common/p11util.c		\
 	usr/sbin/p11sak/p11sak.c usr/lib/config/configuration.c		\
+	usr/lib/common/uri.c usr/lib/common/buffer.c			\
 	usr/lib/config/cfgparse.y usr/lib/config/cfglex.l
 
 nodist_usr_sbin_p11sak_p11sak_SOURCES = usr/lib/api/mechtable.c
