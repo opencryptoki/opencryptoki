df0c180d1026d8d211daf84b7f5b237292810132
Ingo Franzki <ifranzki@linux.ibm.com>
2022-06-15 13:53:10 +0200

    common: Update SHM counters in object after loading
    When a token object is loaded from disk, also update the SHM
    counters in the object, so that it is not being reloaded in case
    the counters in SHM are not zero, i.e. if the object has been
    updated previously.
    
    Also refactor object_mgr_restore_obj_withSize() while at it
    to make it better readable.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/common/h_extern.h
+++ usr/lib/common/h_extern.h
@@ -2098,6 +2098,8 @@ CK_RV object_mgr_add_to_map(STDLL_TokData_t *tokdata,
 
 void object_mgr_add_to_shm(OBJECT *obj, LW_SHM_TYPE *shm);
 CK_RV object_mgr_del_from_shm(OBJECT *obj, LW_SHM_TYPE *shm);
+CK_RV object_mgr_get_shm_entry_for_obj(STDLL_TokData_t *tokdata, OBJECT *obj,
+                                       TOK_OBJ_ENTRY **entry);
 CK_RV object_mgr_check_shm(STDLL_TokData_t *tokdata, OBJECT *obj);
 CK_RV object_mgr_search_shm_for_obj(TOK_OBJ_ENTRY *list,
                                     CK_ULONG lo,
