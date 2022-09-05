644f3100d4596cebeddbd7c1c2dee7e9e250e0e7
Ingo Franzki <ifranzki@linux.ibm.com>
2022-09-05 13:40:16 +0200

    CCA: HSM MK change: finalize/cancel key objects
    The pkcshsm_mk_change tool sends the EVENT_TYPE_MK_CHANGE_FINALIZE
    or EVENT_TYPE_MK_CHANGE_CANCEL events to all tokens to finalize or cancel
    the master key change operation.
    
    For finalizing, the tokens move the old secure key blob from
    CKA_IBM_OPAQUE into CKA_IBM_OPAQUE_OLD, and move the re-enciphered secure
    key blob from CKA_IBM_OPAQUE_REENC into CKA_IBM_OPAQUE.
    
    For canceling, the tokens remove attributes CKA_IBM_OPAQUE_REENC and
    CKA_IBM_OPAQUE_OLD (if available) from the key object.
    
    Token objects are finalized/canceled by the pkcshsm_mk_change tool
    process only, but session objects are finalized/canceled by each OCK
    application themselves.
    
    After completion of the finalize/cancel processing, the tokens deactivate
    the current master key change operation.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/cca_stdll/cca_specific.c
+++ usr/lib/cca_stdll/cca_specific.c
@@ -8825,6 +8825,47 @@ static CK_RV cca_reencipher_cancel_objects_cb(STDLL_TokData_t *tokdata,
     return rc;
 }
 
+static CK_BBOOL cca_reencipher_finalize_is_new_mk_cb(STDLL_TokData_t *tokdata,
+                                                     OBJECT *obj,
+                                                     CK_BYTE *sec_key,
+                                                     CK_ULONG sec_key_len,
+                                                     void *cb_private)
+{
+    enum cca_token_type keytype;
+    unsigned int keybitsize;
+    const CK_BYTE *mkvp = NULL;
+    CK_BBOOL new_mk = FALSE;
+
+    UNUSED(cb_private);
+    UNUSED(obj);
+
+    if (analyse_cca_key_token(sec_key, sec_key_len,
+                              &keytype, &keybitsize, &mkvp) == FALSE)
+        return FALSE;
+
+    if (check_expected_mkvp(tokdata, keytype, mkvp, &new_mk) != CKR_OK)
+        return FALSE;
+
+    return new_mk;
+}
+
+static CK_RV cca_reencipher_finalize_objects_cb(STDLL_TokData_t *tokdata,
+                                                OBJECT *obj, void *cb_data)
+{
+    CK_RV rc;
+
+    UNUSED(cb_data);
+
+    rc = obj_mgr_reencipher_secure_key_finalize(tokdata, obj,
+                                    cca_reencipher_finalize_is_new_mk_cb, NULL);
+    if (rc == CKR_ATTRIBUTE_TYPE_INVALID)
+        rc = CKR_OK;
+    if (rc == CKR_OBJECT_HANDLE_INVALID) /* Obj was deleted by other proc */
+        rc = CKR_OK;
+
+    return rc;
+}
+
 /*
  * ATTENTION: This function is called in a separate thread. All actions
  * performed by this function must be thread save and use locks to lock
@@ -8954,6 +8995,122 @@ out:
  * performed by this function must be thread save and use locks to lock
  * against concurrent access by other threads.
  */
+static CK_RV cca_mk_change_finalize_cancel(STDLL_TokData_t *tokdata,
+                                           event_mk_change_data_t *op,
+                                           struct hsm_mk_change_info *info,
+                                           CK_BBOOL cancel)
+{
+    struct cca_private_data *cca_private = tokdata->private_data;
+    struct cca_mk_change_op *mk_change_op;
+    CK_RV rc = CKR_OK;
+    CK_BBOOL token_objs = FALSE;
+
+    UNUSED(info);
+
+    TRACE_DEVEL("%s %s MK change op: %s\n", __func__,
+                cancel ? "canceling" : "finalizing", op->id);
+
+    if ((op->flags & EVENT_MK_CHANGE_FLAGS_TOK_OBJS) != 0 ||
+        (op->flags & EVENT_MK_CHANGE_FLAGS_TOK_OBJS_FINAL) != 0) {
+        token_objs = TRUE;
+        /* The tool should have logged in a R/W USER session */
+        if (!session_mgr_user_session_exists(tokdata)) {
+            TRACE_ERROR("%s No user session exists\n", __func__);
+            OCK_SYSLOG(LOG_ERR, "Slot %lu: No user session exists\n",
+                       tokdata->slot_id);
+            return CKR_FUNCTION_FAILED;
+        }
+    }
+
+    if (pthread_rwlock_wrlock(&tokdata->hsm_mk_change_rwlock) != 0) {
+        TRACE_DEVEL("HSM-MK-change Write-Lock failed.\n");
+        OCK_SYSLOG(LOG_ERR, "Slot %lu: HSM-MK-change Write-Lock failed\n",
+                   tokdata->slot_id);
+        rc = CKR_CANT_LOCK;
+        goto out;
+    }
+
+    mk_change_op = cca_mk_change_find_op(tokdata, op->id, NULL);
+    if (mk_change_op == NULL)
+        goto out;
+
+    /*
+     * Finalize/cancel token objects.
+     * If flag EVENT_MK_CHANGE_FLAGS_TOK_OBJS_FINAL is on, only process such
+     * token objects that do have the CKA_IBM_OPAQUE_REENC attribute. Those
+     * Objects have been newly created by another process after the first token
+     * finalization/cancellation (flag EVENT_MK_CHANGE_FLAGS_TOK_OBJS) has
+     * been performed, and before all processes have deactivated the MK change
+     * operation. Thus, they were created with the re-enciphered secure key,
+     * and now need to be finalized/canceled.
+     */
+    rc = obj_mgr_iterate_key_objects(tokdata, !token_objs, token_objs,
+                                     token_objs && (op->flags &
+                                         EVENT_MK_CHANGE_FLAGS_TOK_OBJS_FINAL) ?
+                                             cca_reencipher_cancel_filter_cb :
+                                             cca_reencipher_filter_cb,
+                                     mk_change_op,
+                                     cancel ?
+                                         cca_reencipher_cancel_objects_cb :
+                                         cca_reencipher_finalize_objects_cb,
+                                     NULL, TRUE,
+                                     cancel ? "cancel" : "finalize");
+    if (rc != CKR_OK)
+        goto out;
+
+    /*
+     * Deactivate this MK change operation.
+     * For the pkcshsm_mk_change tool: Deactivate only after 2nd token object
+     * processing.
+     */
+    if ((token_objs == FALSE && op->tool_pid != tokdata->real_pid) ||
+        (op->flags & EVENT_MK_CHANGE_FLAGS_TOK_OBJS_FINAL) != 0) {
+
+        if (!cancel) {
+            /* From now on the new MKs are the expected one */
+            if (mk_change_op->new_sym_mkvp_set)
+                memcpy(cca_private->expected_sym_mkvp,
+                       mk_change_op->new_sym_mkvp, CCA_MKVP_LENGTH);
+            if (mk_change_op->new_aes_mkvp_set)
+                memcpy(cca_private->expected_aes_mkvp,
+                       mk_change_op->new_aes_mkvp, CCA_MKVP_LENGTH);
+            if (mk_change_op->new_apka_mkvp_set)
+                memcpy(cca_private->expected_apka_mkvp,
+                       mk_change_op->new_apka_mkvp, CCA_MKVP_LENGTH);
+        }
+
+        mk_change_op->mk_change_active = 0;
+        memset(mk_change_op->mk_change_op, 0,
+               sizeof(mk_change_op->mk_change_op));
+        if (mk_change_op->apqns != NULL)
+            free(mk_change_op->apqns);
+        mk_change_op->apqns = NULL;
+        mk_change_op->num_apqns = 0;
+
+        TRACE_DEVEL("%s %s MK change op: %s\n", __func__,
+                    cancel ? "canceled" : "finalized", op->id);
+        OCK_SYSLOG(LOG_INFO, "Slot %lu: Concurrent HSM master key change "
+                   "operation %s is %s\n",
+                   tokdata->slot_id, op->id, cancel ? "canceled" : "finalized");
+    }
+
+out:
+    if (pthread_rwlock_unlock(&tokdata->hsm_mk_change_rwlock) != 0) {
+        TRACE_DEVEL("HSM-MK-change Unlock failed.\n");
+        OCK_SYSLOG(LOG_ERR, "Slot %lu: HSM-MK-change unlock failed\n",
+                   tokdata->slot_id);
+        rc = CKR_CANT_LOCK;
+        goto out;
+    }
+
+    return rc;
+}
+
+/*
+ * ATTENTION: This function is called in a separate thread. All actions
+ * performed by this function must be thread save and use locks to lock
+ * against concurrent access by other threads.
+ */
 static CK_RV cca_handle_mk_change_event(STDLL_TokData_t *tokdata,
                                         unsigned int event_type,
                                         unsigned int event_flags,
@@ -9027,8 +9184,10 @@ static CK_RV cca_handle_mk_change_event(STDLL_TokData_t *tokdata,
         rc = cca_mk_change_cancel_query(tokdata, hdr, &info);
         break;
     case EVENT_TYPE_MK_CHANGE_FINALIZE:
+        rc = cca_mk_change_finalize_cancel(tokdata, hdr, &info, FALSE);
+        break;
     case EVENT_TYPE_MK_CHANGE_CANCEL:
-        rc = CKR_OK;
+        rc = cca_mk_change_finalize_cancel(tokdata, hdr, &info, TRUE);
         break;
     default:
         rc = CKR_FUNCTION_NOT_SUPPORTED;
