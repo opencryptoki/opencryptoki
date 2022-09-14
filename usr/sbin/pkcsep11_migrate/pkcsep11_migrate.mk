f8b097302a20c61b733712fbb9ae9fa1d75721c2
Holger Dengler <dengler@linux.ibm.com>
2022-09-14 15:51:25 +0200

    pkcsep11_migrate: migrate to common pin handling
    Use the new common implementation for user-pin handling.
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

--- usr/sbin/pkcsep11_migrate/pkcsep11_migrate.mk
+++ usr/sbin/pkcsep11_migrate/pkcsep11_migrate.mk
@@ -1,6 +1,6 @@
 sbin_PROGRAMS += usr/sbin/pkcsep11_migrate/pkcsep11_migrate
 
-usr_sbin_pkcsep11_migrate_pkcsep11_migrate_LDFLAGS = -lc -ldl -lpthread
+usr_sbin_pkcsep11_migrate_pkcsep11_migrate_LDFLAGS = -lc -ldl -lpthread -lcrypto
 
 usr_sbin_pkcsep11_migrate_pkcsep11_migrate_CFLAGS = -DLINUX		\
 	-DPROGRAM_NAME=\"$(@)\" -I${srcdir}/usr/include			\
@@ -9,7 +9,7 @@ usr_sbin_pkcsep11_migrate_pkcsep11_migrate_CFLAGS = -DLINUX		\
 	-I${top_builddir}/usr/lib/api
 
 usr_sbin_pkcsep11_migrate_pkcsep11_migrate_SOURCES =			\
-	usr/lib/common/p11util.c					\
+	usr/lib/common/p11util.c usr/lib/common/pin_prompt.c		\
 	usr/sbin/pkcsep11_migrate/pkcsep11_migrate.c
 
 nodist_usr_sbin_pkcsep11_migrate_pkcsep11_migrate_SOURCES =		\
