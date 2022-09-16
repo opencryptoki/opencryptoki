4d32537336120b3f6345791984e234c06e09ef43
Ingo Franzki <ifranzki@linux.ibm.com>
2022-09-16 11:33:58 +0200

    common Remove support for SKIPJACK key type
    No token is supporting SKIPJACK key type anyway, so remove all
    common code related to SKIPJACK key type as well.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/common/template.c
+++ usr/lib/common/template.c
@@ -196,8 +196,6 @@ CK_RV template_add_default_attributes(TEMPLATE *tmpl, TEMPLATE *basetmpl,
             return des2_set_default_attributes(tmpl, mode);
         case CKK_DES3:
             return des3_set_default_attributes(tmpl, mode);
-        case CKK_SKIPJACK:
-            return skipjack_set_default_attributes(tmpl, mode);
         case CKK_BATON:
             return baton_set_default_attributes(tmpl, mode);
         case CKK_JUNIPER:
@@ -447,8 +445,6 @@ CK_RV template_check_required_attributes(TEMPLATE *tmpl, CK_ULONG class,
             return des2_check_required_attributes(tmpl, mode);
         case CKK_DES3:
             return des3_check_required_attributes(tmpl, mode);
-        case CKK_SKIPJACK:
-            return skipjack_check_required_attributes(tmpl, mode);
         case CKK_BATON:
             return baton_check_required_attributes(tmpl, mode);
         case CKK_JUNIPER:
@@ -1684,8 +1680,6 @@ CK_RV template_validate_attribute(STDLL_TokData_t *tokdata, TEMPLATE *tmpl,
             return des2_validate_attribute(tokdata, tmpl, attr, mode);
         case CKK_DES3:
             return des3_validate_attribute(tokdata, tmpl, attr, mode);
-        case CKK_SKIPJACK:
-            return skipjack_validate_attribute(tokdata, tmpl, attr, mode);
         case CKK_BATON:
             return baton_validate_attribute(tokdata, tmpl, attr, mode);
         case CKK_JUNIPER:
