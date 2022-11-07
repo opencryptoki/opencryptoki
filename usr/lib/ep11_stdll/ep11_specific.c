opencryptoki/opencryptoki

commit 638fa126c8cb28ff7daf2bb383a0461c8f9fe6f8

EP11: Supply CKA_PUBLIC_KEY_INFO with CKM_IBM_BTC_DERIVE of public key
When deriving a public EC key with the CKM_IBM_BTC_DERIVE mechanism,
also supply the SPKI in the CKA_PUBLIC_KEY_INFO attribute.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/ep11_stdll/ep11_specific.c
@@ -4988,6 +4988,9 @@ CK_RV ep11tok_derive_key(STDLL_TokData_t * tokdata, SESSION * session,
     CK_MECHANISM_PTR mech_orig = mech;
     CK_ATTRIBUTE *ec_params;
     CK_IBM_BTC_DERIVE_PARAMS *btc_params = NULL;
+    CK_BYTE *spki = NULL;
+    CK_ULONG spki_length = 0;
+    CK_ATTRIBUTE *spki_attr = NULL;
 
     memset(newblob, 0, sizeof(newblob));
 
@@ -5328,6 +5331,29 @@ CK_RV ep11tok_derive_key(STDLL_TokData_t * tokdata, SESSION * session,
                         __func__, rc);
             goto error;
         }
+
+        /* Extract the SPKI and add CKA_PUBLIC_KEY_INFO to key */
+        rc = publ_key_get_spki(key_obj->template, ktype, FALSE,
+                               &spki, &spki_length);
+        if (rc != CKR_OK) {
+            TRACE_DEVEL("publ_key_get_spki failed\n");
+            goto error;
+        }
+
+        rc = build_attribute(CKA_PUBLIC_KEY_INFO, spki, spki_length, &spki_attr);
+        if (rc != CKR_OK) {
+            TRACE_ERROR("%s build_attribute failed with rc=0x%lx\n",
+                        __func__, rc);
+            goto error;
+        }
+
+        rc = template_update_attribute(key_obj->template, spki_attr);
+        if (rc != CKR_OK) {
+            TRACE_ERROR("%s template_update_attribute failed with "
+                        "rc=0x%lx\n", __func__, rc);
+            goto error;
+        }
+        spki_attr = NULL;
     }
 
     if (class == CKO_SECRET_KEY || class == CKO_PRIVATE_KEY) {
@@ -5380,6 +5406,8 @@ error:
         free(opaque_attr);
     if (chk_attr != NULL)
         free(chk_attr);
+    if (spki_attr != NULL)
+        free(spki_attr);
     if (new_attrs)
         free_attribute_array(new_attrs, new_attrs_len);
     if (new_attrs1)

commit ce269d4113b38642e0b4ba420e405ab7eb37f15b

EP11: Fix memory leak introduced with recent commit
Function publ_key_get_spki() allocates the SPKI buffer, so it must be freed
afterwards.

Fixes: 638fa126c8cb28ff7daf2bb383a0461c8f9fe6f8

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/ep11_stdll/ep11_specific.c
@@ -5451,6 +5451,8 @@ error:
         free(chk_attr);
     if (spki_attr != NULL)
         free(spki_attr);
+    if (spki != NULL)
+        free(spki);
     if (new_attrs)
         free_attribute_array(new_attrs, new_attrs_len);
     if (new_attrs1)