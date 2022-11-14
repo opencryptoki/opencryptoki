dfec09194fab2bae7cf1f92a5e346f4362f39a3e
Ingo Franzki <ifranzki@linux.ibm.com>
2022-11-14 17:06:54 +0100

    TESTCASES: Fix testcase return code
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- testcases/pkcs11/getobjectsize.c
+++ testcases/pkcs11/getobjectsize.c
@@ -133,19 +133,19 @@ int main(int argc, char **argv)
     {
         CK_SESSION_HANDLE hsess = 0;
 
-        rc = funcs->C_GetFunctionStatus(hsess);
-        if (rc != CKR_FUNCTION_NOT_PARALLEL)
-            return rc;
+        rv = funcs->C_GetFunctionStatus(hsess);
+        if (rv != CKR_FUNCTION_NOT_PARALLEL)
+            return rv;
 
-        rc = funcs->C_CancelFunction(hsess);
-        if (rc != CKR_FUNCTION_NOT_PARALLEL)
-            return rc;
+        rv = funcs->C_CancelFunction(hsess);
+        if (rv != CKR_FUNCTION_NOT_PARALLEL)
+            return rv;
     }
 
-    rc = do_GetObjectSize();
+    rv = do_GetObjectSize();
 
     funcs->C_Finalize(NULL);
 
     /* make sure we return non-zero if rv is non-zero */
-    return ((rv == 0) || (rv % 256) ? (int)rv : -1);
+    return testcase_return(rv);
 }
