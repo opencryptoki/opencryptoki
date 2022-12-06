f607211dd9378841859560a843b557204957d2c2
Ingo Franzki <ifranzki@linux.ibm.com>
2022-12-06 15:03:33 +0100

    p11sak: Generic options and argument parsing
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/sbin/p11sak/p11sak.mk
+++ usr/sbin/p11sak/p11sak.mk
@@ -5,17 +5,8 @@ usr_sbin_p11sak_p11sak_LDFLAGS = -ldl -lcrypto
 
 usr_sbin_p11sak_p11sak_CFLAGS = -DLINUX -DPROGRAM_NAME=\"$(@)\"		\
 	-I${srcdir}/usr/include -I${srcdir}/usr/lib/common		\
-	-I${srcdir}/usr/sbin/p11sak -I${srcdir}/usr/lib/config		\
-	-I${top_builddir}/usr/lib/config -I${top_builddir}/usr/lib/api	\
-	-I${srcdir}/usr/lib/api
+	-I${srcdir}/usr/sbin/p11sak
 
 usr_sbin_p11sak_p11sak_SOURCES = usr/lib/common/p11util.c		\
-	usr/sbin/p11sak/p11sak.c usr/lib/config/configuration.c		\
-	usr/lib/common/uri.c usr/lib/common/buffer.c			\
-	usr/lib/common/pin_prompt.c					\
-	usr/lib/config/cfgparse.y usr/lib/config/cfglex.l
-
-nodist_usr_sbin_p11sak_p11sak_SOURCES = usr/lib/api/mechtable.c
-
-usr/sbin/p11sak/p11sak.$(OBJEXT): usr/lib/config/cfgparse.h
+	usr/sbin/p11sak/p11sak.c
 
