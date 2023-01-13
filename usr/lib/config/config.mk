0b9895b2c7e2caf83cc6ed9fb77aabb1341bab82
Ingo Franzki <ifranzki@linux.ibm.com>
2023-01-13 09:56:56 +0100

    config: Create target directory for generated sources
    For out-of-tree builds, the target directory usr/lib/config may not exist
    in the build directory. Create it before running lex/yacc.
    
    Tested-by: Marc Hartmayer <marc@linux.ibm.com>
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/config/config.mk
+++ usr/lib/config/config.mk
@@ -8,10 +8,10 @@ usr/lib/config/cfglex.$(OBJEXT): usr/lib/config/cfgparse.h
 usr/lib/config/cfgparse.$(OBJEXT): usr/lib/config/cfglex.h
 
 usr/lib/config/cfgparse.c: usr/lib/config/cfgparse.y
-	$(AM_V_YACC)$(am__skipyacc) $(SHELL) $(YLWRAP) $< cfgparse.tab.c usr/lib/config/cfgparse.c cfgparse.tab.h usr/lib/config/cfgparse.h cfgparse.output usr/lib/config/cfgparse.output -- $(YACCCOMPILE)
+	$(AM_V_YACC)$(MKDIR_P) usr/lib/config && $(am__skipyacc) $(SHELL) $(YLWRAP) $< cfgparse.tab.c usr/lib/config/cfgparse.c cfgparse.tab.h usr/lib/config/cfgparse.h cfgparse.output usr/lib/config/cfgparse.output -- $(YACCCOMPILE)
 
 usr/lib/config/cfglex.c: usr/lib/config/cfglex.l
-	$(AM_V_LEX)$(am__skiplex) $(SHELL) $(YLWRAP) $< lex.config.c usr/lib/config/cfglex.c cfglex.h usr/lib/config/cfglex.h -- $(LEXCOMPILE)
+	$(AM_V_LEX)$(MKDIR_P) usr/lib/config && $(am__skiplex) $(SHELL) $(YLWRAP) $< lex.config.c usr/lib/config/cfglex.c cfglex.h usr/lib/config/cfglex.h -- $(LEXCOMPILE)
 
 CLEANFILES += usr/lib/config/cfglex.c usr/lib/config/cfglex.h	\
 usr/lib/config/cfgparse.c usr/lib/config/cfgparse.h		\
