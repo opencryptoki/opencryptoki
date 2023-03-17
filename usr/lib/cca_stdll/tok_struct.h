2b0a4a8e06c710bc3907c81542e0f22f9d7a2b63
Joerg Schmidbauer <jschmidb@de.ibm.com>
2023-03-17 17:24:12 +0100

    CCA pkey option: token_specific_set_attrs_for_new_object
    This token specific function is called whenever a new object is
    created. It currently sets attribute CKA_IBM_PROTKEY_EXTRACTABLE
    according to the PKEY_MODE token option, but may also be used
    for other token options and attrs in future.
    
    Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

--- usr/lib/cca_stdll/tok_struct.h
+++ usr/lib/cca_stdll/tok_struct.h
@@ -130,7 +130,7 @@ token_spec_t token_specific = {
     &token_specific_key_unwrap,
     &token_specific_reencrypt_single,
     NULL,                       // set_attribute_values
-    NULL,                       // set_attrs_for_new_object
+    &token_specific_set_attrs_for_new_object,
     &token_specific_handle_event,
 };
 
