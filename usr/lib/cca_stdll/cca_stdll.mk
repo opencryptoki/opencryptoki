7cc3034f4e4c3289f69074c08e7794a6c3e1be18
Joerg Schmidbauer <jschmidb@de.ibm.com>
2023-03-21 15:51:01 +0100

    CCA pkey option: handle non-s390x build envs
    Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

--- usr/lib/cca_stdll/cca_stdll.mk
+++ usr/lib/cca_stdll/cca_stdll.mk
@@ -44,8 +44,12 @@ opencryptoki_stdll_libpkcs11_cca_la_SOURCES = usr/lib/common/asn1.c	\
 	usr/lib/api/policyhelper.c usr/lib/config/configuration.c	\
 	usr/lib/config/cfgparse.y usr/lib/config/cfglex.l		\
 	usr/lib/common/mech_openssl.c usr/lib/common/pqc_supported.c	\
-	usr/lib/hsm_mk_change/hsm_mk_change.c                           \
-	usr/lib/common/pkey_utils.c	
+	usr/lib/hsm_mk_change/hsm_mk_change.c
+
+if !NO_PKEY
+opencryptoki_stdll_libpkcs11_cca_la_SOURCES +=				\
+	usr/lib/common/pkey_utils.c
+endif	
 
 if ENABLE_LOCKS
 opencryptoki_stdll_libpkcs11_cca_la_SOURCES +=				\
