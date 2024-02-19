23352af05a6b5f189d081422585a847f34ace845
Akash Kulhalli <akash.kulhalli@ibm.com>
2024-02-19 12:35:04 +0530

    man: Configure man page only if corresponding feature is enabled
    This extends from the changes made for autoconf. The man pages for pkcsicsf
    and pkcscca were both being configured regardless of whether those features
    were enabled during configuration. This is now fixed, so they will only be
    configured if their corresponding features are enabled.
    
    Additionally, the CLEANFILES variable is now deleted from each of the man page
    makefiles, because it is already tracked by automake for the `distclean` target.
    
    Adding them to the `clean` target causes required files to be cleaned as well,
    breaking the build.
    
    Signed-off-by: Akash Kulhalli <akash.kulhalli@ibm.com>

--- man/man7/man7.mk
+++ man/man7/man7.mk
@@ -1,4 +1,3 @@
 man7_MANS += man/man7/opencryptoki.7
 
 EXTRA_DIST += $(man7_MANS)
-CLEANFILES += man/man7/*.7
--- man/man8/man8.mk
+++ man/man8/man8.mk
@@ -1,4 +1,3 @@
 man8_MANS += man/man8/pkcsslotd.8
 
 EXTRA_DIST += $(man8_MANS)
-CLEANFILES += man/man8/*.8
