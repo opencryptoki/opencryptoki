fc743d366650a57967df18ca335e9927ad8e957e
Akash Kulhalli <akash.kulhalli@ibm.com>
2024-02-28 11:13:44 +0530

    p11sak: Adapt behaviour on AIX
    To handle missing qsort_r on AIX, some functions have been updated to
    externally juggle the opaque data object that qsort_r takes as a parameter.
    This is done via a static object that acts as a placeholder for this opaque
    object across function calls.
    
    Special notes:
    glibc returns a special pointer value when the input malloc size is zero. This
    value can be safely passed to `free`, but this address must not be dereferenced
    by the application itself.
    
    POSIX does not mandate a specific behaviour in case of a zero-size malloc call,
    it is left to the implementation to handle this case. AIX returns a `NULL`
    without setting `errno` in this case. POSIX-compliant applications must be able
    to handle this behaviour correctly.
    
    Not handling this causes issues on AIX, since it assumes the malloc call has
    failed for optional attributes (where the value size is 0). With this fix,
    the `--id/-i` attribute is no longer a mandatory flag for p11sak anymore.
    
    Signed-off-by: Akash Kulhalli <akash.kulhalli@ibm.com>

--- usr/sbin/p11sak/p11sak.mk
+++ usr/sbin/p11sak/p11sak.mk
@@ -5,17 +5,30 @@ EXTRA_DIST += usr/sbin/p11sak/p11sak_defined_attrs.conf
 
 usr_sbin_p11sak_p11sak_LDFLAGS = -ldl -lcrypto
 
-usr_sbin_p11sak_p11sak_CFLAGS = -DLINUX -DPROGRAM_NAME=\"$(@)\"		\
+if AIX
+usr_sbin_p11sak_p11sak_LDFLAGS += -Wl,-blibpath:$(libdir)/opencryptoki:$(libdir)/opencryptoki/stdll:/usr/lib:/usr/lib64
+endif
+
+usr_sbin_p11sak_p11sak_CFLAGS = -DPROGRAM_NAME=\"$(@)\"		\
 	-I${srcdir}/usr/include -I${srcdir}/usr/lib/common		\
 	-I${srcdir}/usr/sbin/p11sak -I${srcdir}/usr/lib/config		\
 	-I${top_builddir}/usr/lib/config -I${top_builddir}/usr/lib/api	\
 	-I${srcdir}/usr/lib/api
+
+if !AIX
+usr_sbin_p11sak_p11sak_CFLAGS += -DLINUX
+endif
 
 usr_sbin_p11sak_p11sak_SOURCES = usr/lib/common/p11util.c		\
 	usr/sbin/p11sak/p11sak.c usr/lib/common/pin_prompt.c		\
 	usr/lib/config/configuration.c usr/lib/common/uri.c		\
 	usr/lib/common/buffer.c						\
 	usr/lib/config/cfgparse.y usr/lib/config/cfglex.l
+
+if AIX
+usr_sbin_p11sak_p11sak_SOURCES += usr/lib/common/aix/err.c \
+	usr/lib/common/aix/getopt_long.c
+endif
 
 nodist_usr_sbin_p11sak_p11sak_SOURCES = usr/lib/api/mechtable.c
 
