259fb864e1b83b31052f559836afc8e866a95a4d
Akash Kulhalli <akash.kulhalli@ibm.com>
2024-02-28 11:27:00 +0530

    pkcshsm_mk_change: Platform-aware patches
    More repeats, nothing too interesting to call out here.
    
    Only major change here is that there is no AIX-equivalent to ELIBACC, which is
    an error code that is supposed to represent an inability of the program to load
    a specific symbol from a shared library with `dlsym`. That has been replaced
    with a generic EINVAL code.
    
    Signed-off-by: Akash Kulhalli <akash.kulhalli@ibm.com>

--- usr/sbin/pkcshsm_mk_change/pkcshsm_mk_change.mk
+++ usr/sbin/pkcshsm_mk_change/pkcshsm_mk_change.mk
@@ -2,6 +2,10 @@ sbin_PROGRAMS += usr/sbin/pkcshsm_mk_change/pkcshsm_mk_change
 
 usr_sbin_pkcshsm_mk_change_pkcshsm_mk_change_LDFLAGS = -lcrypto -ldl -lrt
 
+if AIX
+usr_sbin_pkcshsm_mk_change_pkcshsm_mk_change_LDFLAGS += -lbsd -Wl,-blibpath:$(libdir)/opencryptoki:$(libdir)/opencryptoki/stdll:/usr/lib:/usr/lib64
+endif
+
 usr_sbin_pkcshsm_mk_change_pkcshsm_mk_change_CFLAGS  =	\
 	-DOCK_TOOL					\
 	-DSTDLL_NAME=\"pkcshsm_mk_change\"		\
@@ -18,3 +22,8 @@ usr_sbin_pkcshsm_mk_change_pkcshsm_mk_change_SOURCES =	\
 	usr/lib/common/event_client.c			\
 	usr/lib/common/pin_prompt.c			\
 	usr/lib/hsm_mk_change/hsm_mk_change.c
+
+if AIX
+usr_sbin_pkcshsm_mk_change_pkcshsm_mk_change_SOURCES += \
+	usr/lib/common/aix/err.c usr/lib/common/aix/getopt_long.c
+endif
