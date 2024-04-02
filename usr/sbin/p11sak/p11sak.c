380a4a27cce84abcb1b46fe9200e9d826d4fcb88
Ingo Franzki <ifranzki@linux.ibm.com>
2024-04-02 08:49:59 +0200

    p11sak: Don't copy an empty CKA_ID with extract-pubkey
    Copying an empty (zero length) CKA_ID attribute does not make much sense,
    because the default CKA_ID value is empty anyway.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/sbin/p11sak/p11sak.c
+++ usr/sbin/p11sak/p11sak.c
@@ -9428,7 +9428,7 @@ static CK_RV p11sak_key_extract_pubkey(const struct p11sak_objtype *keytype,
     /* If no new ID is specified, try to use ID from certificate */
     if (opt_new_id == NULL) {
         rc = get_attribute(key, &id_attr);
-        if (rc == CKR_OK) {
+        if (rc == CKR_OK && id_attr.ulValueLen > 0) {
             rc = add_attribute(CKA_ID, id_attr.pValue, id_attr.ulValueLen,
                                &attrs, &num_attrs);
             if (rc != CKR_OK) {
