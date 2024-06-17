opencryptoki/opencryptoki

commit a34694e384c3db19c815f93098d89c122aa2e8dd

COMMON: Add support for SHA based key derivation
Add common code support for SHA based key derivation mechanisms like
CKM_SHAnnn_KEY_DERIVATION and CKM_SHA3_nnn_KEY_DERIVE.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/common/mech_sha.c
@@ -37,6 +37,7 @@
 #include "h_extern.h"
 #include "tok_spec_struct.h"
 #include "trace.h"
+#include "attributes.h"
 
 #include <openssl/evp.h>
 #include <openssl/crypto.h>
@@ -523,4 +524,247 @@ CK_RV ckm_generic_secret_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl)
     }
 
     return token_specific.t_generic_secret_key_gen(tokdata, tmpl);
+}
+
+CK_RV ckm_sha_derive(STDLL_TokData_t *tokdata, SESSION *sess,
+                     CK_MECHANISM *mech, OBJECT *base_key_obj,
+                     CK_ATTRIBUTE *pTemplate, CK_ULONG ulCount,
+                     CK_OBJECT_HANDLE *derived_key_handle)
+{
+    OBJECT *derived_key_obj = NULL;
+    CK_BYTE derived_key_value[MAX_SHA_HASH_SIZE];
+    DIGEST_CONTEXT ctx;
+    CK_MECHANISM digest_mech;
+    CK_ULONG hsize = 0, allowed_keysize = 0;
+    CK_ULONG derived_keytype = 0, derived_keylen = 0;
+    CK_ATTRIBUTE *base_key_value;
+    CK_ATTRIBUTE *value_attr, *vallen_attr = NULL;
+    CK_ULONG base_key_class, base_key_type;
+    CK_RV rc;
+
+    memset(&ctx, 0, sizeof(DIGEST_CONTEXT));
+    memset(&digest_mech, 0, sizeof(CK_MECHANISM));
+
+    rc = get_digest_from_mech(mech->mechanism, &digest_mech.mechanism);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("%s get_digest_from_mech failed\n", __func__);
+        return rc;
+    }
+
+    rc = get_sha_size(digest_mech.mechanism, &hsize);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("%s get_sha_size failed\n", __func__);
+        return CKR_FUNCTION_NOT_SUPPORTED;
+    }
+
+    rc = get_ulong_attribute_by_type(pTemplate, ulCount, CKA_VALUE_LEN,
+                                     &derived_keylen);
+    if (rc == CKR_ATTRIBUTE_VALUE_INVALID) {
+        TRACE_ERROR("%s\n", ock_err(ERR_ATTRIBUTE_VALUE_INVALID));
+        return rc;
+    }
+
+    rc = get_ulong_attribute_by_type(pTemplate, ulCount, CKA_KEY_TYPE,
+                                     &derived_keytype);
+    if (rc == CKR_ATTRIBUTE_VALUE_INVALID) {
+        TRACE_ERROR("%s\n", ock_err(ERR_ATTRIBUTE_VALUE_INVALID));
+        return rc;
+    }
+
+    /*
+     * According to PKCS#11:
+     * - no key length and no key type: CKK_GENERIC_SECRET of size <hsize>.
+     * - no key type, but length given: CKK_GENERIC_SECRET if specified length.
+     * - no key length but key type specified: key must have a well-defined
+     *                                         length, otherwise error.
+     * - key length and key type specified: length must be compatible with key
+     *                                      type, otherwise error.
+     * - key length must always be less or equal to the digest size.
+     */
+    if (derived_keytype == 0)
+        derived_keytype = CKK_GENERIC_SECRET;
+
+    switch (derived_keytype) {
+    case CKK_GENERIC_SECRET:
+        allowed_keysize = hsize;
+        break;
+    case CKK_DES:
+        allowed_keysize = DES_KEY_SIZE;
+        break;
+    case CKK_DES2:
+        allowed_keysize = 2 * DES_KEY_SIZE;
+        break;
+    case CKK_DES3:
+        allowed_keysize = 3 * DES_KEY_SIZE;
+        break;
+    case CKK_AES:
+        switch (derived_keylen) {
+        case AES_KEY_SIZE_128:
+        case AES_KEY_SIZE_192:
+        case AES_KEY_SIZE_256:
+            allowed_keysize = derived_keylen;
+            break;
+        default:
+            TRACE_ERROR("%s\n", ock_err(ERR_TEMPLATE_INCONSISTENT));
+            return CKR_TEMPLATE_INCONSISTENT;
+        }
+        break;
+    case CKK_AES_XTS:
+        switch (derived_keylen) {
+        case 2 * AES_KEY_SIZE_128:
+        case 2 * AES_KEY_SIZE_256:
+            allowed_keysize = derived_keylen;
+            break;
+        default:
+            TRACE_ERROR("%s\n", ock_err(ERR_TEMPLATE_INCONSISTENT));
+            return CKR_TEMPLATE_INCONSISTENT;
+        }
+        break;
+    default:
+        TRACE_ERROR("%s\n", ock_err(ERR_TEMPLATE_INCONSISTENT));
+        return CKR_TEMPLATE_INCONSISTENT;
+    }
+
+    if (derived_keylen == 0)
+        derived_keylen = allowed_keysize;
+
+    if (derived_keylen > hsize || derived_keylen > allowed_keysize) {
+        TRACE_ERROR("%s\n", ock_err(ERR_TEMPLATE_INCONSISTENT));
+        return CKR_TEMPLATE_INCONSISTENT;
+    }
+
+    if (!template_get_class(base_key_obj->template, &base_key_class,
+                            &base_key_type)) {
+        TRACE_ERROR("Could not find CKA_CLASS in the template\n");
+        return CKR_TEMPLATE_INCOMPLETE;
+    }
+
+    if (base_key_class != CKO_SECRET_KEY) {
+        TRACE_ERROR("Base key is not a secret key\n");
+        return CKR_KEY_TYPE_INCONSISTENT;
+    }
+
+    switch (base_key_type) {
+    case CKK_GENERIC_SECRET:
+    case CKK_DES:
+    case CKK_DES2:
+    case CKK_DES3:
+    case CKK_AES:
+    case CKK_AES_XTS:
+        break;
+    default:
+        TRACE_ERROR("Base key type is not supported\n");
+        return CKR_KEY_TYPE_INCONSISTENT;
+    }
+
+    /* Digest the base key to derive the derived key */
+    rc = template_attribute_get_non_empty(base_key_obj->template,
+                                          CKA_VALUE, &base_key_value);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("Could not find CKA_VALUE for the base key.\n");
+        return rc;
+    }
+
+    rc = digest_mgr_init(tokdata, sess, &ctx, &digest_mech, FALSE);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("%s\n", ock_err(ERR_FUNCTION_FAILED));
+        return rc;
+    }
+
+    rc = digest_mgr_digest(tokdata, sess, FALSE, &ctx,
+                           base_key_value->pValue, base_key_value->ulValueLen,
+                           derived_key_value, &hsize);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("digest_mgr_digest failed with rc = %s\n", ock_err(rc));
+        digest_mgr_cleanup(tokdata, sess, &ctx);
+        return rc;
+    }
+
+    rc = build_attribute(CKA_VALUE, derived_key_value, derived_keylen,
+                         &value_attr);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("Failed to build the attribute from CKA_VALUE, rc=%s.\n",
+                    ock_err(rc));
+        return rc;
+    }
+
+    switch (derived_keytype) {
+    case CKK_GENERIC_SECRET:
+    case CKK_AES:
+    case CKK_AES_XTS:
+        /* Supply CKA_VAUE_LEN since this is required for those key types */
+        rc = build_attribute(CKA_VALUE_LEN, (CK_BYTE*)&derived_keylen,
+                             sizeof(derived_keylen), &vallen_attr);
+        if (rc != CKR_OK) {
+            TRACE_ERROR("Failed to build the attribute from CKA_VALUE_LEN, "
+                        "rc=%s.\n", ock_err(rc));
+            goto end;
+        }
+        break;
+    case CKK_DES:
+        if (des_check_weak_key(derived_key_value)) {
+            TRACE_ERROR("Derived key is a weak DES key\n");
+            rc = CKR_FUNCTION_FAILED;
+            goto end;
+        }
+        break;
+    default:
+        break;
+    }
+
+    /* Create the derived key object and update the attributes */
+    rc = object_mgr_create_skel(tokdata, sess, pTemplate, ulCount, MODE_DERIVE,
+                                CKO_SECRET_KEY, derived_keytype,
+                                &derived_key_obj);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("Object Mgr create skeleton failed, rc=%s.\n", ock_err(rc));
+        goto end;
+    }
+
+    /* Update the template in the object with the new attributes */
+    rc = template_update_attribute(derived_key_obj->template, value_attr);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("template_update_attribute failed\n");
+        goto end;
+    }
+    value_attr = NULL;
+
+    if (vallen_attr != NULL) {
+        rc = template_update_attribute(derived_key_obj->template, vallen_attr);
+        if (rc != CKR_OK) {
+            TRACE_ERROR("template_update_attribute failed\n");
+            goto end;
+        }
+        vallen_attr = NULL;
+    }
+
+    rc = key_mgr_derive_always_sensitive_never_extractable_attrs(tokdata,
+                                                base_key_obj, derived_key_obj);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("key_mgr_derive_always_sensitive_never_extractable_attrs "
+                    "failed\n");
+        goto end;
+    }
+
+    rc = object_mgr_create_final(tokdata, sess, derived_key_obj,
+                                 derived_key_handle);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("Object Mgr create final failed, rc=%s.\n", ock_err(rc));
+        goto end;
+    }
+
+    rc = CKR_OK;
+
+end:
+    if (rc != CKR_OK && derived_key_obj != NULL) {
+        object_free(derived_key_obj);
+        derived_key_handle = CK_INVALID_HANDLE;
+    }
+
+    if (value_attr != NULL)
+        free(value_attr);
+    if (vallen_attr != NULL)
+        free(vallen_attr);
+
+    return rc;
 }

usr/lib/common/h_extern.h
@@ -1708,6 +1708,11 @@ CK_RV sha_hmac_verify(STDLL_TokData_t *tokdata,
 //adding the hmac secret key generation here
 CK_RV ckm_generic_secret_key_gen(STDLL_TokData_t *tokdata, TEMPLATE *tmpl);
 
+CK_RV ckm_sha_derive(STDLL_TokData_t *tokdata, SESSION *sess,
+                     CK_MECHANISM *mech, OBJECT *base_key_obj,
+                     CK_ATTRIBUTE *pTemplate, CK_ULONG ulCount,
+                     CK_OBJECT_HANDLE *derived_key_obj);
+
 #if !(NOMD2)
 // MD2 mechanisms
 //
@@ -2033,6 +2038,11 @@ CK_RV digest_mgr_digest_final(STDLL_TokData_t *tokdata,
 //
 CK_RV key_mgr_apply_always_sensitive_never_extractable_attrs(
                                     STDLL_TokData_t *tokdata, OBJECT *key_obj);
+
+CK_RV key_mgr_derive_always_sensitive_never_extractable_attrs(
+                                                    STDLL_TokData_t *tokdata,
+                                                    OBJECT *base_key_obj,
+                                                    OBJECT *derived_key_obj);
 
 CK_RV key_mgr_generate_key(STDLL_TokData_t *tokdata,
                            SESSION *sess,

usr/lib/common/key_mgr.c
@@ -102,6 +102,97 @@ error:
     return rc;
 }
 
+CK_RV key_mgr_derive_always_sensitive_never_extractable_attrs(
+                                                    STDLL_TokData_t *tokdata,
+                                                    OBJECT *base_key_obj,
+                                                    OBJECT *derived_key_obj)
+{
+    CK_ATTRIBUTE *always_sens_attr = NULL, *never_extract_attr = NULL;
+    CK_BBOOL flag;
+    CK_RV rc;
+
+    UNUSED(tokdata);
+
+    /*
+     * If base key has ALWAYS_SENSITIVE = FALSE, then new key does too
+     * otherwise, the value of CKA_ALWAYS_SENSITIVE = CKA_SENSITIVE
+     */
+    rc = template_attribute_get_bool(base_key_obj->template,
+                                     CKA_ALWAYS_SENSITIVE, &flag);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("Could not find CKA_ALWAYS_SENSITIVE in the template\n");
+        goto end;
+    }
+
+    if (flag == TRUE) {
+        rc = template_attribute_get_bool(derived_key_obj->template,
+                                         CKA_SENSITIVE, &flag);
+        if (rc != CKR_OK) {
+            TRACE_ERROR("Could not find CKA_SENSITIVE in the template\n");
+            goto end;
+        }
+    }
+
+    rc = build_attribute(CKA_ALWAYS_SENSITIVE, &flag, sizeof(CK_BBOOL),
+                         &always_sens_attr);
+    if (rc != CKR_OK) {
+        TRACE_DEVEL("Failed to build CKA_ALWAYS_SENSITIVE attribute.\n");
+        goto end;
+    }
+
+    /*
+     * If base key has NEVER_EXTRACTABLE = FALSE, the new key does too
+     * otherwise, the value of CKA_NEVER_EXTRACTABLE = !CKA_EXTRACTABLE
+     */
+    rc = template_attribute_get_bool(base_key_obj->template,
+                                     CKA_NEVER_EXTRACTABLE, &flag);
+    if (rc != CKR_OK) {
+        TRACE_DEVEL("Could not find CKA_NEVER_EXTRACTABLE in the template.\n");
+        goto end;
+    }
+
+    if (flag == TRUE) {
+        rc = template_attribute_get_bool(derived_key_obj->template,
+                                         CKA_EXTRACTABLE, &flag);
+        if (rc != CKR_OK) {
+            TRACE_DEVEL("Could not find CKA_EXTRACTABLE in the template.\n");
+            goto end;
+        }
+
+        flag = !flag;
+    }
+
+    rc = build_attribute(CKA_NEVER_EXTRACTABLE, &flag, sizeof(CK_BBOOL),
+                         &never_extract_attr);
+    if (rc != CKR_OK) {
+        TRACE_DEVEL("Failed to build CKA_NEVER_EXTRACTABLE attribute.\n");
+        goto end;
+    }
+
+    rc = template_update_attribute(derived_key_obj->template, always_sens_attr);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("template_update_attribute failed\n");
+        goto end;
+    }
+    always_sens_attr = NULL;
+
+    rc = template_update_attribute(derived_key_obj->template,
+                                   never_extract_attr);
+    if (rc != CKR_OK) {
+        TRACE_ERROR("template_update_attribute failed\n");
+        goto end;
+    }
+    never_extract_attr = NULL;
+
+end:
+    if (always_sens_attr != NULL)
+        free(always_sens_attr);
+    if (never_extract_attr != NULL)
+        free(never_extract_attr);
+
+    return rc;
+}
+
 //
 //
 CK_RV key_mgr_generate_key(STDLL_TokData_t *tokdata,
@@ -1571,6 +1662,23 @@ CK_RV key_mgr_derive_key(STDLL_TokData_t *tokdata,
         }
         rc = ecdh_pkcs_derive(tokdata, sess, mech, base_key_obj, new_attrs,
                               new_attr_count, derived_key);
+        break;
+    case CKM_SHA1_KEY_DERIVATION:
+    case CKM_SHA224_KEY_DERIVATION:
+    case CKM_SHA256_KEY_DERIVATION:
+    case CKM_SHA384_KEY_DERIVATION:
+    case CKM_SHA512_KEY_DERIVATION:
+    case CKM_SHA3_224_KEY_DERIVE:
+    case CKM_SHA3_256_KEY_DERIVE:
+    case CKM_SHA3_384_KEY_DERIVE:
+    case CKM_SHA3_512_KEY_DERIVE:
+        if (!derived_key) {
+            TRACE_ERROR("%s received bad argument(s)\n", __func__);
+            rc = CKR_FUNCTION_FAILED;
+            break;
+        }
+        rc = ckm_sha_derive(tokdata, sess, mech, base_key_obj, new_attrs,
+                            new_attr_count, derived_key);
         break;
     default:
         TRACE_ERROR("%s\n", ock_err(ERR_MECHANISM_INVALID));

usr/lib/common/mech_ssl3.c
@@ -1022,12 +1022,9 @@ CK_RV ssl3_master_key_derive(STDLL_TokData_t *tokdata,
     CK_ATTRIBUTE *attr = NULL;
     CK_ATTRIBUTE *value_attr = NULL;
     CK_ATTRIBUTE *value_len_attr = NULL;
-    CK_ATTRIBUTE *always_sens_attr = NULL;
-    CK_ATTRIBUTE *extract_attr = NULL;
     CK_BYTE *base_key_value = NULL;
     CK_BYTE key_data[48];
     CK_ULONG base_key_len;
-    CK_BBOOL flag;
     CK_OBJECT_CLASS class;
     CK_KEY_TYPE keytype;
     CK_ULONG value_len;
@@ -1166,61 +1163,12 @@ CK_RV ssl3_master_key_derive(STDLL_TokData_t *tokdata,
         TRACE_DEVEL("Failed to build CKA_VALUE_LEN attribute.\n");
         goto error;
     }
-    //
-    // now, adjust the CKA_ALWAYS_SENSITIVE and CKA_NEVER_EXTRACTABLE
-    // attributes based on the corresponding values from the base key
-    //
 
-    // if base key has ALWAYS_SENSITIVE = FALSE, then new key does too
-    // otherwise, the value of CKA_ALWAYS_SENSITIVE = CKA_SENSITIVE
-    //
-    rc = template_attribute_get_bool(base_key_obj->template,
-                                    CKA_ALWAYS_SENSITIVE, &flag);
+    rc = key_mgr_derive_always_sensitive_never_extractable_attrs(tokdata,
+                                                base_key_obj, derived_key_obj);
     if (rc != CKR_OK) {
-        TRACE_ERROR("Could not find CKA_ALWAYS_SENSITIVE in the template\n");
-        goto error;
-    }
-
-    if (flag == TRUE) {
-        rc = template_attribute_get_bool(derived_key_obj->template,
-                                         CKA_SENSITIVE, &flag);
-        if (rc != CKR_OK) {
-            TRACE_ERROR("Could not find CKA_SENSITIVE in the template\n");
-            goto error;
-        }
-    }
-
-    rc = build_attribute(CKA_ALWAYS_SENSITIVE, &flag, sizeof(CK_BBOOL),
-                         &always_sens_attr);
-    if (rc != CKR_OK) {
-        TRACE_DEVEL("Failed to build CKA_ALWAYS_SENSITIVE attribute.\n");
-        goto error;
-    }
-    // if base key has NEVER_EXTRACTABLE = FASE, the new key does too
-    // otherwise, the value of CKA_NEVER_EXTRACTABLE = !CKA_EXTRACTABLE
-    //
-    rc = template_attribute_get_bool(base_key_obj->template,
-                                     CKA_NEVER_EXTRACTABLE, &flag);
-    if (rc != CKR_OK) {
-        TRACE_DEVEL("Could not find CKA_NEVER_EXTRACTABLE in the template.\n");
-        goto error;
-    }
-
-    if (flag == TRUE) {
-        rc = template_attribute_get_bool(derived_key_obj->template,
-                                         CKA_EXTRACTABLE, &flag);
-        if (rc != CKR_OK) {
-            TRACE_DEVEL("Could not find CKA_EXTRACTABLE in the template.\n");
-            goto error;
-        }
-
-        flag = (~flag) & 0x1;
-    }
-
-    rc = build_attribute(CKA_NEVER_EXTRACTABLE, &flag, sizeof(CK_BBOOL),
-                         &extract_attr);
-    if (rc != CKR_OK) {
-        TRACE_DEVEL("Failed to build CKA_NEVER_EXTRACTABLE attribute.\n");
+        TRACE_ERROR("key_mgr_derive_always_sensitive_never_extractable_attrs "
+                    "failed\n");
         goto error;
     }
 
@@ -1236,18 +1184,6 @@ CK_RV ssl3_master_key_derive(STDLL_TokData_t *tokdata,
         goto error;
     }
     value_len_attr = NULL;
-    rc = template_update_attribute(derived_key_obj->template, always_sens_attr);
-    if (rc != CKR_OK) {
-        TRACE_ERROR("template_update_attribute failed\n");
-        goto error;
-    }
-    always_sens_attr = NULL;
-    rc = template_update_attribute(derived_key_obj->template, extract_attr);
-    if (rc != CKR_OK) {
-        TRACE_ERROR("template_update_attribute failed\n");
-        goto error;
-    }
-    extract_attr = NULL;
 
     // at this point, the derived key is fully constructed...assign an
     // object handle and store the key
@@ -1274,10 +1210,6 @@ error:
         free(value_attr);
     if (value_len_attr)
         free(value_len_attr);
-    if (always_sens_attr)
-        free(always_sens_attr);
-    if (extract_attr)
-        free(extract_attr);
     if (derived_key_obj)
         object_free(derived_key_obj);
 

usr/lib/common/utility_common.c
@@ -276,46 +276,55 @@ CK_RV get_digest_from_mech(CK_MECHANISM_TYPE mech, CK_MECHANISM_TYPE *digest)
     case CKM_ECDSA_SHA1:
     case CKM_SHA1_RSA_PKCS:
     case CKM_SHA1_RSA_PKCS_PSS:
+    case CKM_SHA1_KEY_DERIVATION:
         *digest = CKM_SHA_1;
         break;
     case CKM_ECDSA_SHA224:
     case CKM_SHA224_RSA_PKCS:
     case CKM_SHA224_RSA_PKCS_PSS:
+    case CKM_SHA224_KEY_DERIVATION:
         *digest = CKM_SHA224;
         break;
     case CKM_ECDSA_SHA256:
     case CKM_SHA256_RSA_PKCS:
     case CKM_SHA256_RSA_PKCS_PSS:
+    case CKM_SHA256_KEY_DERIVATION:
         *digest = CKM_SHA256;
         break;
     case CKM_ECDSA_SHA384:
     case CKM_SHA384_RSA_PKCS:
     case CKM_SHA384_RSA_PKCS_PSS:
+    case CKM_SHA384_KEY_DERIVATION:
         *digest = CKM_SHA384;
         break;
     case CKM_ECDSA_SHA512:
     case CKM_SHA512_RSA_PKCS:
     case CKM_SHA512_RSA_PKCS_PSS:
+    case CKM_SHA512_KEY_DERIVATION:
         *digest = CKM_SHA512;
         break;
     case CKM_ECDSA_SHA3_224:
     case CKM_SHA3_224_RSA_PKCS:
     case CKM_SHA3_224_RSA_PKCS_PSS:
+    case CKM_SHA3_224_KEY_DERIVE:
         *digest = CKM_SHA3_224;
         break;
     case CKM_ECDSA_SHA3_256:
     case CKM_SHA3_256_RSA_PKCS:
     case CKM_SHA3_256_RSA_PKCS_PSS:
+    case CKM_SHA3_256_KEY_DERIVE:
         *digest = CKM_SHA3_256;
         break;
     case CKM_ECDSA_SHA3_384:
     case CKM_SHA3_384_RSA_PKCS:
     case CKM_SHA3_384_RSA_PKCS_PSS:
+    case CKM_SHA3_384_KEY_DERIVE:
         *digest = CKM_SHA3_384;
         break;
     case CKM_ECDSA_SHA3_512:
     case CKM_SHA3_512_RSA_PKCS:
     case CKM_SHA3_512_RSA_PKCS_PSS:
+    case CKM_SHA3_512_KEY_DERIVE:
         *digest = CKM_SHA3_512;
         break;
     default:

commit 7ba5eb374b617f05b9e190dfc3b6d9c43ecac312

COMMON: Update statistics for SHA based key derivation mechanisms
When adding common code support for the SHA based key derivation mechanisms
it was forgotten to also increase the statistics counters for those
mechanisms on successful completion.

Fixes: a34694e384c3db19c815f93098d89c122aa2e8dd

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/common/mech_sha.c
@@ -753,6 +753,8 @@ CK_RV ckm_sha_derive(STDLL_TokData_t *tokdata, SESSION *sess,
         goto end;
     }
 
+    INC_COUNTER(tokdata, sess, mech, base_key_obj, POLICY_STRENGTH_IDX_0);
+
     rc = CKR_OK;
 
 end: