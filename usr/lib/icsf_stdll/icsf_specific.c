opencryptoki/opencryptoki

commit 259e9472e0bb8362e95ebcc9b6f6df2a008385c0

ICSF: Add support for SHA3 mechanisms
Add support for the SHA3 mechanisms like CKM_SHA3_nnn and CKM_SHA3_nnn_HMAC
to the ICSF token.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/icsf_stdll/icsf_specific.c
@@ -88,6 +88,14 @@ static const MECH_LIST_ELEMENT icsf_mech_list[] = {
     {CKM_SHA384_HMAC, {0, 0, CKF_HW | CKF_SIGN | CKF_VERIFY}},
     {CKM_SHA512, {0, 0, CKF_HW | CKF_DIGEST}},
     {CKM_SHA512_HMAC, {0, 0, CKF_HW | CKF_SIGN | CKF_VERIFY}},
+    {CKM_SHA3_224, {0, 0, CKF_HW | CKF_DIGEST}},
+    {CKM_SHA3_224_HMAC, {0, 0, CKF_HW | CKF_SIGN | CKF_VERIFY}},
+    {CKM_SHA3_256, {0, 0, CKF_HW | CKF_DIGEST}},
+    {CKM_SHA3_256_HMAC, {0, 0, CKF_HW | CKF_SIGN | CKF_VERIFY}},
+    {CKM_SHA3_384, {0, 0, CKF_HW | CKF_DIGEST}},
+    {CKM_SHA3_384_HMAC, {0, 0, CKF_HW | CKF_SIGN | CKF_VERIFY}},
+    {CKM_SHA3_512, {0, 0, CKF_HW | CKF_DIGEST}},
+    {CKM_SHA3_512_HMAC, {0, 0, CKF_HW | CKF_SIGN | CKF_VERIFY}},
     {CKM_MD5, {0, 0, CKF_DIGEST}},
     {CKM_MD5_HMAC, {0, 0, CKF_SIGN | CKF_VERIFY}},
     {CKM_AES_KEY_GEN, {16, 32, CKF_HW | CKF_GENERATE}},

usr/lib/icsf_stdll/icsf.c
@@ -1635,6 +1635,14 @@ static const char *get_algorithm_rule(CK_MECHANISM_PTR mech, int arg)
         return "SHA-384";
     case CKM_SHA512_HMAC:
         return "SHA-512";
+    case CKM_SHA3_224_HMAC:
+        return "SHA3-224";
+    case CKM_SHA3_256_HMAC:
+        return "SHA3-256";
+    case CKM_SHA3_384_HMAC:
+        return "SHA3-384";
+    case CKM_SHA3_512_HMAC:
+        return "SHA3-512";
     case CKM_MD5_HMAC:
         return "MD5";
     case CKM_SSL3_MD5_MAC:

commit 03691f5176aa967d8f871f0ec6c68a69beb15796

ICSF: Complete the support for SHA3-HMAC mechanisms
A couple of places are missing SHA3-HMAC support, fix this.

Note that HMAC-SHA3 is only supported by z/OS 2.5.0 or later.
It will fail with CKR_FUNCTION_FAILED if used with a z/OS version
that does not support it.

Fixes: 259e9472e0bb8362e95ebcc9b6f6df2a008385c0

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/icsf_stdll/icsf_specific.c
@@ -3987,6 +3987,14 @@ int get_signverify_len(CK_MECHANISM mech)
         return SHA384_HASH_SIZE;
     case CKM_SHA512_HMAC:
         return SHA512_HASH_SIZE;
+    case CKM_SHA3_224_HMAC:
+        return SHA3_224_HASH_SIZE;
+    case CKM_SHA3_256_HMAC:
+        return SHA3_256_HASH_SIZE;
+    case CKM_SHA3_384_HMAC:
+        return SHA3_384_HASH_SIZE;
+    case CKM_SHA3_512_HMAC:
+        return SHA3_512_HASH_SIZE;
     }
 
     return -1;
@@ -4048,6 +4056,10 @@ CK_RV icsftok_sign_init(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
         /* hmacs can do mulitpart and do not require a
          *  mechanism parameter.
          */
@@ -4226,6 +4238,10 @@ CK_RV icsftok_sign(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
     case CKM_SSL3_MD5_MAC:
     case CKM_SSL3_SHA1_MAC:
         if (length_only) {
@@ -4374,6 +4390,10 @@ CK_RV icsftok_sign_update(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
     case CKM_SSL3_MD5_MAC:
     case CKM_SSL3_SHA1_MAC:
         rc = icsf_hmac_sign(session_state->ld, &reason,
@@ -4535,6 +4555,10 @@ CK_RV icsftok_sign_final(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
     case CKM_SSL3_MD5_MAC:
     case CKM_SSL3_SHA1_MAC:
         if (length_only) {
@@ -4673,6 +4697,10 @@ CK_RV icsftok_verify_init(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
         /* hmacs can do mulitpart and do not require a
          *  mechanism parameter.
          */
@@ -4855,6 +4883,10 @@ CK_RV icsftok_verify(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
     case CKM_SSL3_MD5_MAC:
     case CKM_SSL3_SHA1_MAC:
         rc = icsf_hmac_verify(session_state->ld, &reason,
@@ -4974,6 +5006,10 @@ CK_RV icsftok_verify_update(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
     case CKM_SSL3_MD5_MAC:
     case CKM_SSL3_SHA1_MAC:
         rc = icsf_hmac_verify(session_state->ld, &reason,
@@ -5141,6 +5177,10 @@ CK_RV icsftok_verify_final(STDLL_TokData_t * tokdata,
     case CKM_SHA256_HMAC:
     case CKM_SHA384_HMAC:
     case CKM_SHA512_HMAC:
+    case CKM_SHA3_224_HMAC:
+    case CKM_SHA3_256_HMAC:
+    case CKM_SHA3_384_HMAC:
+    case CKM_SHA3_512_HMAC:
     case CKM_SSL3_MD5_MAC:
     case CKM_SSL3_SHA1_MAC:
         /* get the chain data */