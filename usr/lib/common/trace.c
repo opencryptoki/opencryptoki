702db697c6f99342e0cdd64fd63145becbc44e2b
Ingo Franzki <ifranzki@linux.ibm.com>
2024-08-27 11:28:48 +0200

    TRACE: Print the initial trace message for all trace levels
    Print the initial trace message as ERROR instead of as INFO, although this
    is really not an error. However, when a trace level lower than INFO is
    selected, then the initial trace message would not be written.
    
    The initial trace message contains information about the Opencryptoki
    version as well as the trace level selected. This information is always
    important, even for lower trace levels.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/common/trace.c
+++ usr/lib/common/trace.c
@@ -214,8 +214,8 @@ CK_RV trace_initialize(void)
     }
 
 #ifdef PACKAGE_VERSION
-    TRACE_INFO("**** OCK Trace level %d activated for OCK version %s ****\n",
-            trace.level, PACKAGE_VERSION);
+    TRACE_ERROR("**** OCK Trace level %d activated for OCK version %s ****\n",
+                trace.level, PACKAGE_VERSION);
 #endif
 
     return (CKR_OK);
