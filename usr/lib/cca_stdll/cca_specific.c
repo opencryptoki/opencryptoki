opencryptoki/opencryptoki

commit 1d87606113c8ccc673fdc70436da6a94c7b981db

CCA pkey option: handle concurrent MK change
Handle the situation where a concurrent master key change op is
active and the key is already encrypted with the new MK.

Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

usr/lib/cca_stdll/cca_specific.c
@@ -2856,12 +2856,20 @@ static CK_RV ccatok_pkey_skey2pkey(STDLL_TokData_t *tokdata,
     enum cca_token_type key_type;
     unsigned int token_keybitsize;
     const CK_BYTE *mkvp;
+    CK_BBOOL new_mk;
+    unsigned int num_retries = 0;
 
     /* Determine CCA key type */
     if (analyse_cca_key_token(skey_attr->pValue, skey_attr->ulValueLen,
                               &key_type, &token_keybitsize, &mkvp) != TRUE) {
         TRACE_ERROR("Invalid/unknown cca token, cannot get key type\n");
         ret = CKR_FUNCTION_FAILED;
+        goto done;
+    }
+
+    if (check_expected_mkvp(tokdata, key_type, mkvp, &new_mk) != CKR_OK) {
+        TRACE_ERROR("%s\n", ock_err(ERR_DEVICE_ERROR));
+        ret = CKR_DEVICE_ERROR;
         goto done;
     }
 
@@ -2874,8 +2882,19 @@ static CK_RV ccatok_pkey_skey2pkey(STDLL_TokData_t *tokdata,
     pkey_wrap_handler_data.pkey_buflen_p = &pkey_buflen;
     pkey_wrap_handler_data.keytype = key_type;
 
-    ret = cca_iterate_adapters(tokdata, ccatok_pkey_sec2prot,
-                               &pkey_wrap_handler_data);
+    while (num_retries < 3600) {
+        ret = cca_iterate_adapters(tokdata, ccatok_pkey_sec2prot,
+                                   &pkey_wrap_handler_data);
+        if (ret == CKR_OK && pkey_wrap_handler_data.wrap_was_successful)
+            break;
+
+        /* Retry the op if key encrypted with new MK and an MK change is active */
+        if (new_mk == CK_TRUE &&
+            cca_mk_change_find_op_by_keytype(tokdata, key_type) != NULL) {
+            sleep(1);
+            num_retries++;
+        }
+    }
 
     if (ret != CKR_OK || !pkey_wrap_handler_data.wrap_was_successful) {
         TRACE_ERROR("cca_iterate_adapters failed or no APQN could create the pkey.\n");

commit 80bf98b3271f1a45a645443c31b9f9c5ce72c831

CCA: Fix endless retry loop in cca_pkey_skey2pkey
In case the pkey ioctl fails, but no MK change is active, break the
loop also, otherwise we end up in an endless retry loop.

Fixes: 1d87606113c8ccc673fdc70436da6a94c7b981db

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/cca_stdll/cca_specific.c
@@ -2608,7 +2608,9 @@ static CK_RV ccatok_pkey_skey2pkey(STDLL_TokData_t *tokdata,
             cca_mk_change_find_op_by_keytype(tokdata, key_type) != NULL) {
             sleep(1);
             num_retries++;
+            continue;
         }
+        break;
     }
 
     if (ret != CKR_OK || !pkey_wrap_handler_data.wrap_was_successful) {