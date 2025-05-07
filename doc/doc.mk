4dda122536224387bcd0276916ce0285be71bbae
Ingo Franzki <ifranzki@linux.ibm.com>
2025-05-07 09:30:41 +0200

    MAKEFILE: Add certain files to EXTRA_DIST or doc_DATA
    Those files are referenced in the example RPM spec.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- doc/doc.mk
+++ doc/doc.mk
@@ -1,4 +1,4 @@
-doc_DATA = doc/strength-example.conf
+doc_DATA = doc/strength-example.conf doc/opencryptoki-howto.md doc/README.token_data
 nodist_doc_DATA = doc/policy-example.conf
 
 doc/policy-example.conf: tools/policyexamplegen
@@ -6,3 +6,16 @@ doc/policy-example.conf: tools/policyexamplegen
 
 EXTRA_DIST += $(doc_DATA)
 CLEANFILES += doc/policy-example.conf
+
+if ENABLE_CCATOK
+EXTRA_DIST += doc/README.cca_stdll
+endif
+if ENABLE_EP11TOK
+EXTRA_DIST += doc/README.ep11_stdll
+endif
+if ENABLE_ICSFTOK
+EXTRA_DIST += doc/README.icsf_stdll
+endif
+if ENABLE_TPMTOK
+EXTRA_DIST += doc/README.tpm_stdll
+endif
