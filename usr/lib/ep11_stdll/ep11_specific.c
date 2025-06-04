opencryptoki/opencryptoki

commit f24ae99bc150bb9556370bca3668670549c5fafe

EP11: Reject strict-session of VHSM mode in a Secure Execution guest
Strict-session of VHSM mode uses the m_Login() function to login the
respective EP11 sessions. In an Secure Execution for Linux guest only
m_LoginExtended() with XCP_LOGIN_ALG_F2021 is supported, and can be
correctly intercepted by the Ultravisor.

Reject the use of strict-session of VHSM mode when running in a Secure
Execution for Linux guest.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/ep11_stdll/ep11_specific.c
@@ -186,6 +186,8 @@ static CK_RV obj_opaque_2_blob(STDLL_TokData_t *tokdata, OBJECT *key_obj,
 static CK_RV obj_opaque_2_reenc_blob(STDLL_TokData_t *tokdata, OBJECT *key_obj,
                                      CK_BYTE **blob, size_t *blobsize);
 
+static CK_RV file_fgets(const char *fname, char *buf, size_t buflen);
+
 /* EP11 Firmware levels that contain the HMAC min/max keysize fix */
 static const CK_VERSION cex4p_hmac_fix = { .major = 4, .minor = 20 };
 static const CK_VERSION cex5p_hmac_fix = { .major = 6, .minor = 3 };
@@ -3209,6 +3211,21 @@ static CK_RV ep11tok_load_libica(STDLL_TokData_t *tokdata)
     return CKR_OK;
 }
 
+static CK_BBOOL ep11tok_is_sel_guest(void)
+{
+    char buf[20];
+    CK_RV rc;
+
+    if (access(SYSFS_SEL_GUEST, R_OK) != 0)
+        return FALSE;
+
+    rc = file_fgets(SYSFS_SEL_GUEST, buf, sizeof(buf));
+    if (rc != CKR_OK)
+        return FALSE;
+
+    return (buf[0] == '1');
+}
+
 CK_RV ep11tok_init(STDLL_TokData_t * tokdata, CK_SLOT_ID SlotNumber,
                    char *conf_name)
 {
@@ -3356,6 +3373,16 @@ CK_RV ep11tok_init(STDLL_TokData_t * tokdata, CK_SLOT_ID SlotNumber,
 #endif /* NO_PKEY */
 
     if (ep11_data->vhsm_mode || ep11_data->fips_session_mode) {
+        if (ep11tok_is_sel_guest()) {
+            TRACE_ERROR("Strict-session or VHSM mode is not supported in a "
+                        "Secure Execution for Linux guest\n");
+            OCK_SYSLOG(LOG_ERR, "Slot %lu: Strict-session or VHSM mode is "
+                       "not supported in a Secure Execution for Linux guest\n",
+                       tokdata->slot_id);
+            rc = CKR_FUNCTION_FAILED;
+            goto error;
+        }
+
         if (pthread_mutex_init(&ep11_data->session_mutex, NULL) != 0) {
             TRACE_ERROR("Initializing session lock failed.\n");
             rc = CKR_CANT_LOCK;

usr/lib/ep11_stdll/ep11_specific.h
@@ -343,6 +343,8 @@ typedef struct {
 #define REGEX_SUB_CARD_PATTERN  "[0-9a-fA-F]+\\.[0-9a-fA-F]+"
 #define MASK_EP11               0x04000000
 
+#define SYSFS_SEL_GUEST        "/sys/firmware/uv/prot_virt_guest"
+
 typedef struct {
     STDLL_TokData_t *tokdata;
     ep11_session_t *ep11_session;

commit 992cab9f9a84585a8cc802ceb35983815e547678

EP11: Version checks for strict-session or VHSM mode in a Secure Execution guest
In a Secure Execution guest strict-session or VHSM mode only works when
using an EP11 host library version 4.1 or later and when all APQNs configured
for the token support API ordinal 6 or later.

An EP11 host library version 4.1 or later translates a m_Login() call into
a m_LoginExtended() call with XCP_LOGIN_ALG_PRE_F2021 when the API ordinal
used is >= 6 and this can be correctly intercepted by the Ultravisor.

Only reject the use of strict-session or VHSM mode in a Secure Execution guest
if the requirements are not fulfilled.

Fixes: f24ae99bc150bb9556370bca3668670549c5fafe

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/ep11_stdll/ep11_specific.c
@@ -3373,12 +3373,18 @@ CK_RV ep11tok_init(STDLL_TokData_t * tokdata, CK_SLOT_ID SlotNumber,
 #endif /* NO_PKEY */
 
     if (ep11_data->vhsm_mode || ep11_data->fips_session_mode) {
-        if (ep11tok_is_sel_guest()) {
-            TRACE_ERROR("Strict-session or VHSM mode is not supported in a "
-                        "Secure Execution for Linux guest\n");
-            OCK_SYSLOG(LOG_ERR, "Slot %lu: Strict-session or VHSM mode is "
-                       "not supported in a Secure Execution for Linux guest\n",
-                       tokdata->slot_id);
+        if (ep11tok_is_sel_guest() && (
+#ifndef EP11_HSMSIM
+             compare_ck_version(&ep11_data->ep11_lib_version, &ver4_1) < 0 ||
+#endif
+             ep11_data->target_info->used_firmware_API_version < 6)) {
+            TRACE_ERROR("Strict-session or VHSM mode requires an EP11 "
+                        "host library >= v4.1 and API ordinal >= 6 when "
+                        "running in a Secure Execution for Linux guest\n");
+            OCK_SYSLOG(LOG_ERR, "Slot %lu: Strict-session or VHSM mode "
+                       "requires an EP11 host library >= v4.1 and API ordinal "
+                       ">= 6 when running in a Secure Execution for Linux "
+                       "guest\n", tokdata->slot_id);
             rc = CKR_FUNCTION_FAILED;
             goto error;
         }