eda8259e8d21ca71329bcef17bcd2355c861281f
Ingo Franzki <ifranzki@linux.ibm.com>
2026-02-11 11:13:46 +0100

    TESTCASES: Add tests for ML-DSA of PKCS#11 v3.2
    Test ML-DSA of PKCS#11 v3.2.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- testcases/crypto/crypto.mk
+++ testcases/crypto/crypto.mk
@@ -6,12 +6,15 @@ noinst_PROGRAMS += testcases/crypto/aes_tests				\
 	testcases/crypto/rsaupdate_tests				\
 	testcases/crypto/dilithium_tests testcases/crypto/ab_tests	\
 	testcases/crypto/kyber_tests testcases/crypto/ibm_ml_dsa_tests	\
-	testcases/crypto/ibm_ml_kem_tests
+	testcases/crypto/ibm_ml_kem_tests				\
+	testcases/crypto/ml_dsa_tests
 noinst_HEADERS +=							\
 	testcases/crypto/aes.h testcases/crypto/des.h			\
 	testcases/crypto/des3.h testcases/crypto/digest.h		\
 	testcases/crypto/ec.h testcases/crypto/rsa.h			\
-	testcases/crypto/dilithium.h testcases/crypto/kyber.h
+	testcases/crypto/dilithium.h testcases/crypto/kyber.h		\
+	testcases/crypto/ibm_ml_dsa.h testcases/crypto/ibm_ml_kem.h	\
+	testcases/crypto/ml_dsa.h
 
 testcases_crypto_aes_tests_CFLAGS = ${testcases_inc}
 testcases_crypto_aes_tests_LDADD = testcases/common/libcommon.la
@@ -74,3 +77,7 @@ testcases_crypto_ibm_ml_dsa_tests_SOURCES = testcases/crypto/ibm_ml_dsa_func.c
 testcases_crypto_ibm_ml_kem_tests_CFLAGS = ${testcases_inc}
 testcases_crypto_ibm_ml_kem_tests_LDADD = testcases/common/libcommon.la
 testcases_crypto_ibm_ml_kem_tests_SOURCES = testcases/crypto/ibm_ml_kem_func.c
+
+testcases_crypto_ml_dsa_tests_CFLAGS = ${testcases_inc}
+testcases_crypto_ml_dsa_tests_LDADD = testcases/common/libcommon.la
+testcases_crypto_ml_dsa_tests_SOURCES = testcases/crypto/ml_dsa_func.c
