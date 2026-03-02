f0205d02bb68df8293e3c2506f99ecf4b2fce54b
Ingo Franzki <ifranzki@linux.ibm.com>
2026-03-02 11:39:52 +0100

    EP11: Add support for encapsulate/decapsulate with PKCS#11 v3.2 ML-KEM mechanisms
    Internally the PKCS#11 v3.2 mechanism CKM_ML_KEM is mapped to the IBM-specific
    mechanism CKM_IBM_ML_KEM.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/ep11_stdll/tok_struct.h
+++ usr/lib/ep11_stdll/tok_struct.h
@@ -160,8 +160,8 @@ token_spec_t token_specific = {
     &token_specific_ml_dsa_sign,
     &token_specific_ml_dsa_verify,
     NULL,                       // ml_kem_generate_keypair
-    NULL,                       // ml_kem_encapsulate_key
-    NULL,                       // ml_kem_decapsulate_key
+    &token_specific_ml_kem_encapsulate_key,
+    &token_specific_ml_kem_decapsulate_key,
     NULL,                       // get_mechanism_list
     NULL,                       // get mechanism_info
     &token_specific_object_add,
