opencryptoki/opencryptoki

commit f4fae7029bf749ba9af0f4e1ae941f9c1bae9ca3

COMMON: Fix DH derive to produce the requested secret length
PKCS#11 states for CKM_DH_PKCS_DERIVE:
  "It computes a Diffie-Hellman secret value from the public value and private
  key according to [PKCS #3], and truncates the result according to the
  CKA_KEY_TYPE attribute of the template and, if it has one and the key type
  supports it, the CKA_VALUE_LEN attribute of the template. (The truncation
  removes bytes from the leading end of the secret value.)."

The truncation is currently not done, resulting an a CKA_VALUE of the size
of the prime.

Fix this by determining the desired secret size from either the key type, or
the CKA_VALUE_LEN attribute. If the desired secret length can not be
determined, or the desired secret length is larger than the prime, then
an error is returned.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/common/mech_dh.c
@@ -41,6 +41,21 @@
 
 #ifndef NODH
 
+static CK_ULONG keylen_from_keytype(CK_ULONG keytype)
+{
+    switch (keytype) {
+    case CKK_DES:
+        return 8;
+    case CKK_DES2:
+        return 16;
+    case CKK_DES3:
+        return 24;
+        /* for all other keytypes CKA_VALUE_LEN must be specified */
+    default:
+        return 0;
+    }
+}
+
 //
 //
 CK_RV dh_pkcs_derive(STDLL_TokData_t *tokdata,
@@ -54,6 +69,7 @@ CK_RV dh_pkcs_derive(STDLL_TokData_t *tokdata,
     CK_RV rc;
     CK_ULONG keyclass = 0, keytype = 0;
     CK_ATTRIBUTE *new_attr;
+    CK_ULONG value_len = 0, secret_len;
     OBJECT *temp_obj = NULL;
 
     CK_BYTE secret_key_value[256];
@@ -102,8 +118,29 @@ CK_RV dh_pkcs_derive(STDLL_TokData_t *tokdata,
     if (rc != CKR_OK)
         return rc;
 
+    rc = get_ulong_attribute_by_type(pTemplate, ulCount, CKA_VALUE_LEN,
+                                     &value_len);
+    if (rc == CKR_ATTRIBUTE_VALUE_INVALID) {
+        TRACE_ERROR("%s\n", ock_err(ERR_ATTRIBUTE_VALUE_INVALID));
+        return CKR_ATTRIBUTE_VALUE_INVALID;
+    }
+
+    secret_len = keylen_from_keytype(keytype);
+    if (secret_len == 0)
+        secret_len = value_len;
+    if (secret_len == 0) {
+        /* Neither CKA_VALUE_LEN nor predefined length by key type */
+        TRACE_ERROR("%s\n", ock_err(ERR_TEMPLATE_INCONSISTENT));
+        return CKR_TEMPLATE_INCONSISTENT;
+    }
+    if (secret_len > secret_key_value_len) {
+        /* Requested key size can not be derived */
+        TRACE_ERROR("%s\n", ock_err(ERR_KEY_SIZE_RANGE));
+        return CKR_KEY_SIZE_RANGE;
+    }
+
     // Build the attribute from the vales that were returned back
-    rc = build_attribute(CKA_VALUE, secret_key_value, secret_key_value_len,
+    rc = build_attribute(CKA_VALUE, secret_key_value, secret_len,
                          &new_attr);
     if (rc != CKR_OK) {
         TRACE_DEVEL("Failed to build the new attribute.\n");

commit c2e800168ca87762beb203a7e569cd7c2572df9b

COMMON: Fix DH derive to truncate the derived secret on the left
PKCS#11 states for CKM_DH_PKCS_DERIVE:
  "It computes a Diffie-Hellman secret value from the public value and private
  key according to [PKCS #3], and truncates the result according to the
  CKA_KEY_TYPE attribute of the template and, if it has one and the key type
  supports it, the CKA_VALUE_LEN attribute of the template. (The truncation
  removes bytes from the leading end of the secret value.)."

Commit f4fae7029bf749ba9af0f4e1ae941f9c1bae9ca3 added the missing truncation,
but erroneously truncates at the trailing end (on the right), but not as required
by PKCS#11 on the leading end (on the left).

Fixes: f4fae7029bf749ba9af0f4e1ae941f9c1bae9ca3

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/lib/common/mech_dh.c
@@ -140,8 +140,9 @@ CK_RV dh_pkcs_derive(STDLL_TokData_t *tokdata,
     }
 
     // Build the attribute from the vales that were returned back
-    rc = build_attribute(CKA_VALUE, secret_key_value, secret_len,
-                         &new_attr);
+    rc = build_attribute(CKA_VALUE, secret_key_value +
+                                        secret_key_value_len - secret_len,
+                         secret_len, &new_attr);
     if (rc != CKR_OK) {
         TRACE_DEVEL("Failed to build the new attribute.\n");
         return rc;