opencryptoki/opencryptoki

commit 1440e408f84e0ebba15eedfeb6c7191cdba239aa

COMMON: Add OIDs of ML-DSA and ML-KEM algorithms
While at it, rename the length info element for dilithium to ml_dsa,
and use it for both, Dilithium as well as ML-DSA. The key components
are the same, just different sizes dependent on the variant.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/include/pqc_oids.h
@@ -45,4 +45,28 @@
 #define OCK_KYBER_R2_1024          { 0x06, 0x0B, 0x2B, 0x06, 0x01, 0x04, \
                                      0x01, 0x02, 0x82, 0x0B, 0x05, 0x04, 0x04 }
 
+/* ML-DSA 44: 2.16.840.1.101.3.4.3.17 */
+#define OCK_ML_DSA_44              { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+                                     0x65, 0x03, 0x04, 0x03, 0x11 }
+
+/* ML-DSA 65: 2.16.840.1.101.3.4.3.18 */
+#define OCK_ML_DSA_65              { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+                                     0x65, 0x03, 0x04, 0x03, 0x12 }
+
+/* ML-DSA 87: 2.16.840.1.101.3.4.3.19 */
+#define OCK_ML_DSA_87              { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+                                     0x65, 0x03, 0x04, 0x03, 0x13 }
+
+/* ML-KEM 512: 2.16.840.1.101.3.4.4.1 */
+#define OCK_ML_KEM_512             { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+                                     0x65, 0x03, 0x04, 0x04, 0x01 }
+
+/* ML-KEM 768: 2.16.840.1.101.3.4.4.2 */
+#define OCK_ML_KEM_786             { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+                                     0x65, 0x03, 0x04, 0x04, 0x02 }
+
+/* ML-KEM 1024: 2.16.840.1.101.3.4.4.3 */
+#define OCK_ML_KEM_1024            { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+                                     0x65, 0x03, 0x04, 0x04, 0x03 }
+
 #endif                          // _PQC_OIDS_H_

usr/lib/common/pqc_defs.h
@@ -24,6 +24,14 @@ extern const CK_BYTE dilithium_r3_87[];
 extern const CK_BYTE kyber_r2_768[];
 extern const CK_BYTE kyber_r2_1024[];
 
+extern const CK_BYTE ml_dsa_44[];
+extern const CK_BYTE ml_dsa_65[];
+extern const CK_BYTE ml_dsa_87[];
+
+extern const CK_BYTE ml_kem_512[];
+extern const CK_BYTE ml_kem_786[];
+extern const CK_BYTE ml_kem_1024[];
+
 struct pqc_oid {
     const CK_BYTE *oid;
     CK_ULONG oid_len;
@@ -39,12 +47,22 @@ struct pqc_oid {
             CK_ULONG s2_len;
             CK_ULONG t0_len;
             CK_ULONG t1_len;
-        } dilithium;
+            CK_ULONG priv_seed_len;
+        } ml_dsa;
+        struct {
+            CK_ULONG sk_len;
+            CK_ULONG pk_len;
+            CK_ULONG fs_len;
+            CK_ULONG priv_seed_len;
+            CK_ULONG pubseed_len;
+        } ml_kem;
     } len_info;
 };
 
 extern const struct pqc_oid dilithium_oids[];
 extern const struct pqc_oid kyber_oids[];
+extern const struct pqc_oid ml_dsa_oids[];
+extern const struct pqc_oid ml_kem_oids[];
 
 const struct pqc_oid *find_pqc_by_keyform(const struct pqc_oid *pqcs,
                                           CK_ULONG keyform);

usr/lib/common/pqc_supported.c
@@ -22,38 +22,38 @@ const struct pqc_oid dilithium_oids[] = {
     { .oid = dilithium_r2_65, .oid_len = sizeof(dilithium_r2_65),
       .keyform = CK_IBM_DILITHIUM_KEYFORM_ROUND2_65,
       .policy_size = 256, .policy_siglen = 3366,
-      .len_info.dilithium.rho_len = 32, .len_info.dilithium.seed_len = 32,
-      .len_info.dilithium.tr_len = 48, .len_info.dilithium.s1_len = 480,
-      .len_info.dilithium.s2_len = 576, .len_info.dilithium.t0_len = 2688,
-      .len_info.dilithium.t1_len = 1728, },
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 48, .len_info.ml_dsa.s1_len = 480,
+      .len_info.ml_dsa.s2_len = 576, .len_info.ml_dsa.t0_len = 2688,
+      .len_info.ml_dsa.t1_len = 1728, },
     { .oid = dilithium_r2_87, .oid_len = sizeof(dilithium_r2_87),
       .keyform = CK_IBM_DILITHIUM_KEYFORM_ROUND2_87,
       .policy_size = 256, .policy_siglen = 4668,
-      .len_info.dilithium.rho_len = 32, .len_info.dilithium.seed_len = 32,
-      .len_info.dilithium.tr_len = 48, .len_info.dilithium.s1_len = 672,
-      .len_info.dilithium.s2_len = 768, .len_info.dilithium.t0_len = 3584,
-      .len_info.dilithium.t1_len = 2304, },
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 48, .len_info.ml_dsa.s1_len = 672,
+      .len_info.ml_dsa.s2_len = 768, .len_info.ml_dsa.t0_len = 3584,
+      .len_info.ml_dsa.t1_len = 2304, },
     { .oid = dilithium_r3_44, .oid_len = sizeof(dilithium_r3_44),
       .keyform = CK_IBM_DILITHIUM_KEYFORM_ROUND3_44,
       .policy_size = 256, .policy_siglen = 2420,
-      .len_info.dilithium.rho_len = 32, .len_info.dilithium.seed_len = 32,
-      .len_info.dilithium.tr_len = 32, .len_info.dilithium.s1_len = 384,
-      .len_info.dilithium.s2_len = 384, .len_info.dilithium.t0_len = 1664,
-      .len_info.dilithium.t1_len = 1280, },
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 32, .len_info.ml_dsa.s1_len = 384,
+      .len_info.ml_dsa.s2_len = 384, .len_info.ml_dsa.t0_len = 1664,
+      .len_info.ml_dsa.t1_len = 1280, },
     { .oid = dilithium_r3_65, .oid_len = sizeof(dilithium_r3_65),
       .keyform = CK_IBM_DILITHIUM_KEYFORM_ROUND3_65,
       .policy_size = 256, .policy_siglen = 3293,
-      .len_info.dilithium.rho_len = 32, .len_info.dilithium.seed_len = 32,
-      .len_info.dilithium.tr_len = 32, .len_info.dilithium.s1_len = 640,
-      .len_info.dilithium.s2_len = 768, .len_info.dilithium.t0_len = 2496,
-      .len_info.dilithium.t1_len = 1920, },
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 32, .len_info.ml_dsa.s1_len = 640,
+      .len_info.ml_dsa.s2_len = 768, .len_info.ml_dsa.t0_len = 2496,
+      .len_info.ml_dsa.t1_len = 1920, },
     { .oid = dilithium_r3_87, .oid_len = sizeof(dilithium_r3_87),
       .keyform = CK_IBM_DILITHIUM_KEYFORM_ROUND3_87,
       .policy_size = 256, .policy_siglen = 4595,
-      .len_info.dilithium.rho_len = 32, .len_info.dilithium.seed_len = 32,
-      .len_info.dilithium.tr_len = 32, .len_info.dilithium.s1_len = 672,
-      .len_info.dilithium.s2_len = 768, .len_info.dilithium.t0_len = 3328,
-      .len_info.dilithium.t1_len = 2560, },
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 32, .len_info.ml_dsa.s1_len = 672,
+      .len_info.ml_dsa.s2_len = 768, .len_info.ml_dsa.t0_len = 3328,
+      .len_info.ml_dsa.t1_len = 2560, },
     { .oid = NULL, .oid_len = 0, .keyform = 0,
       .policy_size = 0, .policy_siglen = 0 }
 };
@@ -64,10 +64,71 @@ const CK_BYTE kyber_r2_1024[] = OCK_KYBER_R2_1024;
 const struct pqc_oid kyber_oids[] = {
     { .oid = kyber_r2_768, .oid_len = sizeof(kyber_r2_768),
        .keyform = CK_IBM_KYBER_KEYFORM_ROUND2_768,
-       .policy_size = 256, .policy_siglen = 0 },
+       .policy_size = 256, .policy_siglen = 0,
+       .len_info.ml_kem.sk_len = 2400, .len_info.ml_kem.pk_len = 1184,
+       .len_info.ml_kem.fs_len = 64, .len_info.ml_kem.pubseed_len = 32 },
     { .oid = kyber_r2_1024, .oid_len = sizeof(kyber_r2_1024),
       .keyform = CK_IBM_KYBER_KEYFORM_ROUND2_1024,
-      .policy_size = 256, .policy_siglen = 0 },
+      .policy_size = 256, .policy_siglen = 0,
+      .len_info.ml_kem.sk_len = 3168, .len_info.ml_kem.pk_len = 1568,
+      .len_info.ml_kem.fs_len = 64, .len_info.ml_kem.pubseed_len = 32 },
+    { .oid = NULL, .oid_len = 0, .keyform = 0,
+      .policy_size = 0, .policy_siglen = 0 }
+};
+
+const CK_BYTE ml_dsa_44[] = OCK_ML_DSA_44;
+const CK_BYTE ml_dsa_65[] = OCK_ML_DSA_65;
+const CK_BYTE ml_dsa_87[] = OCK_ML_DSA_87;
+
+const struct pqc_oid ml_dsa_oids[] = {
+    { .oid = ml_dsa_44, .oid_len = sizeof(ml_dsa_44),
+      .keyform = CKP_IBM_ML_DSA_44,
+      .policy_size = 128, .policy_siglen = 128,
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 64, .len_info.ml_dsa.s1_len = 384,
+      .len_info.ml_dsa.s2_len = 384, .len_info.ml_dsa.t0_len = 1664,
+      .len_info.ml_dsa.t1_len = 1280, .len_info.ml_dsa.priv_seed_len = 32, },
+    { .oid = ml_dsa_65, .oid_len = sizeof(ml_dsa_65),
+      .keyform = CKP_IBM_ML_DSA_65,
+      .policy_size = 192, .policy_siglen = 192,
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 64, .len_info.ml_dsa.s1_len = 640,
+      .len_info.ml_dsa.s2_len = 768, .len_info.ml_dsa.t0_len = 2496,
+      .len_info.ml_dsa.t1_len = 1920, .len_info.ml_dsa.priv_seed_len = 32, },
+    { .oid = ml_dsa_87, .oid_len = sizeof(ml_dsa_87),
+      .keyform = CKP_IBM_ML_DSA_87,
+      .policy_size = 256, .policy_siglen = 256,
+      .len_info.ml_dsa.rho_len = 32, .len_info.ml_dsa.seed_len = 32,
+      .len_info.ml_dsa.tr_len = 64, .len_info.ml_dsa.s1_len = 672,
+      .len_info.ml_dsa.s2_len = 768, .len_info.ml_dsa.t0_len = 3328,
+      .len_info.ml_dsa.t1_len = 2560, .len_info.ml_dsa.priv_seed_len = 32, },
+    { .oid = NULL, .oid_len = 0, .keyform = 0,
+      .policy_size = 0, .policy_siglen = 0 }
+};
+
+const CK_BYTE ml_kem_512[] = OCK_ML_KEM_512;
+const CK_BYTE ml_kem_786[] = OCK_ML_KEM_786;
+const CK_BYTE ml_kem_1024[] = OCK_ML_KEM_1024;
+
+const struct pqc_oid ml_kem_oids[] = {
+    { .oid = ml_kem_512, .oid_len = sizeof(ml_kem_512),
+      .keyform = CKP_IBM_ML_KEM_512,
+      .policy_size = 126, .policy_siglen = 0,
+      .len_info.ml_kem.sk_len = 1632, .len_info.ml_kem.pk_len = 800,
+      .len_info.ml_kem.fs_len = 64, .len_info.ml_kem.priv_seed_len = 64,
+      .len_info.ml_kem.pubseed_len = 32, },
+    { .oid = ml_kem_786, .oid_len = sizeof(ml_kem_786),
+      .keyform = CKP_IBM_ML_KEM_768,
+      .policy_size = 192, .policy_siglen = 0,
+      .len_info.ml_kem.sk_len = 2400, .len_info.ml_kem.pk_len = 1184,
+      .len_info.ml_kem.fs_len = 64, .len_info.ml_kem.priv_seed_len = 64,
+      .len_info.ml_kem.pubseed_len = 32 },
+    { .oid = ml_kem_1024, .oid_len = sizeof(ml_kem_1024),
+      .keyform = CKP_IBM_ML_KEM_1024,
+      .policy_size = 256, .policy_siglen = 0,
+      .len_info.ml_kem.sk_len = 3168, .len_info.ml_kem.pk_len = 1568,
+      .len_info.ml_kem.fs_len = 64, .len_info.ml_kem.priv_seed_len = 64,
+      .len_info.ml_kem.pubseed_len = 32 },
     { .oid = NULL, .oid_len = 0, .keyform = 0,
       .policy_size = 0, .policy_siglen = 0 }
 };

usr/lib/cca_stdll/cca_specific.c
@@ -9557,8 +9557,8 @@ CK_RV token_create_ibm_dilithium_keypair(TEMPLATE *publ_tmpl,
     t1_len = be16toh(*((uint16_t *)(publ_tok + CCA_QSA_EXTTOK_PUBLKEY_OFFSET +
                                     CCA_QSA_EXTTOK_T1_OFFSET)));
 
-    if (rho_len != oid->len_info.dilithium.rho_len ||
-        t1_len != oid->len_info.dilithium.t1_len) {
+    if (rho_len != oid->len_info.ml_dsa.rho_len ||
+        t1_len != oid->len_info.ml_dsa.t1_len) {
         TRACE_ERROR("CCA QSA key token has invalid key component length\n");
         return CKR_FUNCTION_FAILED;
     }

usr/lib/common/mech_pqc.c
@@ -183,12 +183,12 @@ CK_RV ibm_dilithium_pack_priv_key(TEMPLATE *templ, const struct pqc_oid *oid,
     CK_RV rc;
 
     if (buf == NULL) {
-        *buf_len = oid->len_info.dilithium.rho_len +
-                   oid->len_info.dilithium.seed_len +
-                   oid->len_info.dilithium.tr_len +
-                   oid->len_info.dilithium.s1_len +
-                   oid->len_info.dilithium.s2_len +
-                   oid->len_info.dilithium.t0_len;
+        *buf_len = oid->len_info.ml_dsa.rho_len +
+                   oid->len_info.ml_dsa.seed_len +
+                   oid->len_info.ml_dsa.tr_len +
+                   oid->len_info.ml_dsa.s1_len +
+                   oid->len_info.ml_dsa.s2_len +
+                   oid->len_info.ml_dsa.t0_len;
         return CKR_OK;
     }
 
@@ -234,12 +234,12 @@ CK_RV ibm_dilithium_pack_priv_key(TEMPLATE *templ, const struct pqc_oid *oid,
         return rc;
     }
 
-    PACK_PART(rho, oid->len_info.dilithium.rho_len, buf, *buf_len, ofs);
-    PACK_PART(seed, oid->len_info.dilithium.seed_len, buf, *buf_len, ofs);
-    PACK_PART(tr, oid->len_info.dilithium.tr_len, buf, *buf_len, ofs);
-    PACK_PART(s1, oid->len_info.dilithium.s1_len, buf, *buf_len, ofs);
-    PACK_PART(s2, oid->len_info.dilithium.s2_len, buf, *buf_len, ofs);
-    PACK_PART(t0, oid->len_info.dilithium.t0_len, buf, *buf_len, ofs);
+    PACK_PART(rho, oid->len_info.ml_dsa.rho_len, buf, *buf_len, ofs);
+    PACK_PART(seed, oid->len_info.ml_dsa.seed_len, buf, *buf_len, ofs);
+    PACK_PART(tr, oid->len_info.ml_dsa.tr_len, buf, *buf_len, ofs);
+    PACK_PART(s1, oid->len_info.ml_dsa.s1_len, buf, *buf_len, ofs);
+    PACK_PART(s2, oid->len_info.ml_dsa.s2_len, buf, *buf_len, ofs);
+    PACK_PART(t0, oid->len_info.ml_dsa.t0_len, buf, *buf_len, ofs);
 
     *buf_len = ofs;
 
@@ -254,8 +254,8 @@ CK_RV ibm_dilithium_pack_pub_key(TEMPLATE *templ, const struct pqc_oid *oid,
     CK_RV rc;
 
     if (buf == NULL) {
-        *buf_len = oid->len_info.dilithium.rho_len +
-                   oid->len_info.dilithium.t1_len;
+        *buf_len = oid->len_info.ml_dsa.rho_len +
+                   oid->len_info.ml_dsa.t1_len;
         return CKR_OK;
     }
 
@@ -273,8 +273,8 @@ CK_RV ibm_dilithium_pack_pub_key(TEMPLATE *templ, const struct pqc_oid *oid,
         return rc;
     }
 
-    PACK_PART(rho, oid->len_info.dilithium.rho_len, buf, *buf_len, ofs);
-    PACK_PART(t1, oid->len_info.dilithium.t1_len, buf, *buf_len, ofs);
+    PACK_PART(rho, oid->len_info.ml_dsa.rho_len, buf, *buf_len, ofs);
+    PACK_PART(t1, oid->len_info.ml_dsa.t1_len, buf, *buf_len, ofs);
 
     *buf_len = ofs;
 
@@ -304,17 +304,17 @@ CK_RV ibm_dilithium_unpack_priv_key(CK_BYTE *buf, CK_ULONG buf_len,
     CK_ULONG ofs = 0;
     CK_RV rc;
 
-    UNPACK_PART(rho, CKA_IBM_DILITHIUM_RHO, oid->len_info.dilithium.rho_len,
+    UNPACK_PART(rho, CKA_IBM_DILITHIUM_RHO, oid->len_info.ml_dsa.rho_len,
                 buf, buf_len, ofs, rc, out);
-    UNPACK_PART(seed, CKA_IBM_DILITHIUM_SEED, oid->len_info.dilithium.seed_len,
+    UNPACK_PART(seed, CKA_IBM_DILITHIUM_SEED, oid->len_info.ml_dsa.seed_len,
                 buf, buf_len, ofs, rc, out);
-    UNPACK_PART(tr, CKA_IBM_DILITHIUM_TR, oid->len_info.dilithium.tr_len,
+    UNPACK_PART(tr, CKA_IBM_DILITHIUM_TR, oid->len_info.ml_dsa.tr_len,
                 buf, buf_len, ofs, rc, out);
-    UNPACK_PART(s1, CKA_IBM_DILITHIUM_S1, oid->len_info.dilithium.s1_len,
+    UNPACK_PART(s1, CKA_IBM_DILITHIUM_S1, oid->len_info.ml_dsa.s1_len,
                 buf, buf_len, ofs, rc, out);
-    UNPACK_PART(s2, CKA_IBM_DILITHIUM_S2, oid->len_info.dilithium.s2_len,
+    UNPACK_PART(s2, CKA_IBM_DILITHIUM_S2, oid->len_info.ml_dsa.s2_len,
                 buf, buf_len, ofs, rc, out);
-    UNPACK_PART(t0, CKA_IBM_DILITHIUM_T0, oid->len_info.dilithium.t0_len,
+    UNPACK_PART(t0, CKA_IBM_DILITHIUM_T0, oid->len_info.ml_dsa.t0_len,
                 buf, buf_len, ofs, rc, out);
 
     rc = template_update_attribute(templ, rho);
@@ -384,9 +384,9 @@ CK_RV ibm_dilithium_unpack_pub_key(CK_BYTE *buf, CK_ULONG buf_len,
     CK_ULONG ofs = 0;
     CK_RV rc;
 
-    UNPACK_PART(rho, CKA_IBM_DILITHIUM_RHO, oid->len_info.dilithium.rho_len,
+    UNPACK_PART(rho, CKA_IBM_DILITHIUM_RHO, oid->len_info.ml_dsa.rho_len,
                 buf, buf_len, ofs, rc, out);
-    UNPACK_PART(t1, CKA_IBM_DILITHIUM_T1, oid->len_info.dilithium.t1_len,
+    UNPACK_PART(t1, CKA_IBM_DILITHIUM_T1, oid->len_info.ml_dsa.t1_len,
                 buf, buf_len, ofs, rc, out);
 
     rc = template_update_attribute(templ, rho);

usr/sbin/p11sak/p11sak.c
@@ -6641,12 +6641,12 @@ static CK_RV p11sak_import_dilithium_pkey(const struct p11tool_objtype *keytype,
         if (rc != CKR_OK)
             goto out;
 
-        if (priv_len != oid->len_info.dilithium.rho_len +
-                        oid->len_info.dilithium.seed_len +
-                        oid->len_info.dilithium.tr_len +
-                        oid->len_info.dilithium.s1_len +
-                        oid->len_info.dilithium.s2_len +
-                        oid->len_info.dilithium.t0_len) {
+        if (priv_len != oid->len_info.ml_dsa.rho_len +
+                        oid->len_info.ml_dsa.seed_len +
+                        oid->len_info.ml_dsa.tr_len +
+                        oid->len_info.ml_dsa.s1_len +
+                        oid->len_info.ml_dsa.s2_len +
+                        oid->len_info.ml_dsa.t0_len) {
             warnx("Size of private Dilithium key is not valid.");
             rc = CKR_FUNCTION_FAILED;
             goto out;
@@ -6655,47 +6655,47 @@ static CK_RV p11sak_import_dilithium_pkey(const struct p11tool_objtype *keytype,
         ofs = 0;
         rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_RHO,
                                    priv_key + ofs,
-                                   oid->len_info.dilithium.rho_len,
+                                   oid->len_info.ml_dsa.rho_len,
                                    attrs, num_attrs);
         if (rc != CKR_OK)
            goto out;
 
-        ofs += oid->len_info.dilithium.rho_len;
+        ofs += oid->len_info.ml_dsa.rho_len;
         rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_SEED,
                                    priv_key + ofs,
-                                   oid->len_info.dilithium.seed_len,
+                                   oid->len_info.ml_dsa.seed_len,
                                    attrs, num_attrs);
         if (rc != CKR_OK)
            goto out;
 
-        ofs += oid->len_info.dilithium.seed_len;
+        ofs += oid->len_info.ml_dsa.seed_len;
         rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_TR,
                                    priv_key + ofs,
-                                   oid->len_info.dilithium.tr_len,
+                                   oid->len_info.ml_dsa.tr_len,
                                    attrs, num_attrs);
         if (rc != CKR_OK)
            goto out;
 
-        ofs += oid->len_info.dilithium.tr_len;
+        ofs += oid->len_info.ml_dsa.tr_len;
         rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_S1,
                                    priv_key + ofs,
-                                   oid->len_info.dilithium.s1_len,
+                                   oid->len_info.ml_dsa.s1_len,
                                    attrs, num_attrs);
         if (rc != CKR_OK)
            goto out;
 
-        ofs += oid->len_info.dilithium.s1_len;
+        ofs += oid->len_info.ml_dsa.s1_len;
         rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_S2,
                                    priv_key + ofs,
-                                   oid->len_info.dilithium.s2_len,
+                                   oid->len_info.ml_dsa.s2_len,
                                    attrs, num_attrs);
         if (rc != CKR_OK)
            goto out;
 
-        ofs += oid->len_info.dilithium.s2_len;
+        ofs += oid->len_info.ml_dsa.s2_len;
         rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_T0,
                                    priv_key + ofs,
-                                   oid->len_info.dilithium.t0_len,
+                                   oid->len_info.ml_dsa.t0_len,
                                    attrs, num_attrs);
         if (rc != CKR_OK)
            goto out;
@@ -6707,8 +6707,8 @@ static CK_RV p11sak_import_dilithium_pkey(const struct p11tool_objtype *keytype,
     if (rc != CKR_OK)
         goto out;
 
-    if (pub_len != oid->len_info.dilithium.rho_len +
-                   oid->len_info.dilithium.t1_len) {
+    if (pub_len != oid->len_info.ml_dsa.rho_len +
+                   oid->len_info.ml_dsa.t1_len) {
         warnx("Size of public Dilithium key is not valid.");
         rc = CKR_FUNCTION_FAILED;
         goto out;
@@ -6716,14 +6716,14 @@ static CK_RV p11sak_import_dilithium_pkey(const struct p11tool_objtype *keytype,
 
     ofs = 0;
     rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_RHO,
-                               pub_key + ofs, oid->len_info.dilithium.rho_len,
+                               pub_key + ofs, oid->len_info.ml_dsa.rho_len,
                                attrs, num_attrs);
     if (rc != CKR_OK)
        goto out;
 
-    ofs += oid->len_info.dilithium.rho_len;
+    ofs += oid->len_info.ml_dsa.rho_len;
     rc = p11tool_add_attribute(CKA_IBM_DILITHIUM_T1,
-                               pub_key + ofs, oid->len_info.dilithium.t1_len,
+                               pub_key + ofs, oid->len_info.ml_dsa.t1_len,
                                attrs, num_attrs);
     if (rc != CKR_OK)
        goto out;
@@ -8338,12 +8338,12 @@ static CK_RV p11sak_export_dilithium_pkey(const struct p11tool_objtype *keytype,
     }
 
     if (private) {
-        priv_len = oid->len_info.dilithium.rho_len +
-                   oid->len_info.dilithium.seed_len +
-                   oid->len_info.dilithium.tr_len +
-                   oid->len_info.dilithium.s1_len +
-                   oid->len_info.dilithium.s2_len +
-                   oid->len_info.dilithium.t0_len;
+        priv_len = oid->len_info.ml_dsa.rho_len +
+                   oid->len_info.ml_dsa.seed_len +
+                   oid->len_info.ml_dsa.tr_len +
+                   oid->len_info.ml_dsa.s1_len +
+                   oid->len_info.ml_dsa.s2_len +
+                   oid->len_info.ml_dsa.t0_len;
 
         priv_key = calloc(1, priv_len);
         if (priv_key == NULL) {
@@ -8354,22 +8354,22 @@ static CK_RV p11sak_export_dilithium_pkey(const struct p11tool_objtype *keytype,
 
         ofs = 0;
         priv_attrs[0].pValue = priv_key + ofs;
-        priv_attrs[0].ulValueLen = oid->len_info.dilithium.rho_len;
-        ofs += oid->len_info.dilithium.rho_len;
+        priv_attrs[0].ulValueLen = oid->len_info.ml_dsa.rho_len;
+        ofs += oid->len_info.ml_dsa.rho_len;
         priv_attrs[1].pValue = priv_key + ofs;
-        priv_attrs[1].ulValueLen = oid->len_info.dilithium.seed_len;
-        ofs += oid->len_info.dilithium.seed_len;
+        priv_attrs[1].ulValueLen = oid->len_info.ml_dsa.seed_len;
+        ofs += oid->len_info.ml_dsa.seed_len;
         priv_attrs[2].pValue = priv_key + ofs;
-        priv_attrs[2].ulValueLen = oid->len_info.dilithium.tr_len;
-        ofs += oid->len_info.dilithium.tr_len;
+        priv_attrs[2].ulValueLen = oid->len_info.ml_dsa.tr_len;
+        ofs += oid->len_info.ml_dsa.tr_len;
         priv_attrs[3].pValue = priv_key + ofs;
-        priv_attrs[3].ulValueLen = oid->len_info.dilithium.s1_len;
-        ofs += oid->len_info.dilithium.s1_len;
+        priv_attrs[3].ulValueLen = oid->len_info.ml_dsa.s1_len;
+        ofs += oid->len_info.ml_dsa.s1_len;
         priv_attrs[4].pValue = priv_key + ofs;
-        priv_attrs[4].ulValueLen = oid->len_info.dilithium.s2_len;
-        ofs += oid->len_info.dilithium.s2_len;
+        priv_attrs[4].ulValueLen = oid->len_info.ml_dsa.s2_len;
+        ofs += oid->len_info.ml_dsa.s2_len;
         priv_attrs[5].pValue = priv_key + ofs;
-        priv_attrs[5].ulValueLen = oid->len_info.dilithium.t0_len;
+        priv_attrs[5].ulValueLen = oid->len_info.ml_dsa.t0_len;
 
         rc = p11tool_pkcs11_funcs->C_GetAttributeValue(p11tool_pkcs11_session,
                                                        key, priv_attrs, 6);
@@ -8382,12 +8382,12 @@ static CK_RV p11sak_export_dilithium_pkey(const struct p11tool_objtype *keytype,
             goto out;
         }
 
-        if (priv_attrs[0].ulValueLen != oid->len_info.dilithium.rho_len ||
-            priv_attrs[1].ulValueLen != oid->len_info.dilithium.seed_len ||
-            priv_attrs[2].ulValueLen != oid->len_info.dilithium.tr_len ||
-            priv_attrs[3].ulValueLen != oid->len_info.dilithium.s1_len ||
-            priv_attrs[4].ulValueLen != oid->len_info.dilithium.s2_len ||
-            priv_attrs[5].ulValueLen != oid->len_info.dilithium.t0_len) {
+        if (priv_attrs[0].ulValueLen != oid->len_info.ml_dsa.rho_len ||
+            priv_attrs[1].ulValueLen != oid->len_info.ml_dsa.seed_len ||
+            priv_attrs[2].ulValueLen != oid->len_info.ml_dsa.tr_len ||
+            priv_attrs[3].ulValueLen != oid->len_info.ml_dsa.s1_len ||
+            priv_attrs[4].ulValueLen != oid->len_info.ml_dsa.s2_len ||
+            priv_attrs[5].ulValueLen != oid->len_info.ml_dsa.t0_len) {
             warnx("Failed to retrieve private key attributes from %s key "
                   "object \"%s\": Private key component lengths are wrong.",
                   keytype->name, label);
@@ -8396,8 +8396,8 @@ static CK_RV p11sak_export_dilithium_pkey(const struct p11tool_objtype *keytype,
         }
     }
 
-    pub_len = oid->len_info.dilithium.rho_len +
-              oid->len_info.dilithium.t1_len;
+    pub_len = oid->len_info.ml_dsa.rho_len +
+              oid->len_info.ml_dsa.t1_len;
 
     pub_key = calloc(1, pub_len);
     if (pub_key == NULL) {
@@ -8408,10 +8408,10 @@ static CK_RV p11sak_export_dilithium_pkey(const struct p11tool_objtype *keytype,
 
     ofs = 0;
     pub_attrs[0].pValue = pub_key + ofs;
-    pub_attrs[0].ulValueLen = oid->len_info.dilithium.rho_len;
-    ofs += oid->len_info.dilithium.rho_len;
+    pub_attrs[0].ulValueLen = oid->len_info.ml_dsa.rho_len;
+    ofs += oid->len_info.ml_dsa.rho_len;
     pub_attrs[1].pValue = pub_key + ofs;
-    pub_attrs[1].ulValueLen = oid->len_info.dilithium.t1_len;
+    pub_attrs[1].ulValueLen = oid->len_info.ml_dsa.t1_len;
 
     rc = p11tool_pkcs11_funcs->C_GetAttributeValue(p11tool_pkcs11_session, key,
                                                    pub_attrs, 2);
@@ -8424,8 +8424,8 @@ static CK_RV p11sak_export_dilithium_pkey(const struct p11tool_objtype *keytype,
         goto out;
     }
 
-    if (pub_attrs[0].ulValueLen != oid->len_info.dilithium.rho_len ||
-        pub_attrs[1].ulValueLen != oid->len_info.dilithium.t1_len) {
+    if (pub_attrs[0].ulValueLen != oid->len_info.ml_dsa.rho_len ||
+        pub_attrs[1].ulValueLen != oid->len_info.ml_dsa.t1_len) {
         warnx("Failed to retrieve public key attributes from %s key "
               "object \"%s\": Public key component lengths are wrong.",
               keytype->name, label);

commit 47b60586ff00c45cb17a76a9259764efddff44b0

COMMON: Fix typo in ML-KEM OID define and constant
External define OCK_ML_KEM_786 has a typo, it must be OCK_ML_KEM_768. Fix
this, but keep the old define for backward compatibility.

Also constant ml_kem_786 has the same typo, it must be ml_kem_768. Fix
this also (not externally visible).

Fixes: 1440e408f84e0ebba15eedfeb6c7191cdba239aa

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/include/pqc_oids.h
@@ -62,8 +62,9 @@
                                      0x65, 0x03, 0x04, 0x04, 0x01 }
 
 /* ML-KEM 768: 2.16.840.1.101.3.4.4.2 */
-#define OCK_ML_KEM_786             { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
+#define OCK_ML_KEM_768             { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \
                                      0x65, 0x03, 0x04, 0x04, 0x02 }
+#define OCK_ML_KEM_786              OCK_ML_KEM_768 /* backward  compatibility */
 
 /* ML-KEM 1024: 2.16.840.1.101.3.4.4.3 */
 #define OCK_ML_KEM_1024            { 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, \

usr/lib/common/pqc_defs.h
@@ -29,7 +29,7 @@ extern const CK_BYTE ml_dsa_65[];
 extern const CK_BYTE ml_dsa_87[];
 
 extern const CK_BYTE ml_kem_512[];
-extern const CK_BYTE ml_kem_786[];
+extern const CK_BYTE ml_kem_768[];
 extern const CK_BYTE ml_kem_1024[];
 
 struct pqc_oid {

usr/lib/common/pqc_supported.c
@@ -107,7 +107,7 @@ const struct pqc_oid ml_dsa_oids[] = {
 };
 
 const CK_BYTE ml_kem_512[] = OCK_ML_KEM_512;
-const CK_BYTE ml_kem_786[] = OCK_ML_KEM_786;
+const CK_BYTE ml_kem_768[] = OCK_ML_KEM_768;
 const CK_BYTE ml_kem_1024[] = OCK_ML_KEM_1024;
 
 const struct pqc_oid ml_kem_oids[] = {
@@ -117,7 +117,7 @@ const struct pqc_oid ml_kem_oids[] = {
       .len_info.ml_kem.sk_len = 1632, .len_info.ml_kem.pk_len = 800,
       .len_info.ml_kem.fs_len = 64, .len_info.ml_kem.priv_seed_len = 64,
       .len_info.ml_kem.pubseed_len = 32, },
-    { .oid = ml_kem_786, .oid_len = sizeof(ml_kem_786),
+    { .oid = ml_kem_768, .oid_len = sizeof(ml_kem_768),
       .keyform = CKP_ML_KEM_768,
       .policy_size = 192, .policy_siglen = 0,
       .len_info.ml_kem.sk_len = 2400, .len_info.ml_kem.pk_len = 1184,