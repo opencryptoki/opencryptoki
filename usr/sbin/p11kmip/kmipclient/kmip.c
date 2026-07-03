bafa169472d6c933b699c2b19dc7d66f52342889
Ingo Franzki <ifranzki@linux.ibm.com>
2026-07-03 10:02:28 +0200

    P11KMIP: Fix invalid return of -1 for an unsigned int
    Functions kmip_node_get_structure_element_count() and
    kmip_node_get_structure_element_by_tag_count() return an unsigned int,
    but the error case returns -1. This leads to a wrap around actually
    returning 0xffffffff which the caller might interpret as a very large
    number of elements.
    
    Return 0 in case of an error instead.
    
    Also fix some callers to not unconditionally subtract -1 from the
    returned value,  but return an error if the returned value is zero.
    These callers check the preconditions upfront, so the error case
    won't be hit anyway.
    
    Assisted-by: IBM Bob:2.0.0
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/sbin/p11kmip/kmipclient/kmip.c
+++ usr/sbin/p11kmip/kmipclient/kmip.c
@@ -257,7 +257,7 @@ int kmip_node_add_structure_elements(struct kmip_node *node,
  *
  * @param node              the KMIP node
  *
- * @returns the number of elements, or -1 if  the node is not of type structure
+ * @returns the number of elements, or 0 if  the node is not of type structure
  */
 unsigned int kmip_node_get_structure_element_count(const struct kmip_node *node)
 {
@@ -265,10 +265,10 @@ unsigned int kmip_node_get_structure_element_count(const struct kmip_node *node)
 	unsigned int i;
 
 	if (node == NULL)
-		return -1;
+		return 0;
 
 	if (node->type != KMIP_TYPE_STRUCTURE)
-		return -1;
+		return 0;
 
 	element = node->structure_value;
 	for (i = 0; element != NULL; i++)
@@ -318,7 +318,7 @@ struct kmip_node *kmip_node_get_structure_element_by_index(
  * @param node              the KMIP node
  * @param tag               the tag to find
  *
- * @returns the number of elements, or -1 if  the node is not of type structure
+ * @returns the number of elements, or 0 if  the node is not of type structure
  */
 unsigned int kmip_node_get_structure_element_by_tag_count(
 					const struct kmip_node *node,
@@ -328,10 +328,10 @@ unsigned int kmip_node_get_structure_element_by_tag_count(
 	unsigned int i;
 
 	if (node == NULL)
-		return -1;
+		return 0;
 
 	if (node->type != KMIP_TYPE_STRUCTURE)
-		return -1;
+		return 0;
 
 	element = node->structure_value;
 	for (i = 0; element != NULL; element = element->next) {
