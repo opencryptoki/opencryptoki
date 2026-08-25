opencryptoki/opencryptoki

commit 7174f72dc6ea7a4a2951f78d3c30fc94a2e820f9

PKCSTOK_ADMIN: Validate token name for allowed characters
The token name is used to build the token directory path. If it contains
characters like '/' it might produce a path that is located outside
of the CONFIG_PATH. The token name must not be '.' or '..', and must
not contain '/'.

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/sbin/pkcstok_admin/pkcstok_admin.mk
@@ -1,6 +1,6 @@
 sbin_PROGRAMS += usr/sbin/pkcstok_admin/pkcstok_admin
 
-usr_sbin_pkcstok_admin_pkcstok_admin_LDFLAGS = -lrt
+usr_sbin_pkcstok_admin_pkcstok_admin_LDFLAGS = -lrt -lcrypto
 
 usr_sbin_pkcstok_admin_pkcstok_admin_CFLAGS  =		\
 	-DSTDLL_NAME=\"pkcstok_admin\"			\
@@ -9,7 +9,8 @@ usr_sbin_pkcstok_admin_pkcstok_admin_CFLAGS  =		\
 	-I${srcdir}/usr/sbin/pkcstok_admin
 
 usr_sbin_pkcstok_admin_pkcstok_admin_SOURCES =		\
-	usr/sbin/pkcstok_admin/pkcstok_admin.c
+	usr/sbin/pkcstok_admin/pkcstok_admin.c		\
+	usr/lib/common/pkcs_utils.c
 
 if AIX
 usr_sbin_pkcstok_admin_pkcstok_admin_SOURCES += 	\

usr/sbin/pkcstok_admin/pkcstok_admin.c
@@ -38,6 +38,8 @@
 #include <pwd.h>
 
 #include "slotmgr.h"
+#define OCK_TOOL
+#include "pkcs_utils.h"
 
 #if defined(_AIX)
 #define SHM_PREFIX  "/"
@@ -54,6 +56,8 @@
 
 static bool verbose = false;
 static bool force = false;
+
+pkcs_trace_level_t trace_level = TRACE_LEVEL_NONE;
 
 static void print_usage(const char *progname)
 {
@@ -999,6 +1003,13 @@ int main(int argc, char **argv)
     }
     if (strcmp(token, "HSM_MK_CHANGE") == 0) {
         warnx("The token name 'HSM_MK_CHANGE' is reserved and can not be used.");
+        rc = EXIT_FAILURE;
+        goto out;
+    }
+    if (!is_valid_filename_component(token)) {
+        warnx("The token name '%s' is not valid. It must be 1 to %d "
+              "characters long and must not be '.' or '..', and must "
+              "not contain '/'", token, NAME_MAX);
         rc = EXIT_FAILURE;
         goto out;
     }

commit 27bfcfd01c41763f9a779e5b6a665d6d4bd5f22e

PKCSTOK_ADMIN: Fix build error due to missing mechtable-gen.h
Include file mechtable-gen.h is generated, and thus needs statements
in the make file.

Fixes: 7174f72dc6ea7a4a2951f78d3c30fc94a2e820f9

Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

usr/sbin/pkcstok_admin/pkcstok_admin.mk
@@ -6,11 +6,15 @@ usr_sbin_pkcstok_admin_pkcstok_admin_CFLAGS  =		\
 	-DSTDLL_NAME=\"pkcstok_admin\"			\
 	-I${srcdir}/usr/include 			\
 	-I${srcdir}/usr/lib/common			\
+	-I${srcdir}/usr/lib/api				\
+	-I${top_builddir}/usr/lib/api			\
 	-I${srcdir}/usr/sbin/pkcstok_admin
 
 usr_sbin_pkcstok_admin_pkcstok_admin_SOURCES =		\
 	usr/sbin/pkcstok_admin/pkcstok_admin.c		\
 	usr/lib/common/pkcs_utils.c
+
+nodist_usr_sbin_pkcstok_admin_pkcstok_admin_SOURCES = usr/lib/api/mechtable.c
 
 if AIX
 usr_sbin_pkcstok_admin_pkcstok_admin_SOURCES += 	\